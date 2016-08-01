7c5fccd7a80a7eb164d08afb8c5e95e5be787e32
Mrunal Patel <mrunalp@gmail.com>
2016-08-01 16:48:12 -0700

    Add utility functions to set process as subreaper.
    This code is borrowed from libcontainer system package.
    
    Signed-off-by: Mrunal Patel <mrunalp@gmail.com>

--- utils/utils.go
+++ utils/utils.go
@@ -3,7 +3,10 @@ package utils
 import (
 	"bytes"
 	"os/exec"
+	"syscall"
 )
+
+const PR_SET_CHILD_SUBREAPER = 36
 
 func ExecCmd(name string, args ...string) (string, error) {
 	cmd := exec.Command(name, args...)
@@ -16,4 +19,18 @@ func ExecCmd(name string, args ...string) (string, error) {
 	}
 
 	return out.String(), nil
+}
+
+// SetSubreaper sets the value i as the subreaper setting for the calling process
+func SetSubreaper(i int) error {
+	return Prctl(PR_SET_CHILD_SUBREAPER, uintptr(i), 0, 0, 0)
+}
+
+// Prctl is a way to make the prctl linux syscall
+func Prctl(option int, arg2, arg3, arg4, arg5 uintptr) (err error) {
+	_, _, e1 := syscall.Syscall6(syscall.SYS_PRCTL, uintptr(option), arg2, arg3, arg4, arg5, 0)
+	if e1 != 0 {
+		err = e1
+	}
+	return
 }
