hmeng-19/ocid

Add first version of createPodSandbox
Signed-off-by: Haiyan Meng <hmeng@redhat.com>

cmd/client/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -14,6 +15,39 @@ import (
 const (
 	address = "localhost:49999"
 )
+
+func loadPodSandboxConfig(path string) (*pb.PodSandboxConfig, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil, fmt.Errorf("pod sandbox config at %s not found", path)
+		}
+		return nil, err
+	}
+	defer f.Close()
+
+	var config pb.PodSandboxConfig
+	if err := json.NewDecoder(f).Decode(&config); err != nil {
+		return nil, err
+	}
+	return &config, nil
+}
+
+// CreatePodSandbox sends a CreatePodSandboxRequest to the server, and parses
+// the returned CreatePodSandboxResponse.
+func CreatePodSandbox(client pb.RuntimeServiceClient, path string) error {
+	config, err := loadPodSandboxConfig(path)
+	if err != nil {
+		return err
+	}
+
+	r, err := client.CreatePodSandbox(context.Background(), &pb.CreatePodSandboxRequest{Config: config})
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
 
 // Version sends a VersionRequest to the server, and parses the returned VersionResponse.
 func Version(client pb.RuntimeServiceClient, version string) error {
@@ -32,6 +66,7 @@ func main() {
 
 	app.Commands = []cli.Command{
 		runtimeVersionCommand,
+		createPodSandboxCommand,
 		pullImageCommand,
 	}
 
@@ -86,6 +121,34 @@ var runtimeVersionCommand = cli.Command{
 		err = Version(client, version)
 		if err != nil {
 			return fmt.Errorf("Getting the runtime version failed: %v", err)
+		}
+		return nil
+	},
+}
+
+var createPodSandboxCommand = cli.Command{
+	Name:  "createpodsandbox",
+	Usage: "create a pod sandbox",
+	Flags: []cli.Flag{
+		cli.StringFlag{
+			Name:  "config",
+			Value: "podsandboxconfig.json",
+			Usage: "the path of a pod sandbox config file",
+		},
+	},
+	Action: func(context *cli.Context) error {
+		// Set up a connection to the server.
+		conn, err := grpc.Dial(address, grpc.WithInsecure())
+		if err != nil {
+			return fmt.Errorf("Failed to connect: %v", err)
+		}
+		defer conn.Close()
+		client := pb.NewRuntimeServiceClient(conn)
+
+		// Test RuntimeServiceClient.CreatePodSandbox
+		err = CreatePodSandbox(client, context.String("config"))
+		if err != nil {
+			return fmt.Errorf("Creating the pod sandbox failed: %v", err)
 		}
 		return nil
 	},

cmd/server/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"log"
 	"net"
+	"os"
 
 	"github.com/kubernetes/kubernetes/pkg/kubelet/api/v1alpha1/runtime"
 	"github.com/mrunalp/ocid/server"
+	"github.com/urfave/cli"
 	"google.golang.org/grpc"
 )
 
@@ -14,16 +16,39 @@ const (
 )
 
 func main() {
-	lis, err := net.Listen("tcp", port)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+	app := cli.NewApp()
+	app.Name = "ocic"
+	app.Usage = "client for ocid"
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "sandboxdir",
+			Value: "/var/lib/ocid/sandbox",
+			Usage: "ocid pod sandbox dir",
+		},
 	}
-	s := grpc.NewServer()
-	service, err := server.New("")
-	if err != nil {
+
+	app.Action = func(c *cli.Context) error {
+		lis, err := net.Listen("tcp", port)
+		if err != nil {
+			log.Fatalf("failed to listen: %v", err)
+		}
+
+		s := grpc.NewServer()
+
+		sandboxDir := c.String("sandboxdir")
+		service, err := server.New("", sandboxDir)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		runtime.RegisterRuntimeServiceServer(s, service)
+		runtime.RegisterImageServiceServer(s, service)
+		s.Serve(lis)
+		return nil
+	}
+
+	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
-	runtime.RegisterRuntimeServiceServer(s, service)
-	runtime.RegisterImageServiceServer(s, service)
-	s.Serve(lis)
 }

server/runtime.go
@@ -1,6 +1,9 @@
 package server
 
 import (
+	"fmt"
+	"io/ioutil"
+	"os"
 	"path/filepath"
 
 	pb "github.com/kubernetes/kubernetes/pkg/kubelet/api/v1alpha1/runtime"
@@ -37,16 +40,112 @@ func (s *Server) Version(ctx context.Context, req *pb.VersionRequest) (*pb.Versi
 func (s *Server) CreatePodSandbox(ctx context.Context, req *pb.CreatePodSandboxRequest) (*pb.CreatePodSandboxResponse, error) {
 	var err error
 
-	// TODO: Parametrize as a global argument to ocid
-	ocidSandboxDir := "/var/lib/ocid/sandbox"
-	podSandboxDir := filepath.Join(ocidSandboxDir, req.GetConfig().GetName())
+	// process req.Name
+	name := req.GetConfig().GetName()
+	if name == "" {
+		d, err := ioutil.TempDir(s.sandboxDir, "")
+		if err != nil {
+			return nil, err
+		}
+		name = filepath.Base(d)
+	}
+	podSandboxDir := filepath.Join(s.sandboxDir, name)
+	if err := os.MkdirAll(podSandboxDir, 0755); err != nil {
+		return nil, err
+	}
 
+	// creates a spec Generator with the default spec.
 	g := generate.New()
 
-	// TODO: Customize the config per the settings in the req
-	err = g.SaveToFile(filepath.Join(podSandboxDir, "config.json"))
+	// process req.Hostname
+	hostname := req.GetConfig().GetHostname()
+	if hostname != "" {
+		g.SetHostname(hostname)
+	}
 
-	return nil, err
+	// process req.LogDirectory
+	logDir := req.GetConfig().GetLogDirectory()
+	if logDir == "" {
+		logDir = fmt.Sprintf("/var/log/ocid/pods/%s", name)
+	}
+
+	// TODO: construct /etc/resolv.conf based on dnsOpts.
+	dnsOpts := req.GetConfig().GetDnsOptions()
+	fmt.Println(dnsOpts)
+
+	// TODO: the unit of cpu here is cores. How to map it into specs.Spec.Linux.Resouces.CPU?
+	cpu := req.GetConfig().GetResources().GetCpu()
+	if cpu != nil {
+		limits := cpu.GetLimits()
+		requests := cpu.GetRequests()
+		fmt.Println(limits)
+		fmt.Println(requests)
+	}
+
+	memory := req.GetConfig().GetResources().GetMemory()
+	if memory != nil {
+		// limits sets specs.Spec.Linux.Resouces.Memory.Limit
+		limits := memory.GetLimits()
+		if limits != 0 {
+			g.SetLinuxResourcesMemoryLimit(uint64(limits))
+		}
+
+		// requests sets specs.Spec.Linux.Resouces.Memory.Reservation
+		requests := memory.GetRequests()
+		if requests != 0 {
+			g.SetLinuxResourcesMemoryReservation(uint64(requests))
+		}
+	}
+
+	labels := req.GetConfig().GetLabels()
+	s.sandboxes = append(s.sandboxes, &sandbox{
+		name:   name,
+		logDir: logDir,
+		labels: labels,
+	})
+
+	annotations := req.GetConfig().GetAnnotations()
+	for k, v := range annotations {
+		err := g.AddAnnotation(fmt.Sprintf("%s=%s", k, v))
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// TODO: double check cgroupParent.
+	cgroupParent := req.GetConfig().GetLinux().GetCgroupParent()
+	if cgroupParent != "" {
+		g.SetLinuxCgroupsPath(cgroupParent)
+	}
+
+	// set up namespaces
+	if req.GetConfig().GetLinux().GetNamespaceOptions().GetHostNetwork() == false {
+		err := g.AddOrReplaceLinuxNamespace("network", "")
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if req.GetConfig().GetLinux().GetNamespaceOptions().GetHostPid() == false {
+		err := g.AddOrReplaceLinuxNamespace("pid", "")
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if req.GetConfig().GetLinux().GetNamespaceOptions().GetHostIpc() == false {
+		err := g.AddOrReplaceLinuxNamespace("ipc", "")
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	err = g.SaveToFile(filepath.Join(podSandboxDir, "config.json"))
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.CreatePodSandboxResponse{PodSandboxId: &name}, nil
 }
 
 // StopPodSandbox stops the sandbox. If there are any running containers in the

cri-o/cri-o

cmd/client/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net"
@@ -30,6 +31,39 @@ func getClientConnection() (*grpc.ClientConn, error) {
 	return conn, nil
 }
 
+func loadPodSandboxConfig(path string) (*pb.PodSandboxConfig, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil, fmt.Errorf("pod sandbox config at %s not found", path)
+		}
+		return nil, err
+	}
+	defer f.Close()
+
+	var config pb.PodSandboxConfig
+	if err := json.NewDecoder(f).Decode(&config); err != nil {
+		return nil, err
+	}
+	return &config, nil
+}
+
+// CreatePodSandbox sends a CreatePodSandboxRequest to the server, and parses
+// the returned CreatePodSandboxResponse.
+func CreatePodSandbox(client pb.RuntimeServiceClient, path string) error {
+	config, err := loadPodSandboxConfig(path)
+	if err != nil {
+		return err
+	}
+
+	r, err := client.CreatePodSandbox(context.Background(), &pb.CreatePodSandboxRequest{Config: config})
+	if err != nil {
+		return err
+	}
+	fmt.Println(r)
+	return nil
+}
+
 // Version sends a VersionRequest to the server, and parses the returned VersionResponse.
 func Version(client pb.RuntimeServiceClient, version string) error {
 	r, err := client.Version(context.Background(), &pb.VersionRequest{Version: &version})
@@ -47,6 +81,7 @@ func main() {
 
 	app.Commands = []cli.Command{
 		runtimeVersionCommand,
+		createPodSandboxCommand,
 		pullImageCommand,
 	}
 
@@ -101,6 +136,34 @@ var runtimeVersionCommand = cli.Command{
 		err = Version(client, version)
 		if err != nil {
 			return fmt.Errorf("Getting the runtime version failed: %v", err)
+		}
+		return nil
+	},
+}
+
+var createPodSandboxCommand = cli.Command{
+	Name:  "createpodsandbox",
+	Usage: "create a pod sandbox",
+	Flags: []cli.Flag{
+		cli.StringFlag{
+			Name:  "config",
+			Value: "config.json",
+			Usage: "the path of a pod sandbox config file",
+		},
+	},
+	Action: func(context *cli.Context) error {
+		// Set up a connection to the server.
+		conn, err := getClientConnection()
+		if err != nil {
+			return fmt.Errorf("Failed to connect: %v", err)
+		}
+		defer conn.Close()
+		client := pb.NewRuntimeServiceClient(conn)
+
+		// Test RuntimeServiceClient.CreatePodSandbox
+		err = CreatePodSandbox(client, context.String("config"))
+		if err != nil {
+			return fmt.Errorf("Creating the pod sandbox failed: %v", err)
 		}
 		return nil
 	},

cmd/server/main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/kubernetes/kubernetes/pkg/kubelet/api/v1alpha1/runtime"
 	"github.com/mrunalp/ocid/server"
+	"github.com/urfave/cli"
 	"google.golang.org/grpc"
 )
 
@@ -15,22 +16,45 @@ const (
 )
 
 func main() {
-	// Remove the socket if it already exists
-	if _, err := os.Stat(unixDomainSocket); err == nil {
-		if err := os.Remove(unixDomainSocket); err != nil {
+	app := cli.NewApp()
+	app.Name = "ocic"
+	app.Usage = "client for ocid"
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "sandboxdir",
+			Value: "/var/lib/ocid/sandbox",
+			Usage: "ocid pod sandbox dir",
+		},
+	}
+
+	app.Action = func(c *cli.Context) error {
+		// Remove the socket if it already exists
+		if _, err := os.Stat(unixDomainSocket); err == nil {
+			if err := os.Remove(unixDomainSocket); err != nil {
+				log.Fatal(err)
+			}
+		}
+		lis, err := net.Listen("unix", unixDomainSocket)
+		if err != nil {
+			log.Fatalf("failed to listen: %v", err)
+		}
+
+		s := grpc.NewServer()
+
+		sandboxDir := c.String("sandboxdir")
+		service, err := server.New("", sandboxDir)
+		if err != nil {
 			log.Fatal(err)
 		}
+
+		runtime.RegisterRuntimeServiceServer(s, service)
+		runtime.RegisterImageServiceServer(s, service)
+		s.Serve(lis)
+		return nil
 	}
-	lis, err := net.Listen("unix", unixDomainSocket)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-	s := grpc.NewServer()
-	service, err := server.New("")
-	if err != nil {
+
+	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
-	runtime.RegisterRuntimeServiceServer(s, service)
-	runtime.RegisterImageServiceServer(s, service)
-	s.Serve(lis)
 }

server/runtime.go
@@ -1,6 +1,9 @@
 package server
 
 import (
+	"fmt"
+	"io/ioutil"
+	"os"
 	"path/filepath"
 
 	pb "github.com/kubernetes/kubernetes/pkg/kubelet/api/v1alpha1/runtime"
@@ -37,16 +40,124 @@ func (s *Server) Version(ctx context.Context, req *pb.VersionRequest) (*pb.Versi
 func (s *Server) CreatePodSandbox(ctx context.Context, req *pb.CreatePodSandboxRequest) (*pb.CreatePodSandboxResponse, error) {
 	var err error
 
-	// TODO: Parametrize as a global argument to ocid
-	ocidSandboxDir := "/var/lib/ocid/sandbox"
-	podSandboxDir := filepath.Join(ocidSandboxDir, req.GetConfig().GetName())
+	if err := os.MkdirAll(s.sandboxDir, 0755); err != nil {
+		return nil, err
+	}
 
+	// process req.Name
+	name := req.GetConfig().GetName()
+	var podSandboxDir string
+
+	if name == "" {
+		podSandboxDir, err := ioutil.TempDir(s.sandboxDir, "")
+		if err != nil {
+			return nil, err
+		}
+		name = filepath.Base(podSandboxDir)
+	} else {
+		podSandboxDir = filepath.Join(s.sandboxDir, name)
+		if _, err := os.Stat(podSandboxDir); err == nil {
+			return nil, fmt.Errorf("pod sandbox (%s) already exists", podSandboxDir)
+		}
+
+		if err := os.MkdirAll(podSandboxDir, 0755); err != nil {
+			return nil, err
+		}
+
+	}
+
+	// creates a spec Generator with the default spec.
 	g := generate.New()
 
-	// TODO: Customize the config per the settings in the req
-	err = g.SaveToFile(filepath.Join(podSandboxDir, "config.json"))
+	// process req.Hostname
+	hostname := req.GetConfig().GetHostname()
+	if hostname != "" {
+		g.SetHostname(hostname)
+	}
 
-	return nil, err
+	// process req.LogDirectory
+	logDir := req.GetConfig().GetLogDirectory()
+	if logDir == "" {
+		logDir = fmt.Sprintf("/var/log/ocid/pods/%s", name)
+	}
+
+	// TODO: construct /etc/resolv.conf based on dnsOpts.
+	dnsOpts := req.GetConfig().GetDnsOptions()
+	fmt.Println(dnsOpts)
+
+	// TODO: the unit of cpu here is cores. How to map it into specs.Spec.Linux.Resouces.CPU?
+	cpu := req.GetConfig().GetResources().GetCpu()
+	if cpu != nil {
+		limits := cpu.GetLimits()
+		requests := cpu.GetRequests()
+		fmt.Println(limits)
+		fmt.Println(requests)
+	}
+
+	memory := req.GetConfig().GetResources().GetMemory()
+	if memory != nil {
+		// limits sets specs.Spec.Linux.Resouces.Memory.Limit
+		limits := memory.GetLimits()
+		if limits != 0 {
+			g.SetLinuxResourcesMemoryLimit(uint64(limits))
+		}
+
+		// requests sets specs.Spec.Linux.Resouces.Memory.Reservation
+		requests := memory.GetRequests()
+		if requests != 0 {
+			g.SetLinuxResourcesMemoryReservation(uint64(requests))
+		}
+	}
+
+	labels := req.GetConfig().GetLabels()
+	s.sandboxes = append(s.sandboxes, &sandbox{
+		name:   name,
+		logDir: logDir,
+		labels: labels,
+	})
+
+	annotations := req.GetConfig().GetAnnotations()
+	for k, v := range annotations {
+		err := g.AddAnnotation(fmt.Sprintf("%s=%s", k, v))
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// TODO: double check cgroupParent.
+	cgroupParent := req.GetConfig().GetLinux().GetCgroupParent()
+	if cgroupParent != "" {
+		g.SetLinuxCgroupsPath(cgroupParent)
+	}
+
+	// set up namespaces
+	if req.GetConfig().GetLinux().GetNamespaceOptions().GetHostNetwork() == false {
+		err := g.AddOrReplaceLinuxNamespace("network", "")
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if req.GetConfig().GetLinux().GetNamespaceOptions().GetHostPid() == false {
+		err := g.AddOrReplaceLinuxNamespace("pid", "")
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if req.GetConfig().GetLinux().GetNamespaceOptions().GetHostIpc() == false {
+		err := g.AddOrReplaceLinuxNamespace("ipc", "")
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	err = g.SaveToFile(filepath.Join(podSandboxDir, "config.json"))
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.CreatePodSandboxResponse{PodSandboxId: &name}, nil
 }
 
 // StopPodSandbox stops the sandbox. If there are any running containers in the