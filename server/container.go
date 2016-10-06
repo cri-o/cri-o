a56cbb4117db080da6c52595ff462b04bf38ab2a
Mrunal Patel <mrunalp@gmail.com>
2016-10-06 12:49:04 -0700

    Add a helper to get container from request
    Signed-off-by: Mrunal Patel <mrunalp@gmail.com>

--- server/container.go
+++ server/container.go
@@ -41,6 +41,28 @@ func (s *Server) generateContainerIDandName(podName string, name string, attempt
 	return id, name, err
 }
 
+type containerRequest interface {
+	GetContainerId() string
+}
+
+func (s *Server) getContainerFromRequest(req containerRequest) (*oci.Container, error) {
+	ctrID := req.GetContainerId()
+	if ctrID == "" {
+		return nil, fmt.Errorf("container ID should not be empty")
+	}
+
+	containerID, err := s.ctrIDIndex.Get(ctrID)
+	if err != nil {
+		return nil, fmt.Errorf("container with ID starting with %s not found: %v", ctrID, err)
+	}
+
+	c := s.state.containers.Get(containerID)
+	if c == nil {
+		return nil, fmt.Errorf("specified container not found: %s", containerID)
+	}
+	return c, nil
+}
+
 // CreateContainer creates a new container in specified PodSandbox
 func (s *Server) CreateContainer(ctx context.Context, req *pb.CreateContainerRequest) (res *pb.CreateContainerResponse, err error) {
 	sbID := req.GetPodSandboxId()
