d59830330f9a0bd4d150c07b6e1fd5f5121c3025
Crazykev <crazykev@zju.edu.cn>
2016-12-14 14:32:12 +0800

    add a todo for unimplement manager method
    Signed-off-by: Crazykev <crazykev@zju.edu.cn>

---
+++ manager/image_remove.go
@@ -0,0 +1,9 @@
+package manager
+
+import pb "k8s.io/kubernetes/pkg/kubelet/api/v1alpha1/runtime"
+
+// RemoveImage removes the image.
+func (m *Manager) RemoveImage(imageSpec *pb.ImageSpec) error {
+	// TODO: implement this
+	return nil
+}
