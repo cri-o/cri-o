d5abfa1ecff3a4dd0943038b38d3b009034adcfb
Aleksa Sarai <asarai@suse.de>
2017-01-25 04:12:51 +1100

    server: respect ocid.network.plugin_dir setting
    Previously ocicni did not have support for setting the plugin directory.
    Now that it has grown support for it, use it to actually respect the
    setting a user has provided for ocid.network.* options.
    
    Signed-off-by: Aleksa Sarai <asarai@suse.de>

--- server/server.go
+++ server/server.go
@@ -476,7 +476,7 @@ func New(config *Config) (*Server, error) {
 	}
 	sandboxes := make(map[string]*sandbox)
 	containers := oci.NewMemoryStore()
-	netPlugin, err := ocicni.InitCNI(config.NetworkDir)
+	netPlugin, err := ocicni.InitCNI(config.NetworkDir, config.PluginDir)
 	if err != nil {
 		return nil, err
 	}
