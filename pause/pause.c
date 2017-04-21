8219f30f289cddd77263b67b3005e8cc88136254
Mrunal Patel <mrunalp@gmail.com>
2017-04-21 16:17:53 -0700

    pause: Fix compilation warning
    psignal needs a feature check macro
    
    Signed-off-by: Mrunal Patel <mrunalp@gmail.com>

--- pause/pause.c
+++ pause/pause.c
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+#define _GNU_SOURCE
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
