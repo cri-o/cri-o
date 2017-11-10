4f4e228274cee8b6bc82f6585305f13961ca16dd
Antonio Murdaca <runcom@redhat.com>
2017-11-10 14:21:22 +0100

    pkg: storage: image: close image after using it
    Signed-off-by: Antonio Murdaca <runcom@redhat.com>

--- pkg/storage/image.go
+++ pkg/storage/image.go
@@ -159,8 +159,8 @@ func (svc *imageService) ImageStatus(systemContext *types.SystemContext, nameOrI
 	if err != nil {
 		return nil, err
 	}
+	defer img.Close()
 	size := imageSize(img)
-	img.Close()
 
 	result := ImageResult{
 		ID:           image.ID,
