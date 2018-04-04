92693140323a27b0c7ef6458d6c9a15d245ebf5a
W. Trevor King <wking@tremily.us>
2018-04-04 09:27:38 -0700

    server/sandbox_stop: Pass context through StopAllPodSandboxes
    Avoid [1]:
    
      panic: runtime error: invalid memory address or nil pointer dereference
      [signal SIGSEGV: segmentation violation code=0x1 addr=0x20 pc=0x91b13f]
      goroutine 1 [running]:
      ...
      runtime.sigpanic()
              /.../runtime/signal_unix.go:388 +0x17a fp=0xc42093a670 sp=0xc42093a620 pc=0x44816a
      github.com/kubernetes-incubator/cri-o/oci.waitContainerStop(0x0, 0x0, 0xc4208a2140, 0x2540be400, 0x1873be0, 0xc42000e020)
              /.../cri-o/oci/oci.go:589 +0xaf fp=0xc42093a7a0 sp=0xc42093a670 pc=0x91b13f
      github.com/kubernetes-incubator/cri-o/oci.(*Runtime).StopContainer(0xc4205294a0, 0x0, 0x0, 0xc4208a2140, 0xa, 0x0, 0x0)
              /.../cri-o/oci/oci.go:620 +0x57f fp=0xc42093a920 sp=0xc42093a7a0 pc=0x91b99f
      github.com/kubernetes-incubator/cri-o/server.(*Server).StopPodSandbox(0xc4202dc000, 0x0, 0x0, 0xc4201e0fd0, 0x0, 0x0, 0x0)
              /.../cri-o/server/sandbox_stop.go:62 +0x29c fp=0xc42093abc8 sp=0xc42093a920 pc=0x13387bc
      github.com/kubernetes-incubator/cri-o/server.(*Server).StopAllPodSandboxes(0xc4202dc000)
              /.../cri-o/server/sandbox_stop.go:125 +0x112 fp=0xc42093ac80 sp=0xc42093abc8 pc=0x1339712
      github.com/kubernetes-incubator/cri-o/server.(*Server).cleanupSandboxesOnShutdown(0xc4202dc000)
              /.../cri-o/server/server.go:153 +0x85 fp=0xc42093acd0 sp=0xc42093ac80 pc=0x133be15
      github.com/kubernetes-incubator/cri-o/server.(*Server).Shutdown(0xc4202dc000, 0x2, 0x0)
              /.../cri-o/server/server.go:168 +0x2b fp=0xc42093acf8 sp=0xc42093acd0 pc=0x133beeb
      ...
    
    by passing a Context instance down the stack so we don't have to
    hard-code nil in StopAllPodSandboxes' StopPodSandbox call.
    
    Backported from d3b76361 (server/sandbox_stop: Pass context through
    StopAllPodSandboxes, 2018-04-04, #1494), fixing a trivial merge
    conflict in cmd/crio/main.go.
    
    [1]: https://github.com/kubernetes-incubator/cri-o/issues/1490#issue-311282538
    
    Signed-off-by: W. Trevor King <wking@tremily.us>

--- cmd/crio/main.go
+++ cmd/crio/main.go
@@ -164,11 +164,12 @@ func catchShutdown(gserver *grpc.Server, sserver *server.Server, hserver *http.S
 				continue
 			}
 			*signalled = true
+			ctx := context.Background()
 			gserver.GracefulStop()
-			hserver.Shutdown(context.Background())
+			hserver.Shutdown(ctx)
 			sserver.StopStreamServer()
 			sserver.StopExitMonitor()
-			if err := sserver.Shutdown(); err != nil {
+			if err := sserver.Shutdown(ctx); err != nil {
 				logrus.Warnf("error shutting down main service %v", err)
 			}
 			return
@@ -408,6 +409,7 @@ func main() {
 	}
 
 	app.Action = func(c *cli.Context) error {
+		ctx := context.Background()
 		if c.GlobalBool("profile") {
 			profilePort := c.GlobalInt("profile-port")
 			profileEndpoint := fmt.Sprintf("localhost:%v", profilePort)
@@ -454,7 +456,7 @@ func main() {
 
 		s := grpc.NewServer()
 
-		service, err := server.New(config)
+		service, err := server.New(ctx, config)
 		if err != nil {
 			logrus.Fatal(err)
 		}
@@ -522,7 +524,7 @@ func main() {
 		case <-serverCloseCh:
 		}
 
-		service.Shutdown()
+		service.Shutdown(ctx)
 
 		<-streamServerCloseCh
 		logrus.Debug("closed stream server")
