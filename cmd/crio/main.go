82063f941ccd88d37e4201517f609b4f67fae7ba
Giuseppe Scrivano <gscrivan@redhat.com>
2018-05-10 13:45:39 +0200

    crio: add global options --uid-mappings and --gid-mappings
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- cmd/crio/main.go
+++ cmd/crio/main.go
@@ -159,6 +159,12 @@ func mergeConfig(config *server.Config, ctx *cli.Context) error {
 	if ctx.GlobalIsSet("bind-mount-prefix") {
 		config.BindMountPrefix = ctx.GlobalString("bind-mount-prefix")
 	}
+	if ctx.GlobalIsSet("uid-mappings") {
+		config.UIDMappings = ctx.GlobalString("uid-mappings")
+	}
+	if ctx.GlobalIsSet("gid-mappings") {
+		config.GIDMappings = ctx.GlobalString("gid-mappings")
+	}
 	return nil
 }
 
@@ -371,6 +377,16 @@ func main() {
 		cli.StringFlag{
 			Name:  "bind-mount-prefix",
 			Usage: "specify a prefix to prepend to the source of a bind mount",
+		},
+		cli.StringFlag{
+			Name:  "uid-mappings",
+			Usage: "specify the UID mappings to use for the user namespace",
+			Value: "",
+		},
+		cli.StringFlag{
+			Name:  "gid-mappings",
+			Usage: "specify the GID mappings to use for the user namespace",
+			Value: "",
 		},
 	}
 
