6cf08a75f6e2674afb9536a05b36e5a4c43933ed
Antonio Murdaca <runcom@redhat.com>
2018-06-18 11:31:20 +0200

    sandbox_run: fix selinux relabel sharing
    Signed-off-by: Antonio Murdaca <runcom@redhat.com>

--- server/sandbox_run.go
+++ server/sandbox_run.go
@@ -353,7 +353,7 @@ func (s *Server) RunPodSandbox(ctx context.Context, req *pb.RunPodSandboxRequest
 			}
 			return nil, err
 		}
-		if err := label.Relabel(resolvPath, mountLabel, true); err != nil && err != unix.ENOTSUP {
+		if err := label.Relabel(resolvPath, mountLabel, false); err != nil && err != unix.ENOTSUP {
 			return nil, err
 		}
 		mnt := runtimespec.Mount{
@@ -676,7 +676,7 @@ func (s *Server) RunPodSandbox(ctx context.Context, req *pb.RunPodSandboxRequest
 	if err := ioutil.WriteFile(hostnamePath, []byte(hostname+"\n"), 0644); err != nil {
 		return nil, err
 	}
-	if err := label.Relabel(hostnamePath, mountLabel, true); err != nil && err != unix.ENOTSUP {
+	if err := label.Relabel(hostnamePath, mountLabel, false); err != nil && err != unix.ENOTSUP {
 		return nil, err
 	}
 	mnt = runtimespec.Mount{
