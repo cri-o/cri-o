7d54b74f660afc457b2ceebaf176054d733c4787
Qi Wang <qiwan@redhat.com>
2018-10-31 14:33:27 -0400

    vendor libpod changes
    Signed-off-by: Qi Wang <qiwan@redhat.com>

--- vendor/github.com/containers/libpod/pkg/rootless/rootless_unsupported.go
+++ vendor/github.com/containers/libpod/pkg/rootless/rootless_unsupported.go
@@ -21,3 +21,24 @@ func BecomeRootInUserNS() (bool, int, error) {
 func GetRootlessUID() int {
 	return -1
 }
+
+// SetSkipStorageSetup tells the runtime to not setup containers/storage
+func SetSkipStorageSetup(bool) {
+}
+
+// SkipStorageSetup tells if we should skip the containers/storage setup
+func SkipStorageSetup() bool {
+	return false
+}
+
+// JoinNS re-exec podman in a new userNS and join the user namespace of the specified
+// PID.
+func JoinNS(pid uint) (bool, int, error) {
+	return false, -1, errors.New("this function is not supported on this os")
+}
+
+// JoinNSPath re-exec podman in a new userNS and join the owner user namespace of the
+// specified path.
+func JoinNSPath(path string) (bool, int, error) {
+	return false, -1, errors.New("this function is not supported on this os")
+}
