cri-o/cri-o

commit 26b177d8c24982c979ff81c02f20857f955ec708

Add network namespace interface
Signed-off-by: Sascha Grunert <sgrunert@suse.com>

lib/sandbox/sandbox.go
@@ -7,6 +7,7 @@ import (
 	"sync"
 	"time"
 
+	"github.com/containernetworking/plugins/pkg/ns"
 	"github.com/kubernetes-sigs/cri-o/oci"
 	"github.com/sirupsen/logrus"
 	"k8s.io/apimachinery/pkg/fields"
@@ -24,8 +25,8 @@ func isSymbolicLink(path string) (bool, error) {
 }
 
 // NetNsGet returns the NetNs associated with the given nspath and name
-func NetNsGet(nspath, name string) (*NetNs, error) {
-	if err := isNSorErr(nspath); err != nil {
+func (s *Sandbox) NetNsGet(nspath, name string) (*NetNs, error) {
+	if err := ns.IsNSorErr(nspath); err != nil {
 		return nil, ErrClosedNetNS
 	}
 
@@ -58,7 +59,7 @@ func NetNsGet(nspath, name string) (*NetNs, error) {
 
 		netNs.symlink = fd
 	} else {
-		if err := netNs.symlinkCreate(name); err != nil {
+		if err := netNs.SymlinkCreate(name); err != nil {
 			return nil, err
 		}
 	}
@@ -86,7 +87,7 @@ type Sandbox struct {
 	containers     oci.ContainerStorer
 	processLabel   string
 	mountLabel     string
-	netns          *NetNs
+	netns          NetNsIface
 	metadata       *pb.PodSandboxMetadata
 	nsOpts         *pb.NamespaceOption
 	shmPath        string
@@ -106,6 +107,27 @@ type Sandbox struct {
 	seccompProfilePath string
 	createdAt          time.Time
 	hostNetwork        bool
+}
+
+// NetNsIface provides a generic network namespace interface
+type NetNsIface interface {
+	// Close closes this network namespace
+	Close() error
+
+	// Get returns the native NetNs
+	Get() *NetNs
+
+	// Initialize does the necessary setup
+	Initialize() (*NetNs, error)
+
+	// Initialized returns true if already initialized
+	Initialized() bool
+
+	// Remove ensures this network namespace handle is closed and removed
+	Remove() error
+
+	// SymlinkCreate creates all necessary symlinks
+	SymlinkCreate(string) error
 }
 
 const (
@@ -352,20 +374,24 @@ func (s *Sandbox) RemoveInfraContainer() {
 // NetNs retrieves the network namespace of the sandbox
 // If the sandbox uses the host namespace, nil is returned
 func (s *Sandbox) NetNs() *NetNs {
-	return s.netns
+	if s.netns == nil {
+		return nil
+	}
+	return s.netns.Get()
 }
 
 // NetNsPath returns the path to the network namespace of the sandbox.
 // If the sandbox uses the host namespace, nil is returned
 func (s *Sandbox) NetNsPath() string {
-	if s.netns == nil || s.netns.symlink == nil {
+	if s.netns == nil || s.netns.Get() == nil ||
+		s.netns.Get().symlink == nil {
 		if s.infraContainer != nil {
 			return fmt.Sprintf("/proc/%v/ns/net", s.infraContainer.State().Pid)
 		}
 		return ""
 	}
 
-	return s.netns.symlink.Name()
+	return s.netns.Get().symlink.Name()
 }
 
 // UserNsPath returns the path to the user namespace of the sandbox.
@@ -378,27 +404,33 @@ func (s *Sandbox) UserNsPath() string {
 }
 
 // NetNsCreate creates a new network namespace for the sandbox
-func (s *Sandbox) NetNsCreate() error {
-	if s.netns != nil {
-		return fmt.Errorf("net NS already created")
+func (s *Sandbox) NetNsCreate(netNs NetNsIface) error {
+	// Create a new netNs if nil provided
+	if netNs == nil {
+		netNs = &NetNs{}
 	}
 
-	netNS, err := newNetNs()
+	// Check if interface is already initialized
+	if netNs.Initialized() {
+		return fmt.Errorf("net NS already initialized")
+	}
+
+	netNS, err := netNs.Initialize()
 	if err != nil {
 		return err
 	}
-	s.netns = netNS
 
-	if err := s.netns.symlinkCreate(s.name); err != nil {
+	if err := netNs.SymlinkCreate(s.name); err != nil {
 		logrus.Warnf("Could not create nentns symlink %v", err)
 
-		if err1 := s.netns.Close(); err1 != nil {
+		if err1 := netNs.Close(); err1 != nil {
 			return err1
 		}
 
 		return err
 	}
 
+	s.netns = netNS
 	return nil
 }
 
@@ -422,7 +454,7 @@ func (s *Sandbox) NetNsJoin(nspath, name string) error {
 		return fmt.Errorf("sandbox already has a network namespace, cannot join another")
 	}
 
-	netNS, err := NetNsGet(nspath, name)
+	netNS, err := s.NetNsGet(nspath, name)
 	if err != nil {
 		return err
 	}

lib/sandbox/sandbox_linux.go
@@ -15,17 +15,24 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-func isNSorErr(nspath string) error {
-	return ns.IsNSorErr(nspath)
+// Get returns the NetNs for a given NetNsIface
+func (n *NetNs) Get() *NetNs {
+	return n
 }
 
-func newNetNs() (*NetNs, error) {
+// Initialized returns true if the NetNs is already initialized
+func (n *NetNs) Initialized() bool {
+	return n.initialized
+}
+
+// Initialize does the necessary setup for a NetNs
+func (n *NetNs) Initialize() (*NetNs, error) {
 	netNS, err := ns.NewNS()
 	if err != nil {
 		return nil, err
 	}
 
-	return &NetNs{netNS: netNS, closed: false}, nil
+	return &NetNs{netNS: netNS, closed: false, initialized: true}, nil
 }
 
 func getNetNs(path string) (*NetNs, error) {
@@ -40,13 +47,15 @@ func getNetNs(path string) (*NetNs, error) {
 // NetNs handles data pertaining a network namespace
 type NetNs struct {
 	sync.Mutex
-	netNS    ns.NetNS
-	symlink  *os.File
-	closed   bool
-	restored bool
+	netNS       ns.NetNS
+	symlink     *os.File
+	closed      bool
+	restored    bool
+	initialized bool
 }
 
-func (netns *NetNs) symlinkCreate(name string) error {
+// SymlinkCreate creates the necessary symlinks for the NetNs
+func (n *NetNs) SymlinkCreate(name string) error {
 	b := make([]byte, 4)
 	_, randErr := rand.Reader.Read(b)
 	if randErr != nil {
@@ -56,7 +65,7 @@ func (netns *NetNs) symlinkCreate(name string) error {
 	nsName := fmt.Sprintf("%s-%x", name, b)
 	symlinkPath := filepath.Join(NsRunDir, nsName)
 
-	if err := os.Symlink(netns.netNS.Path(), symlinkPath); err != nil {
+	if err := os.Symlink(n.netNS.Path(), symlinkPath); err != nil {
 		return err
 	}
 
@@ -69,54 +78,54 @@ func (netns *NetNs) symlinkCreate(name string) error {
 		return err
 	}
 
-	netns.symlink = fd
+	n.symlink = fd
 
 	return nil
 }
 
 // Path returns the path of the network namespace handle
-func (netns *NetNs) Path() string {
-	if netns == nil || netns.netNS == nil {
+func (n *NetNs) Path() string {
+	if n == nil || n.netNS == nil {
 		return ""
 	}
-	return netns.netNS.Path()
+	return n.netNS.Path()
 }
 
 // Close closes this network namespace
-func (netns *NetNs) Close() error {
-	if netns == nil || netns.netNS == nil {
+func (n *NetNs) Close() error {
+	if n == nil || n.netNS == nil {
 		return nil
 	}
-	return netns.netNS.Close()
+	return n.netNS.Close()
 }
 
 // Remove ensures this network namespace handle is closed and removed
-func (netns *NetNs) Remove() error {
-	netns.Lock()
-	defer netns.Unlock()
+func (n *NetNs) Remove() error {
+	n.Lock()
+	defer n.Unlock()
 
-	if netns.closed {
+	if n.closed {
 		// netNsRemove() can be called multiple
 		// times without returning an error.
 		return nil
 	}
 
-	if err := netns.symlinkRemove(); err != nil {
+	if err := n.symlinkRemove(); err != nil {
 		return err
 	}
 
-	if err := netns.Close(); err != nil {
+	if err := n.Close(); err != nil {
 		return err
 	}
 
-	netns.closed = true
+	n.closed = true
 
-	if netns.restored {
+	if n.restored {
 		// we got namespaces in the form of
 		// /var/run/netns/cni-0d08effa-06eb-a963-f51a-e2b0eceffc5d
 		// but /var/run on most system is symlinked to /run so we first resolve
 		// the symlink and then try and see if it's mounted
-		fp, err := symlink.FollowSymlinkInScope(netns.Path(), "/")
+		fp, err := symlink.FollowSymlinkInScope(n.Path(), "/")
 		if err != nil {
 			return err
 		}
@@ -126,8 +135,8 @@ func (netns *NetNs) Remove() error {
 			}
 		}
 
-		if netns.Path() != "" {
-			if err := os.RemoveAll(netns.Path()); err != nil {
+		if n.Path() != "" {
+			if err := os.RemoveAll(n.Path()); err != nil {
 				return err
 			}
 		}
@@ -136,12 +145,12 @@ func (netns *NetNs) Remove() error {
 	return nil
 }
 
-func (netns *NetNs) symlinkRemove() error {
-	if err := netns.symlink.Close(); err != nil {
+func (n *NetNs) symlinkRemove() error {
+	if err := n.symlink.Close(); err != nil {
 		return fmt.Errorf("failed to close net ns symlink: %v", err)
 	}
 
-	if err := os.RemoveAll(netns.symlink.Name()); err != nil {
+	if err := os.RemoveAll(n.symlink.Name()); err != nil {
 		return fmt.Errorf("failed to remove net ns symlink: %v", err)
 	}
 

lib/sandbox/sandbox_unsupported.go
@@ -7,12 +7,12 @@ import (
 	"os"
 )
 
-func isNSorErr(nspath string) error {
-	return fmt.Errorf("netns is not implemented for this platform")
+func (n *NetNs) Initialize() (*NetNs, error) {
+	return &NetNs{}, fmt.Errorf("netns is not implemented for this platform")
 }
 
-func newNetNs() (*NetNs, error) {
-	return &NetNs{}, fmt.Errorf("netns is not implemented for this platform")
+func (n *NetNs) Initialized() bool {
+	return false
 }
 
 func getNetNs(path string) (*NetNs, error) {
@@ -25,23 +25,27 @@ type NetNs struct {
 	symlink *os.File
 }
 
-func (netns *NetNs) Path() string {
+func (n *NetNs) Get() *NetNs {
+	return n
+}
+
+func (n *NetNs) Path() string {
 	return ""
 }
 
-func (netns *NetNs) symlinkCreate(name string) error {
+func (n *NetNs) SymlinkCreate(name string) error {
 	return nil
 }
 
-func (netns *NetNs) symlinkRemove() error {
+func (n *NetNs) symlinkRemove() error {
 	return nil
 }
 
-func (netns *NetNs) Close() error {
+func (n *NetNs) Close() error {
 	return nil
 }
 
-func (netns *NetNs) Remove() error {
+func (n *NetNs) Remove() error {
 	return nil
 }
 

server/sandbox_run_linux.go
@@ -460,7 +460,7 @@ func (s *Server) runPodSandbox(ctx context.Context, req *pb.RunPodSandboxRequest
 	} else {
 		if s.config.Config.ManageNetworkNSLifecycle {
 			// Create the sandbox network namespace
-			if err = sb.NetNsCreate(); err != nil {
+			if err = sb.NetNsCreate(nil); err != nil {
 				return nil, err
 			}
 

commit 281d80cbfba8e8d57f461fc2e7c559ca73c2504f

sandbox: fix segfault with manage_network_ns_lifecycle
fix this segfault:

github.com/kubernetes-sigs/cri-o/lib/sandbox.(*NetNs).SymlinkCreate(0xc0002107b0, 0xc000882180, 0x40, 0x0, 0xc000210690)
       src/github.com/kubernetes-sigs/cri-o/lib/sandbox/sandbox_linux.go:68 +0x1d2
github.com/kubernetes-sigs/cri-o/lib/sandbox.(*Sandbox).NetNsCreate(0xc0002a4000, 0x1d042a0, 0xc0002107b0, 0x1c, 0xc0003e2b08)
       src/github.com/kubernetes-sigs/cri-o/lib/sandbox/sandbox.go:414 +0x166
github.com/kubernetes-sigs/cri-o/server.(*Server).runPodSandbox(0xc0003d0480, 0x1cf14c0, 0xc0007fcd20, 0xc0005a28e0, 0x0, 0x0, 0x0)
       src/github.com/kubernetes-sigs/cri-o/server/sandbox_run_linux.go:460 +0x698c
github.com/kubernetes-sigs/cri-o/server.(*Server).RunPodSandbox(0xc0003d0480, 0x1cf14c0, 0xc0007fcd20, 0xc0005a28e0, 0xc0003d0480, 0x7f8028c0cc00, 0xc000229c28)
       src/github.com/kubernetes-sigs/cri-o/server/sandbox_run.go:122 +0x49
github.com/kubernetes-sigs/cri-o/vendor/k8s.io/kubernetes/pkg/kubelet/apis/cri/runtime/v1alpha2._RuntimeService_RunPodSandbox_Handler(0x1a017a0, 0xc0003d0480, 0x1cf14c0, 0xc0007fcd20, 0xc00017d0e0, 0x0, 0x0, 0x0, 0x0, 0x0)

regression introduced by: 26b177d8c24982c979ff81c02f20857f955ec708

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

lib/sandbox/sandbox.go
@@ -115,7 +115,7 @@ type NetNsIface interface {
 	Get() *NetNs
 
 	// Initialize does the necessary setup
-	Initialize() (*NetNs, error)
+	Initialize() (NetNsIface, error)
 
 	// Initialized returns true if already initialized
 	Initialized() bool
@@ -406,7 +406,7 @@ func (s *Sandbox) NetNsCreate(netNs NetNsIface) error {
 		return fmt.Errorf("net NS already initialized")
 	}
 
-	netNS, err := netNs.Initialize()
+	netNs, err := netNs.Initialize()
 	if err != nil {
 		return err
 	}
@@ -421,7 +421,7 @@ func (s *Sandbox) NetNsCreate(netNs NetNsIface) error {
 		return err
 	}
 
-	s.netns = netNS
+	s.netns = netNs
 	return nil
 }
 

lib/sandbox/sandbox_linux.go
@@ -26,13 +26,15 @@ func (n *NetNs) Initialized() bool {
 }
 
 // Initialize does the necessary setup for a NetNs
-func (n *NetNs) Initialize() (*NetNs, error) {
+func (n *NetNs) Initialize() (NetNsIface, error) {
 	netNS, err := ns.NewNS()
 	if err != nil {
 		return nil, err
 	}
-
-	return &NetNs{netNS: netNS, closed: false, initialized: true}, nil
+	n.netNS = netNS
+	n.closed = false
+	n.initialized = true
+	return n, nil
 }
 
 func getNetNs(path string) (*NetNs, error) {

lib/sandbox/sandbox_test.go
@@ -271,10 +271,9 @@ var _ = t.Describe("Sandbox", func() {
 			// Given
 			gomock.InOrder(
 				netNsIfaceMock.EXPECT().Initialized().Return(false),
-				netNsIfaceMock.EXPECT().Initialize().
-					Return(&sandbox.NetNs{}, nil),
-				netNsIfaceMock.EXPECT().SymlinkCreate(gomock.Any()).
-					Return(nil),
+				netNsIfaceMock.EXPECT().Initialize().Return(netNsIfaceMock, nil),
+				netNsIfaceMock.EXPECT().SymlinkCreate(gomock.Any()).Return(nil),
+				netNsIfaceMock.EXPECT().Remove().Return(nil),
 			)
 
 			// When
@@ -282,7 +281,7 @@ var _ = t.Describe("Sandbox", func() {
 
 			// Then
 			Expect(err).To(BeNil())
-			Expect(testSandbox.NetNsRemove()).NotTo(BeNil())
+			Expect(testSandbox.NetNsRemove()).To(BeNil())
 		})
 
 		It("should not crash when parameter is nil", func() {
@@ -298,7 +297,7 @@ var _ = t.Describe("Sandbox", func() {
 			gomock.InOrder(
 				netNsIfaceMock.EXPECT().Initialized().Return(false),
 				netNsIfaceMock.EXPECT().Initialize().
-					Return(&sandbox.NetNs{}, nil),
+					Return(netNsIfaceMock, nil),
 				netNsIfaceMock.EXPECT().SymlinkCreate(gomock.Any()).
 					Return(t.TestError),
 				netNsIfaceMock.EXPECT().Close().Return(nil),
@@ -316,7 +315,7 @@ var _ = t.Describe("Sandbox", func() {
 			gomock.InOrder(
 				netNsIfaceMock.EXPECT().Initialized().Return(false),
 				netNsIfaceMock.EXPECT().Initialize().
-					Return(&sandbox.NetNs{}, nil),
+					Return(netNsIfaceMock, nil),
 				netNsIfaceMock.EXPECT().SymlinkCreate(gomock.Any()).
 					Return(t.TestError),
 				netNsIfaceMock.EXPECT().Close().Return(t.TestError),
@@ -375,7 +374,7 @@ var _ = t.Describe("Sandbox", func() {
 			gomock.InOrder(
 				netNsIfaceMock.EXPECT().Initialized().Return(false),
 				netNsIfaceMock.EXPECT().Initialize().
-					Return(&sandbox.NetNs{}, nil),
+					Return(netNsIfaceMock, nil),
 				netNsIfaceMock.EXPECT().SymlinkCreate(gomock.Any()).
 					Return(nil),
 			)
@@ -384,6 +383,9 @@ var _ = t.Describe("Sandbox", func() {
 
 		It("should succeed", func() {
 			// Given
+			gomock.InOrder(
+				netNsIfaceMock.EXPECT().Get().Return(&sandbox.NetNs{}),
+			)
 			// When
 			ns, err := testSandbox.NetNsGet("/proc/self/ns",
 				"../../../tmp/test")
@@ -401,6 +403,9 @@ var _ = t.Describe("Sandbox", func() {
 
 		It("should succeed with symlink", func() {
 			// Given
+			gomock.InOrder(
+				netNsIfaceMock.EXPECT().Get().Return(&sandbox.NetNs{}),
+			)
 			const link = "ns-link"
 			Expect(os.Symlink("/proc/self/ns", link)).To(BeNil())
 			defer os.RemoveAll(link)

test/mocks/sandbox/sandbox.go
@@ -62,10 +62,10 @@ func (mr *MockNetNsIfaceMockRecorder) Get() *gomock.Call {
 }
 
 // Initialize mocks base method
-func (m *MockNetNsIface) Initialize() (*sandbox.NetNs, error) {
+func (m *MockNetNsIface) Initialize() (sandbox.NetNsIface, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "Initialize")
-	ret0, _ := ret[0].(*sandbox.NetNs)
+	ret0, _ := ret[0].(sandbox.NetNsIface)
 	ret1, _ := ret[1].(error)
 	return ret0, ret1
 }