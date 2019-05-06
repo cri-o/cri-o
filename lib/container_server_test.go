3616943cdf8e7543f79e83aaf19e94289d0c9372
Urvashi Mohnani <umohnani@redhat.com>
2019-05-06 06:28:51 -0400

    Update test suites
    Signed-off-by: Urvashi Mohnani <umohnani@redhat.com>

--- lib/container_server_test.go
+++ lib/container_server_test.go
@@ -338,6 +338,7 @@ var _ = t.Describe("ContainerServer", func() {
 		It("should succeed with removed container", func() {
 			// Given
 			mockDirs(testManifest)
+			createDummyState()
 			gomock.InOrder(
 				storeMock.EXPECT().Containers().
 					Return([]cstorage.Container{{}}, nil),
@@ -357,6 +358,7 @@ var _ = t.Describe("ContainerServer", func() {
 		It("should succeed with already added sandbox", func() {
 			// Given
 			sut.AddSandbox(mySandbox)
+			createDummyState()
 			mockDirs(testManifest)
 			gomock.InOrder(
 				storeMock.EXPECT().Containers().
@@ -374,6 +376,7 @@ var _ = t.Describe("ContainerServer", func() {
 
 		It("should fail when storage fails", func() {
 			// Given
+			createDummyState()
 			gomock.InOrder(
 				storeMock.EXPECT().Containers().Return(nil, t.TestError),
 			)
@@ -389,6 +392,7 @@ var _ = t.Describe("ContainerServer", func() {
 	t.Describe("LoadSandbox", func() {
 		It("should succeed", func() {
 			// Given
+			createDummyState()
 			mockDirs(testManifest)
 
 			// When
@@ -400,6 +404,7 @@ var _ = t.Describe("ContainerServer", func() {
 
 		It("should succeed with invalid network namespace", func() {
 			// Given
+			createDummyState()
 			manifest := bytes.Replace(testManifest,
 				[]byte(`{"type": "network", "path": "default"}`),
 				[]byte(`{"type": "", "path": ""},{"type": "network", "path": ""}`), 1,
@@ -415,6 +420,7 @@ var _ = t.Describe("ContainerServer", func() {
 
 		It("should succeed with missing network namespace", func() {
 			// Given
+			createDummyState()
 			manifest := bytes.Replace(testManifest,
 				[]byte(`{"type": "network", "path": "default"}`),
 				[]byte(`{}`), 1,
@@ -655,6 +661,7 @@ var _ = t.Describe("ContainerServer", func() {
 	t.Describe("LoadContainer", func() {
 		It("should succeed", func() {
 			// Given
+			createDummyState()
 			sut.AddSandbox(mySandbox)
 			mockDirs(testManifest)
 
