7fd9dad0532b5c68f331f7f20e398f627e583b42
Mrunal Patel <mrunalp@gmail.com>
2019-05-13 14:50:24 -0700

    Add CLI flag for --conmon-cgroup
    Signed-off-by: Mrunal Patel <mrunalp@gmail.com>

--- cmd/crio/main.go
+++ cmd/crio/main.go
@@ -143,6 +143,9 @@ func mergeConfig(config *server.Config, ctx *cli.Context) (string, error) {
 	if ctx.GlobalIsSet("cgroup-manager") {
 		config.CgroupManager = ctx.GlobalString("cgroup-manager")
 	}
+	if ctx.GlobalIsSet("conmon-cgroup") {
+		config.ConmonCgroup = ctx.GlobalString("conmon-cgroup")
+	}
 	if ctx.GlobalIsSet("hooks-dir") {
 		config.HooksDir = ctx.GlobalStringSlice("hooks-dir")
 	}
@@ -288,6 +291,10 @@ func main() {
 		cli.StringFlag{
 			Name:  "conmon",
 			Usage: fmt.Sprintf("path to the conmon executable (default: %q)", defConf.Conmon),
+		},
+		cli.StringFlag{
+			Name:  "conmon-cgroup",
+			Usage: fmt.Sprintf("cgroup used for conmon process (default: %q)", defConf.ConmonCgroup),
 		},
 		cli.StringFlag{
 			Name:  "listen",
