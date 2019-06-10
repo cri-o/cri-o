a2348719f7a3a76b8e73cb922dfdc7486c1cc971
Peter Hunt <pehunt@redhat.com>
2019-06-10 13:45:11 -0400

    Disable file locking by default
    Signed-off-by: Peter Hunt <pehunt@redhat.com>

--- lib/config.go
+++ lib/config.go
@@ -368,7 +368,7 @@ func DefaultConfig() *Config {
 			Storage:         storage.DefaultStoreOptions.GraphDriverName,
 			StorageOptions:  storage.DefaultStoreOptions.GraphDriverOptions,
 			LogDir:          "/var/log/crio/pods",
-			FileLocking:     true,
+			FileLocking:     false,
 			FileLockingPath: lockPath,
 		},
 		RuntimeConfig: RuntimeConfig{
