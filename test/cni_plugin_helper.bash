3725006039e1d933e41cfecebc5017a35e37f2f6
Michael Cambria <mcambria@redhat.com>
2019-09-12 11:58:55 -0400

    Update vendor code for cni and ocicni and libpod, fix build errors
    Signed-off-by: Michael Cambria <mcambria@redhat.com>

--- test/cni_plugin_helper.bash
+++ test/cni_plugin_helper.bash
@@ -3,8 +3,12 @@
 # This script wraps the CNI 'bridge' plugin to provide additional testing
 # capabilities
 
-if [[ -z "${CNI_ARGS}" ]]; then
-	exit 1
+# The VERSION command doesn't receive the same arguments as other CNI plugin commands.
+# It has to be handled separately
+if [[ "${CNI_COMMAND-}" = "VERSION" ]]; then
+    result=$(/opt/cni/bin/bridge $@) || exit $?
+    echo $result
+    exit 0
 fi
 
 IFS=';' read -ra array <<< "${CNI_ARGS}"
@@ -19,12 +23,16 @@ done
 
 if [[ -z "${CNI_CONTAINERID}" ]]; then
 	exit 1
-elif [[ -z "${K8S_POD_NAMESPACE}" ]]; then
-	exit 1
-elif [[ -z "${K8S_POD_NAME}" ]]; then
-	exit 1
 fi
-
+K8S_POD_NAMESPACE=${K8S_POD_NAMESPACE:-}
+K8S_POD_NAME=${K8S_POD_NAME:-}
+if [[ "${CNI_COMMAND}" != "VERSION" ]]; then
+ if [[ -z "${K8S_POD_NAMESPACE}" ]]; then
+	exit 1
+ elif [[ -z "${K8S_POD_NAME}" ]]; then
+	exit 1
+ fi
+fi
 echo "FOUND_CNI_CONTAINERID=${CNI_CONTAINERID}" >> /tmp/plugin_test_args.out
 echo "FOUND_K8S_POD_NAMESPACE=${K8S_POD_NAMESPACE}" >> /tmp/plugin_test_args.out
 echo "FOUND_K8S_POD_NAME=${K8S_POD_NAME}" >> /tmp/plugin_test_args.out
