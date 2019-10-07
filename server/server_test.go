a04c91657341655d5ed8c7a3faf1f7c8514e9b9e
Urvashi Mohnani <umohnani@redhat.com>
2019-10-06 22:12:15 -0400

    Move c/image import paths to c/image/v4
    Required by go modules to be able to import
    latest versions of containers/image
    
    Signed-off-by: Urvashi Mohnani <umohnani@redhat.com>

--- server/server_test.go
+++ server/server_test.go
@@ -5,7 +5,7 @@ import (
 	"io/ioutil"
 	"os"
 
-	"github.com/containers/image/types"
+	"github.com/containers/image/v4/types"
 	cstorage "github.com/containers/storage"
 	"github.com/cri-o/cri-o/internal/pkg/signals"
 	"github.com/cri-o/cri-o/server"
