fdcf922d85b38959b97988cc53b292acb2b32f61
Sascha Grunert <sgrunert@suse.com>
2019-11-01 13:00:35 +0100

    Update dependencies
    - Bump github.com/containers/conmon from 2.0.0+incompatible to
      2.0.2+incompatible
    - Bump github.com/prometheus/client_golang from 1.1.0 to 1.2.1
    - Bump github.com/onsi/ginkgo from 1.10.2 to 1.10.3
    - Bump github.com/onsi/gomega from 1.7.0 to 1.7.1
    
    Signed-off-by: Sascha Grunert <sgrunert@suse.com>

--- vendor/github.com/containers/conmon/runner/config/config.go
+++ vendor/github.com/containers/conmon/runner/config/config.go
@@ -3,4 +3,7 @@ package config
 const (
 	// BufSize is the size of buffers passed in to sockets
 	BufSize = 8192
+	// ConnSockBufSize is the size of the socket used for
+	// to attach to the container
+	ConnSockBufSize = 32768
 )
