309a5bf3d701b64d81636b8e45027b0f179f954b
Sascha Grunert <sgrunert@suse.com>
2020-02-28 10:24:34 +0100

    Add release-branch-forward script
    Add a first golang based script for release branch automation.
    
    Signed-off-by: Sascha Grunert <sgrunert@suse.com>

---
+++ scripts/release-branch-forward/release-branch-forward.go
@@ -0,0 +1,114 @@
+// This tools automatically finds the latest CRI-O release branch and merges
+// the latest master branch into it. This happens only if there is no
+// tag present on the release branch.
+package main
+
+import (
+	"flag"
+	"strings"
+
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	"k8s.io/release/pkg/command"
+	kgit "k8s.io/release/pkg/git"
+)
+
+const (
+	remote              = "https://github.com/cri-o/cri-o"
+	git                 = "git"
+	grep                = "grep"
+	tail                = "tail"
+	releaseBranchPrefix = "release-"
+)
+
+var dryRun bool
+
+func main() {
+	flag.BoolVar(
+		&dryRun,
+		"dry-run",
+		true,
+		"do not really push, just do a dry run",
+	)
+	flag.Parse()
+
+	logrus.SetFormatter(&logrus.TextFormatter{DisableTimestamp: true})
+	if err := run(); err != nil {
+		logrus.Fatal(err)
+	}
+}
+
+func run() error {
+	if !command.Available(git, grep, tail) {
+		return errors.Errorf(
+			"please ensure that %s are available in $PATH",
+			strings.Join([]string{git, grep, tail}, ", "),
+		)
+	}
+
+	// Get the latest release branch
+	lsRemoteHeads, err := command.
+		New(git, "ls-remote", "--sort=v:refname", "--heads", remote).
+		Pipe(grep, "-Eo", releaseBranchPrefix+".*").
+		Pipe(tail, "-1").
+		RunSilentSuccessOutput()
+	if err != nil {
+		return errors.Wrap(err, "unable to retrieve latest release branch")
+	}
+	latestReleaseBranch := lsRemoteHeads.OutputTrimNL()
+	logrus.Infof("Latest release branch: %s", latestReleaseBranch)
+
+	// Check if a release has been done on that branch
+	tagPrefix := strings.TrimPrefix(latestReleaseBranch, releaseBranchPrefix)
+	lsRemoteTags, err := command.
+		New(git, "ls-remote", "--sort=v:refname", "--tags", remote).
+		Pipe(grep, "-Eo", "v"+tagPrefix+".*").
+		RunSilentSuccessOutput()
+	if err == nil {
+		logrus.Warnf(
+			"Found existing tag(s) on release branch: %v",
+			strings.Join(strings.Fields(lsRemoteTags.OutputTrimNL()), ", "),
+		)
+		logrus.Infof("We’re all set, doing nothing")
+		return nil
+	}
+
+	// Checkout the release branch
+	repo, err := kgit.OpenRepo(".")
+	if err != nil {
+		return errors.Wrap(err, "unable to open this repository")
+	}
+	if dryRun {
+		logrus.Info("Setting repository to only do a dry-run")
+		repo.SetDry()
+	}
+	currentBranch, err := repo.CurrentBranch()
+	if err != nil {
+		return errors.Wrap(err, "unable to get current branch")
+	}
+	logrus.Infof("Checking out branch: %s", latestReleaseBranch)
+	if err := repo.Checkout(latestReleaseBranch); err != nil {
+		return errors.Wrapf(err,
+			"unable to checkout release branch %s", latestReleaseBranch,
+		)
+	}
+	defer func() {
+		logrus.Infof("Checking out branch: %s", currentBranch)
+		err = repo.Checkout(currentBranch)
+	}()
+
+	// Merge the latest master
+	mergeTarget := kgit.Remotify(kgit.Master)
+	if err := repo.Merge(mergeTarget); err != nil {
+		return errors.Wrapf(err,
+			"unable to merge %s into release branch", mergeTarget,
+		)
+	}
+
+	// Push the changes
+	if err := repo.Push(latestReleaseBranch); err != nil {
+		return errors.Wrap(err, "unable to push to remote branch")
+	}
+
+	return nil
+}
