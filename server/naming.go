b039ef6521d550d094302526c51d17c9c55ec31b
Sascha Grunert <sgrunert@suse.com>
2020-03-11 11:44:58 +0100

    Add SetNameAndID to Container interface
    The main name generation of a container is now done in the new Container
    interface. Necessary getters and tests have been added as well.
    
    Signed-off-by: Sascha Grunert <sgrunert@suse.com>

--- server/naming.go
+++ server/naming.go
@@ -35,17 +35,6 @@ func makeSandboxContainerName(sandboxConfig *pb.PodSandboxConfig) string {
 	}, nameDelimiter)
 }
 
-func makeContainerName(sandboxMetadata *pb.PodSandboxMetadata, containerConfig *pb.ContainerConfig) string {
-	return strings.Join([]string{
-		kubePrefix,
-		containerConfig.Metadata.Name,
-		sandboxMetadata.Name,
-		sandboxMetadata.Namespace,
-		sandboxMetadata.Uid,
-		fmt.Sprintf("%d", containerConfig.Metadata.Attempt),
-	}, nameDelimiter)
-}
-
 func (s *Server) ReservePodIDAndName(config *pb.PodSandboxConfig) (id, name string, err error) {
 	if config == nil || config.Metadata == nil || config.Metadata.Namespace == "" {
 		return "", "", fmt.Errorf("cannot generate pod name without namespace")
@@ -71,17 +60,4 @@ func (s *Server) ReserveSandboxContainerIDAndName(config *pb.PodSandboxConfig) (
 		return "", err
 	}
 	return name, err
-}
-
-func (s *Server) ReserveContainerIDandName(sandboxMetadata *pb.PodSandboxMetadata, config *pb.ContainerConfig) (id, name string, err error) {
-	if config == nil || config.Metadata == nil || sandboxMetadata == nil {
-		return "", "", fmt.Errorf("cannot generate container name without metadata")
-	}
-
-	id = stringid.GenerateNonCryptoID()
-	name, err = s.ReserveContainerName(id, makeContainerName(sandboxMetadata, config))
-	if err != nil {
-		return "", "", err
-	}
-	return id, name, err
 }
