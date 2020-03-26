857bcd34c6a97c6f032ab56124db6caadcc96aec
Peter Hunt <pehunt@redhat.com>
2020-03-26 12:08:02 -0400

    stats: skipped stopped containers on container list stats
    we have run into situations where cri-o reports a cgroup is deleted on list container stats calls, and it takes a while for cri-o to actually remove the container from the state. Stopped containers shouldn't matter with reporting stats, so we can skip them.
    
    This helps to get rid of lots of warnings like this:
    Unable to get stats for container XXX: unable to load cgroup at /kubepods.slice/kubepods-besteffort.slice/kubepods-besteffort-podXXX.slice: cgroup deleted
    
    Signed-off-by: Peter Hunt <pehunt@redhat.com>

--- server/container_stats_list.go
+++ server/container_stats_list.go
@@ -2,13 +2,18 @@ package server
 
 import (
 	"github.com/cri-o/cri-o/internal/log"
+	"github.com/cri-o/cri-o/internal/oci"
 	"golang.org/x/net/context"
 	pb "k8s.io/cri-api/pkg/apis/runtime/v1alpha2"
 )
 
 // ListContainerStats returns stats of all running containers.
 func (s *Server) ListContainerStats(ctx context.Context, req *pb.ListContainerStatsRequest) (*pb.ListContainerStatsResponse, error) {
-	ctrList, err := s.ContainerServer.ListContainers()
+	ctrList, err := s.ContainerServer.ListContainers(
+		func(container *oci.Container) bool {
+			return container.StateNoLock().Status != oci.ContainerStateStopped
+		},
+	)
 	if err != nil {
 		return nil, err
 	}
