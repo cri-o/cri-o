d685a99e78ac976a871dda88bb99f5c32f5d4361
Sascha Grunert <sgrunert@suse.com>
2020-03-30 14:07:43 +0200

    Use crictl for crio wipe
    We encountered multiple issues with crio wipe, for example that the
    containers storage got not properly unmounted or processes are still
    alive. To avoid such pitfalls, we can now utilize crictl to remove all
    pods (including containers) and images via two simple commands. This now
    requires to have a working crio instance running before executing `crio
    wipe`.
    
    Signed-off-by: Sascha Grunert <sgrunert@suse.com>

--- cmd/crio/wipe.go
+++ cmd/crio/wipe.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"os"
 
-	cstorage "github.com/containers/storage"
 	"github.com/cri-o/cri-o/internal/criocli"
-	"github.com/cri-o/cri-o/internal/storage"
 	"github.com/cri-o/cri-o/internal/version"
+	"github.com/cri-o/cri-o/pkg/config"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli/v2"
+	"k8s.io/release/pkg/command"
 )
+
+const crictl = "crictl"
 
 var wipeCommand = &cli.Command{
 	Name:   "wipe",
@@ -28,7 +28,7 @@ var wipeCommand = &cli.Command{
 }
 
 func crioWipe(c *cli.Context) error {
-	config, err := criocli.GetConfigFromContext(c)
+	cfg, err := criocli.GetConfigFromContext(c)
 	if err != nil {
 		return err
 	}
@@ -36,94 +36,35 @@ func crioWipe(c *cli.Context) error {
 	shouldWipe := true
 	// First, check if we need to upgrade at all
 	if !c.IsSet("force") {
-		shouldWipe, err = version.ShouldCrioWipe(config.VersionFile)
+		shouldWipe, err = version.ShouldCrioWipe(cfg.VersionFile)
 		if err != nil {
-			fmt.Fprint(os.Stderr, err.Error())
+			logrus.Warnf("%v", err)
 		}
 	}
 
 	// if we should not wipe, exit with no error
 	if !shouldWipe {
-		fmt.Println("major and minor version unchanged; no wipe needed")
+		logrus.Info("Major and minor version unchanged; no wipe needed")
 		return nil
 	}
 
-	store, err := config.GetStore()
-	if err != nil {
-		return err
+	if !command.Available(crictl) {
+		return errors.Errorf("%s not found in $PATH", crictl)
 	}
 
-	cstore := ContainerStore{store}
-	if err := cstore.wipeCrio(); err != nil {
-		return err
-	}
-
-	return nil
+	return removeContainersAndImages(cfg)
 }
 
-type ContainerStore struct {
-	store cstorage.Store
-}
+func removeContainersAndImages(c *config.Config) error {
+	logrus.Info("Removing all containers and pods")
+	e := fmt.Sprintf("--runtime-endpoint=unix://%s", c.Listen)
+	if err := command.New(crictl, e, "rmp", "-fa").RunSuccess(); err != nil {
+		return errors.Wrap(err, "removing all containers and pods")
+	}
 
-func (c ContainerStore) wipeCrio() error {
-	crioContainers, crioImages, err := c.getCrioContainersAndImages()
-	if err != nil {
-		return err
-	}
-	for _, id := range crioContainers {
-		c.deleteContainer(id)
-	}
-	for _, id := range crioImages {
-		c.deleteImage(id)
+	logrus.Info("Removing all images")
+	if err := command.New(crictl, e, "rmi", "-a").RunSuccess(); err != nil {
+		return errors.Wrap(err, "removing all images")
 	}
 	return nil
-}
-
-func (c ContainerStore) getCrioContainersAndImages() (crioContainers, crioImages []string, err error) {
-	containers, err := c.store.Containers()
-	if err != nil {
-		if os.IsNotExist(errors.Cause(err)) {
-			return crioContainers, crioImages, err
-		}
-		logrus.Warnf("could not read containers and sandboxes: %v", err)
-	}
-
-	for i := range containers {
-		id := containers[i].ID
-		metadataString, err := c.store.Metadata(id)
-		if err != nil {
-			continue
-		}
-
-		metadata := storage.RuntimeContainerMetadata{}
-		if err := json.Unmarshal([]byte(metadataString), &metadata); err != nil {
-			continue
-		}
-		if !storage.IsCrioContainer(&metadata) {
-			continue
-		}
-		crioContainers = append(crioContainers, id)
-		crioImages = append(crioImages, containers[i].ImageID)
-	}
-	return crioContainers, crioImages, nil
-}
-
-func (c ContainerStore) deleteContainer(id string) {
-	if mounted, err := c.store.Unmount(id, true); err != nil || mounted {
-		logrus.Warnf("unable to unmount container %s: %v", id, err)
-		return
-	}
-	if err := c.store.DeleteContainer(id); err != nil {
-		logrus.Warnf("unable to delete container %s: %v", id, err)
-		return
-	}
-	logrus.Infof("deleted container %s", id)
-}
-
-func (c ContainerStore) deleteImage(id string) {
-	if _, err := c.store.DeleteImage(id, true); err != nil {
-		logrus.Warnf("unable to delete image %s: %v", id, err)
-		return
-	}
-	logrus.Infof("deleted image %s", id)
 }
