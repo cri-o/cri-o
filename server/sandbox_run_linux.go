859a65099fde0a802edf88d48e8dd6a13fc92ba7
Urvashi Mohnani <umohnani@redhat.com>
2020-05-06 11:13:25 -0400

    Use the container_kvm_t label when using kata as the runtime
    Add a check to detect when cri-o is using kata as the runtime
    and set the process label to container_kvm_t.
    
    Signed-off-by: Urvashi Mohnani <umohnani@redhat.com>

--- server/sandbox_run_linux.go
+++ server/sandbox_run_linux.go
@@ -472,7 +472,7 @@ func (s *Server) runPodSandbox(ctx context.Context, req *pb.RunPodSandboxRequest
 	g.AddAnnotation(annotations.HostnamePath, hostnamePath)
 	sb.AddHostnamePath(hostnamePath)
 
-	container, err := oci.NewContainer(sbox.ID(), containerName, podContainer.RunDir, logPath, labels, g.Config.Annotations, kubeAnnotations, s.config.PauseImage, "", "", nil, sbox.ID(), false, false, false, sb.RuntimeHandler(), podContainer.Dir, created, podContainer.Config.Config.StopSignal)
+	container, err := oci.NewContainer(sbox.ID(), containerName, podContainer.RunDir, logPath, labels, g.Config.Annotations, kubeAnnotations, s.config.PauseImage, "", "", nil, sbox.ID(), false, false, false, runtimeHandler, podContainer.Dir, created, podContainer.Config.Config.StopSignal)
 	if err != nil {
 		return nil, err
 	}
