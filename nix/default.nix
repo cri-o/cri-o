bd9c0b1f03e1695187f58d1f2b1f5d5fb259133d
Wong Hoi Sing Edison <hswong3i@gmail.com>
2020-05-28 08:59:37 +0800

    Cleanup nix derivation for static builds
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@gmail.com>

--- nix/default.nix
+++ nix/default.nix
@@ -1,59 +1,44 @@
 { system ? builtins.currentSystem }:
 let
-  pkgs = import ./nixpkgs.nix {
+  pkgs = (import ./nixpkgs.nix {
     config = {
       packageOverrides = pkg: {
-        go_1_12 = pkg.go_1_14;
+        gpgme = (static pkg.gpgme);
+        libassuan = (static pkg.libassuan);
+        libgpgerror = (static pkg.libgpgerror);
+        libseccomp = (static pkg.libseccomp);
       };
     };
-  };
+  });
 
-  static = pkg: pkg.overrideAttrs(old: {
-    configureFlags = (old.configureFlags or []) ++
+  static = pkg: pkg.overrideAttrs(x: {
+    configureFlags = (x.configureFlags or []) ++
       [ "--without-shared" "--disable-shared" ];
     dontDisableStatic = true;
     enableSharedExecutables = false;
     enableStatic = true;
   });
 
-  patchLvm2 = pkg: pkg.overrideAttrs(old: {
-    configureFlags = [
-      "--disable-cmdlib" "--disable-readline" "--disable-udev_rules"
-      "--disable-udev_sync" "--enable-pkgconfig" "--enable-static_link"
-    ];
-    preConfigure = old.preConfigure + ''
-      substituteInPlace libdm/Makefile.in --replace \
-        SUBDIRS=dm-tools SUBDIRS=
-      substituteInPlace tools/Makefile.in --replace \
-        "TARGETS += lvm.static" ""
-      substituteInPlace tools/Makefile.in --replace \
-        "INSTALL_LVM_TARGETS += install_tools_static" ""
-    '';
-    postInstall = "";
-  });
-
-  self = {
-    cri-o-static = (pkgs.cri-o.overrideAttrs(old: {
+  self = with pkgs; {
+    cri-o-static = (cri-o.overrideAttrs(x: {
       name = "cri-o-static";
-      buildInputs = old.buildInputs ++ (with pkgs; [ systemd ]);
       src = ./..;
-      buildPhase = ''
-        pushd go/src/github.com/cri-o/cri-o
-        make BUILDTAGS="apparmor seccomp selinux containers_image_ostree_stub netgo" \
-          bin/crio \
-          bin/crio-status \
-          bin/pinns
+      doCheck = false;
+      buildInputs = [
+        glibc
+        glibc.static
+        gpgme
+        libapparmor
+        libassuan
+        libgpgerror
+        libseccomp
+        libselinux
+      ];
+      prePatch = ''
+        export LDFLAGS='-static-libgcc -static'
+        export EXTRA_LDFLAGS='-linkmode external -extldflags "-static -lm"'
+        export BUILDTAGS='static netgo apparmor selinux seccomp exclude_graphdriver_btrfs exclude_graphdriver_devicemapper'
       '';
-      EXTRA_LDFLAGS = ''-linkmode external -extldflags "-static -lm"'';
-      dontStrip = true;
-      # DEBUG = 1; # Uncomment this line to enable debug symbols in the binary
-    })).override {
-      flavor = "-static";
-      gpgme = (static pkgs.gpgme);
-      libassuan = (static pkgs.libassuan);
-      libgpgerror = (static pkgs.libgpgerror);
-      libseccomp = (static pkgs.libseccomp);
-      lvm2 = (patchLvm2 (static pkgs.lvm2));
-    };
+    }));
   };
 in self
