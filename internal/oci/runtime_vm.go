3a6f150c29deb72a9982eea23463f4bec107fa21
Fabiano Fidêncio <fidencio@redhat.com>
2020-06-26 10:43:07 -0400

    runtime_vm: use errors.Is() instead of errors.Cause()
    This is a partial backport of 371a60093655b1fe, which ended up
    introducing a regression on runtime_vm, as the logic in that specific
    check ended up inverted.
    
    The main reason for partially backporting 371a60093655b1fe is to ensure
    another series will cleanly apply atop of this one.
    
    Signed-off-by: Fabiano Fidêncio <fidencio@redhat.com>

--- internal/oci/runtime_vm.go
+++ internal/oci/runtime_vm.go
@@ -626,7 +626,7 @@ func (r *runtimeVM) updateContainerStatus(c *Container) error {
 		ID: c.ID(),
 	})
 	if err != nil {
-		if errors.Cause(err) != ttrpc.ErrClosed {
+		if errors.Is(err, ttrpc.ErrClosed) {
 			return errdefs.FromGRPC(err)
 		}
 		return errdefs.ErrNotFound
