cri-o/cri-o

commit 6025578f117b2905f91d75dded6d2aac346c2af2

Update golangci lint and apply fixes
The new recommended way is to use the golangci install script,
which we now use. All new linter issues have been fixed and
additionally added linters are commented out per default.

Signed-off-by: Sascha Grunert <sgrunert@suse.com>

cmd/crio/main.go
@@ -5,7 +5,6 @@ import (
 	goflag "flag"
 	"fmt"
 	"net/http"
-	_ "net/http/pprof"
 	"os"
 	"os/signal"
 	"path/filepath"

vendor/github.com/golangci/dupl/main.go
@@ -1,148 +1,0 @@
-package dupl
-
-import (
-	"flag"
-	"fmt"
-	"io/ioutil"
-	"os"
-	"path/filepath"
-	"sort"
-
-	"github.com/golangci/dupl/job"
-	"github.com/golangci/dupl/printer"
-	"github.com/golangci/dupl/syntax"
-)
-
-const defaultThreshold = 15
-
-var (
-	paths   = []string{"."}
-	vendor  = flag.Bool("dupl.vendor", false, "")
-	verbose = flag.Bool("dupl.verbose", false, "")
-	files   = flag.Bool("dupl.files", false, "")
-
-	html     = flag.Bool("dupl.html", false, "")
-	plumbing = flag.Bool("dupl.plumbing", false, "")
-)
-
-const (
-	vendorDirPrefix = "vendor" + string(filepath.Separator)
-	vendorDirInPath = string(filepath.Separator) + vendorDirPrefix
-)
-
-func init() {
-	flag.BoolVar(verbose, "dupl.v", false, "alias for -verbose")
-}
-
-func Run(files []string, threshold int) ([]printer.Issue, error) {
-	fchan := make(chan string, 1024)
-	go func() {
-		for _, f := range files {
-			fchan <- f
-		}
-		close(fchan)
-	}()
-	schan := job.Parse(fchan)
-	t, data, done := job.BuildTree(schan)
-	<-done
-
-	// finish stream
-	t.Update(&syntax.Node{Type: -1})
-
-	mchan := t.FindDuplOver(threshold)
-	duplChan := make(chan syntax.Match)
-	go func() {
-		for m := range mchan {
-			match := syntax.FindSyntaxUnits(*data, m, threshold)
-			if len(match.Frags) > 0 {
-				duplChan <- match
-			}
-		}
-		close(duplChan)
-	}()
-
-	return makeIssues(duplChan)
-}
-
-func makeIssues(duplChan <-chan syntax.Match) ([]printer.Issue, error) {
-	groups := make(map[string][][]*syntax.Node)
-	for dupl := range duplChan {
-		groups[dupl.Hash] = append(groups[dupl.Hash], dupl.Frags...)
-	}
-	keys := make([]string, 0, len(groups))
-	for k := range groups {
-		keys = append(keys, k)
-	}
-	sort.Strings(keys)
-
-	p := printer.NewPlumbing(ioutil.ReadFile)
-
-	var issues []printer.Issue
-	for _, k := range keys {
-		uniq := unique(groups[k])
-		if len(uniq) > 1 {
-			i, err := p.MakeIssues(uniq)
-			if err != nil {
-				return nil, err
-			}
-			issues = append(issues, i...)
-		}
-	}
-
-	return issues, nil
-}
-
-func unique(group [][]*syntax.Node) [][]*syntax.Node {
-	fileMap := make(map[string]map[int]struct{})
-
-	var newGroup [][]*syntax.Node
-	for _, seq := range group {
-		node := seq[0]
-		file, ok := fileMap[node.Filename]
-		if !ok {
-			file = make(map[int]struct{})
-			fileMap[node.Filename] = file
-		}
-		if _, ok := file[node.Pos]; !ok {
-			file[node.Pos] = struct{}{}
-			newGroup = append(newGroup, seq)
-		}
-	}
-	return newGroup
-}
-
-func usage() {
-	fmt.Fprintln(os.Stderr, `Usage: dupl [flags] [paths]
-
-Paths:
-  If the given path is a file, dupl will use it regardless of
-  the file extension. If it is a directory, it will recursively
-  search for *.go files in that directory.
-
-  If no path is given, dupl will recursively search for *.go
-  files in the current directory.
-
-Flags:
-  -files
-    	read file names from stdin one at each line
-  -html
-    	output the results as HTML, including duplicate code fragments
-  -plumbing
-    	plumbing (easy-to-parse) output for consumption by scripts or tools
-  -t, -threshold size
-    	minimum token sequence size as a clone (default 15)
-  -vendor
-    	check files in vendor directory
-  -v, -verbose
-    	explain what is being done
-
-Examples:
-  dupl -t 100
-    	Search clones in the current directory of size at least
-    	100 tokens.
-  dupl $(find app/ -name '*_test.go')
-    	Search for clones in tests in the app directory.
-  find app/ -name '*_test.go' |dupl -files
-    	The same as above.`)
-	os.Exit(2)
-}

vendor/github.com/golangci/golangci-lint/cmd/golangci-lint/main.go
@@ -1,24 +1,0 @@
-package main
-
-import (
-	"fmt"
-	"os"
-
-	"github.com/golangci/golangci-lint/pkg/commands"
-)
-
-var (
-	// Populated by goreleaser during build
-	version = "master"
-	commit  = "?"
-	date    = ""
-)
-
-func main() {
-	e := commands.NewExecutor(version, commit, date)
-
-	if err := e.Execute(); err != nil {
-		fmt.Fprintf(os.Stderr, "failed executing command with error %v\n", err)
-		os.Exit(1)
-	}
-}

vendor/github.com/ultraware/funlen/main.go
@@ -1,98 +1,0 @@
-package funlen
-
-import (
-	"fmt"
-	"go/ast"
-	"go/token"
-	"reflect"
-)
-
-const defaultLineLimit = 60
-const defaultStmtLimit = 40
-
-// Run runs this linter on the provided code
-func Run(file *ast.File, fset *token.FileSet, lineLimit, stmtLimit int) []Message {
-	if lineLimit == 0 {
-		lineLimit = defaultLineLimit
-	}
-	if stmtLimit == 0 {
-		stmtLimit = defaultStmtLimit
-	}
-
-	var msgs []Message
-	for _, f := range file.Decls {
-		decl, ok := f.(*ast.FuncDecl)
-		if !ok {
-			continue
-		}
-
-		if stmts := parseStmts(decl.Body.List); stmts > stmtLimit {
-			msgs = append(msgs, makeStmtMessage(fset, decl.Name, stmts, stmtLimit))
-			continue
-		}
-
-		if lines := getLines(fset, decl); lines > lineLimit {
-			msgs = append(msgs, makeLineMessage(fset, decl.Name, lines, lineLimit))
-		}
-	}
-
-	return msgs
-}
-
-// Message contains a message
-type Message struct {
-	Pos     token.Position
-	Message string
-}
-
-func makeLineMessage(fset *token.FileSet, funcInfo *ast.Ident, lines, lineLimit int) Message {
-	return Message{
-		fset.Position(funcInfo.Pos()),
-		fmt.Sprintf("Function '%s' is too long (%d > %d)\n", funcInfo.Name, lines, lineLimit),
-	}
-}
-
-func makeStmtMessage(fset *token.FileSet, funcInfo *ast.Ident, stmts, stmtLimit int) Message {
-	return Message{
-		fset.Position(funcInfo.Pos()),
-		fmt.Sprintf("Function '%s' has too many statements (%d > %d)\n", funcInfo.Name, stmts, stmtLimit),
-	}
-}
-
-func getLines(fset *token.FileSet, f *ast.FuncDecl) int { // nolint: interfacer
-	return fset.Position(f.End()).Line - fset.Position(f.Pos()).Line - 1
-}
-
-func parseStmts(s []ast.Stmt) (total int) {
-	for _, v := range s {
-		total++
-		switch stmt := v.(type) {
-		case *ast.BlockStmt:
-			total += parseStmts(stmt.List) - 1
-		case *ast.ForStmt, *ast.RangeStmt, *ast.IfStmt,
-			*ast.SwitchStmt, *ast.TypeSwitchStmt, *ast.SelectStmt:
-			total += parseBodyListStmts(stmt)
-		case *ast.CaseClause:
-			total += parseStmts(stmt.Body)
-		case *ast.AssignStmt:
-			total += checkInlineFunc(stmt.Rhs[0])
-		case *ast.GoStmt:
-			total += checkInlineFunc(stmt.Call.Fun)
-		case *ast.DeferStmt:
-			total += checkInlineFunc(stmt.Call.Fun)
-		}
-	}
-	return
-}
-
-func checkInlineFunc(stmt ast.Expr) int {
-	if block, ok := stmt.(*ast.FuncLit); ok {
-		return parseStmts(block.Body.List)
-	}
-	return 0
-}
-
-func parseBodyListStmts(t interface{}) int {
-	i := reflect.ValueOf(t).Elem().FieldByName(`Body`).Elem().FieldByName(`List`).Interface()
-	return parseStmts(i.([]ast.Stmt))
-}

commit e6e3c4ad0c543f739d78b6cb0015330701f7d3eb

Bring back pprof
Commit 6025578f11 (Oct 23 2019, "Update golangci lint and apply fixes")
inadvertently removed the pprof import, as a result, pprof can't be used
even when enabled during runtime (via --profile flag), giving 404 on
every URL.

Bring it back.

Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

cmd/crio/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	_ "net/http/pprof"
 	"os"
 	"os/signal"
 	"path/filepath"