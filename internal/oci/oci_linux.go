cri-o/cri-o

commit 512fdb2f9f49ec4ab6047db132de739b9e5bf5bb

Take total_inactive_file into consideration for memory usage
We should take the `total_inactive_file` into consideration
when providing the `WorkingSetBytes` to align with other
container runtimes.

Signed-off-by: Sascha Grunert <sgrunert@suse.com>

internal/oci/oci_linux.go
@@ -3,9 +3,11 @@
 package oci
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 	"syscall"
 	"time"
@@ -110,6 +112,18 @@ func (r *runtimeOCI) containerStats(ctr *Container, cgroup string) (*ContainerSt
 		stats.NetInput, stats.NetOutput = getContainerNetIO(netNsPath)
 	}
 
+	totalInactiveFile, err := getTotalInactiveFile()
+	if err != nil { // nolint: gocritic
+		logrus.Warnf("error in memory working set stats retrieval: %v", err)
+	} else if stats.MemUsage > totalInactiveFile {
+		stats.WorkingSetBytes = stats.MemUsage - totalInactiveFile
+	} else {
+		logrus.Debugf(
+			"unable to account working set stats: total_inactive_file (%d) > memory usage (%d)",
+			totalInactiveFile, stats.MemUsage,
+		)
+	}
+
 	return stats, nil
 }
 
@@ -161,4 +175,41 @@ func metricsToCtrStats(c *Container, m *cgroups.Metrics) *ContainerStats {
 		BlockOutput: blockOutput,
 		PIDs:        pids,
 	}
+}
+
+// getTotalInactiveFile returns the value if `total_inactive_file` as integer
+// from `/sys/fs/cgroup/memory/memory.stat`. It returns an error if the file is
+// not parsable.
+func getTotalInactiveFile() (uint64, error) {
+	// no cgroupv2 support right now
+	if isV2, err := cgroups.IsCgroup2UnifiedMode(); err == nil || isV2 {
+		return 0, nil
+	}
+
+	const memoryStat = "/sys/fs/cgroup/memory/memory.stat"
+	const totalInactiveFilePrefix = "total_inactive_file "
+	f, err := os.Open(memoryStat)
+	if err != nil {
+		return 0, err
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		if strings.Contains(scanner.Text(), totalInactiveFilePrefix) {
+			val, err := strconv.Atoi(
+				strings.TrimPrefix(scanner.Text(), totalInactiveFilePrefix),
+			)
+			if err != nil {
+				return 0, errors.Wrapf(err, "unable to parse total inactive file value")
+			}
+			return uint64(val), nil
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return 0, err
+	}
+
+	return 0, errors.Errorf("%q not found in %v", totalInactiveFilePrefix, memoryStat)
 }

internal/oci/stats.go
@@ -13,18 +13,19 @@ import (
 
 // ContainerStats contains the statistics information for a running container
 type ContainerStats struct {
-	Container   string
-	CPU         float64
-	CPUNano     uint64
-	SystemNano  int64
-	MemUsage    uint64
-	MemLimit    uint64
-	MemPerc     float64
-	NetInput    uint64
-	NetOutput   uint64
-	BlockInput  uint64
-	BlockOutput uint64
-	PIDs        uint64
+	Container       string
+	CPU             float64
+	CPUNano         uint64
+	SystemNano      int64
+	MemUsage        uint64
+	MemLimit        uint64
+	MemPerc         float64
+	NetInput        uint64
+	NetOutput       uint64
+	BlockInput      uint64
+	BlockOutput     uint64
+	PIDs            uint64
+	WorkingSetBytes uint64
 }
 
 // Returns the total number of bytes transmitted and received for the given container stats

server/container_stats.go
@@ -39,7 +39,7 @@ func (s *Server) buildContainerStats(stats *oci.ContainerStats, container *oci.C
 		},
 		Memory: &pb.MemoryUsage{
 			Timestamp:       stats.SystemNano,
-			WorkingSetBytes: &pb.UInt64Value{Value: stats.MemUsage},
+			WorkingSetBytes: &pb.UInt64Value{Value: stats.WorkingSetBytes},
 		},
 		WritableLayer: writableLayer,
 	}

commit eddf148a16915e45be34c15f03c6438aeb741bab

oci_linux: fix working set calculation
Commit 512fdb2f9f49 mistakenly used the value of total_inactive_file
from the top-level cgroup, thus the working set value was either
wrong (too low) or invalid (negative), for example:

> Unable to account working set stats: total_inactive_file (1572753409)
> memory usage (585728)" file="oci/oci_linux.go:93"

We need to use total_inactive_file and memory.usage_in_bytes from
the same cgroup, otherwise it does not make any sense.

While at it
 - promote the above message from debug to warning;
 - optimize getTotalInactiveFile() by using HasPrefix()
   rather than Contains().

Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

internal/oci/oci_linux.go
@@ -6,6 +6,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
 	"syscall"
@@ -88,13 +89,13 @@ func (r *runtimeOCI) containerStats(ctr *Container, cgroup string) (*ContainerSt
 		stats.NetInput, stats.NetOutput = getContainerNetIO(netNsPath)
 	}
 
-	totalInactiveFile, err := getTotalInactiveFile()
+	totalInactiveFile, err := getTotalInactiveFile(cgroupPath)
 	if err != nil { // nolint: gocritic
 		logrus.Warnf("error in memory working set stats retrieval: %v", err)
 	} else if stats.MemUsage > totalInactiveFile {
 		stats.WorkingSetBytes = stats.MemUsage - totalInactiveFile
 	} else {
-		logrus.Debugf(
+		logrus.Warnf(
 			"unable to account working set stats: total_inactive_file (%d) > memory usage (%d)",
 			totalInactiveFile, stats.MemUsage,
 		)
@@ -154,15 +155,15 @@ func metricsToCtrStats(c *Container, m *cgroups.Metrics) *ContainerStats {
 }
 
 // getTotalInactiveFile returns the value if `total_inactive_file` as integer
-// from `/sys/fs/cgroup/memory/memory.stat`. It returns an error if the file is
-// not parsable.
-func getTotalInactiveFile() (uint64, error) {
+// from cgroup's memory.stat. Returns an error if the file does not exists,
+// not parsable, or the value is not found.
+func getTotalInactiveFile(path string) (uint64, error) {
 	// TODO: no cgroupv2 support right now
 	if node.CgroupIsV2() {
 		return 0, nil
 	}
 
-	const memoryStat = "/sys/fs/cgroup/memory/memory.stat"
+	memoryStat := filepath.Join("/sys/fs/cgroup/memory", path, "memory.stat")
 	const totalInactiveFilePrefix = "total_inactive_file "
 	f, err := os.Open(memoryStat)
 	if err != nil {
@@ -172,7 +173,7 @@ func getTotalInactiveFile() (uint64, error) {
 
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
-		if strings.Contains(scanner.Text(), totalInactiveFilePrefix) {
+		if strings.HasPrefix(scanner.Text(), totalInactiveFilePrefix) {
 			val, err := strconv.Atoi(
 				strings.TrimPrefix(scanner.Text(), totalInactiveFilePrefix),
 			)
@@ -182,7 +183,6 @@ func getTotalInactiveFile() (uint64, error) {
 			return uint64(val), nil
 		}
 	}
-
 	if err := scanner.Err(); err != nil {
 		return 0, err
 	}