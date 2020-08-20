cri-o/cri-o

commit e8c12b348d7d6a14e99d2e1c2edbc48f1c1bf2d6

Streamline how done channel is closed in Runtime#WaitContainerStateStopped
Signed-off-by: Ted Yu <yuzhihong@gmail.com>

internal/oci/oci.go
@@ -126,11 +126,10 @@ func (r *Runtime) WaitContainerStateStopped(ctx context.Context, c *Container) (
 				// Check if the container is stopped
 				if err := impl.UpdateContainerStatus(c); err != nil {
 					done <- err
-					close(done)
 					return
 				}
 				if c.State().Status == ContainerStateStopped {
-					close(done)
+					done <- nil
 					return
 				}
 				time.Sleep(100 * time.Millisecond)
@@ -139,6 +138,7 @@ func (r *Runtime) WaitContainerStateStopped(ctx context.Context, c *Container) (
 	}()
 	select {
 	case err = <-done:
+		close(done)
 		break
 	case <-ctx.Done():
 		close(chControl)

commit 9e112eebd849f3d51442338f26817711da4aaf6c

oci: move channel close to writer goroutine
solves the crash:

goroutine 7073 [running]:
github.com/cri-o/cri-o/internal/oci.(*Runtime).WaitContainerStateStopped.func1(0xc000bda240,
0x23e36a0, 0xc000bdc030, 0xc0001b11e0, 0xc000bda1e0)
	src/github.com/cri-o/cri-o/internal/oci/oci.go:132 +0xdc
created by github.com/cri-o/cri-o/internal/oci.(*Runtime).WaitContainerStateStopped
	src/github.com/cri-o/cri-o/internal/oci/oci.go:120 +0x156

regression introduced with e8c12b348d7d6a14e99d2e1c2edbc48f1c1bf2d6

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

internal/oci/oci.go
@@ -118,6 +118,7 @@ func (r *Runtime) WaitContainerStateStopped(ctx context.Context, c *Container) e
 	done := make(chan error)
 	chControl := make(chan struct{})
 	go func() {
+		defer close(done)
 		for {
 			select {
 			case <-chControl:
@@ -138,15 +139,12 @@ func (r *Runtime) WaitContainerStateStopped(ctx context.Context, c *Container) e
 	}()
 	select {
 	case err = <-done:
-		close(done)
 		break
 	case <-ctx.Done():
 		close(chControl)
-		close(done)
 		return ctx.Err()
 	case <-time.After(time.Duration(r.config.CtrStopTimeout) * time.Second):
 		close(chControl)
-		close(done)
 		return fmt.Errorf(
 			"failed to get container stopped status: %ds timeout reached",
 			r.config.CtrStopTimeout,