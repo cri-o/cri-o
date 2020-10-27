0cfc673cd24952b5b173ef0d5046a8ab3fdda2e1
Kir Kolyshkin <kolyshkin@gmail.com>
2020-10-26 19:37:39 -0700

    test: bring back userns testing
    Commit 92caff26d ("Added EnvVar to cri-o cli flags", May 29 2019)
    changed some testing code to use CONTAINER_{U,G}ID_MAPPING
    instead of {U,G}ID_MAPPING environment variables, but a place
    in test/test_runner.sh was omitted. This, I guess, resulted
    in not testing userns (at least partially -- there are tests that enable
    it explicitly rather through a TEST_USERNS env var).
    
    Next, commit b80c31b9cf ("Allow CRI-O to manage IPC and UTS namespaces",
    Dec 16 2019) changed the option ManageNetworkNSLifecycle to
    ManageNSLifecycle, and changed its default to be true. Since this
    commit, it is now needed to disable the option to allow userns testing
    (otherwise we get "cannot use UIDMappings with ManageNSLifecycle"
    error).
    
    Finally, one can't set ManageNSLifecycle=false when DropInfraCtr=true
    (or one will get "cannot drop infra without ManageNSLifecycle" error).
    
    This commit fixes all three issues, bringing back userns containers
    testing.
    
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- test/test_runner.sh
+++ test/test_runner.sh
@@ -7,8 +7,13 @@ cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
 if [[ -n "$TEST_USERNS" ]]; then
     echo "Enabled user namespace testing"
-    export UID_MAPPINGS="0:100000:100000"
-    export GID_MAPPINGS="0:200000:100000"
+    # TODO: rm CONTAINER_MANAGE_NS_LIFECYCLE, CONTAINER_DROP_INFRA_CTR
+    # once userns works with them set to default (true).
+    export \
+        CONTAINER_UID_MAPPINGS="0:100000:100000" \
+        CONTAINER_GID_MAPPINGS="0:200000:100000" \
+        CONTAINER_MANAGE_NS_LIFECYCLE=false \
+        CONTAINER_DROP_INFRA_CTR=false
 
     # Needed for RHEL
     if [[ -w /proc/sys/user/max_user_namespaces ]]; then
