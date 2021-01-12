7a193988226db01849e00bb15189123ba74ce042
Peter Hunt <pehunt@redhat.com>
2021-01-12 12:49:16 -0500

    runtime_vm: set finished time when containers stop
    Signed-off-by: Peter Hunt <pehunt@redhat.com>

--- internal/oci/runtime_vm.go
+++ internal/oci/runtime_vm.go
@@ -534,6 +534,7 @@ func (r *runtimeVM) StopContainer(ctx context.Context, c *Container, timeout int
 
 		err := r.waitCtrTerminate(sig, stopCh, timeoutDuration)
 		if err == nil {
+			c.state.Finished = time.Now()
 			return nil
 		}
 		logrus.Warnf("%v", err)
@@ -550,6 +551,7 @@ func (r *runtimeVM) StopContainer(ctx context.Context, c *Container, timeout int
 		return err
 	}
 
+	c.state.Finished = time.Now()
 	return nil
 }
 
