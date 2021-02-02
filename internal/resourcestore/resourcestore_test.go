4286207fa99b1df9d9014d2d611a85b499f8b66b
Peter Hunt <pehunt@redhat.com>
2021-02-02 11:41:59 -0500

    ResourceStore: update tests to all run
    We need to specifically register "Describe" functions,
    but ginkgo doesn't allow us to register multiple ones.
    
    Wrap different functionality in different Contexts so they all run.
    
    Signed-off-by: Peter Hunt <pehunt@redhat.com>

--- internal/resourcestore/resourcestore_test.go
+++ internal/resourcestore/resourcestore_test.go
@@ -34,81 +34,76 @@ var _ = t.Describe("ResourceStore", func() {
 		cleanupFuncs []func()
 		e            *entry
 	)
-	BeforeEach(func() {
-		sut = resourcestore.New()
-		cleanupFuncs = make([]func(), 0)
-		e = &entry{
-			id: testID,
-		}
-	})
-	It("Put should be able to get resource after adding", func() {
-		// Given
-
-		// When
-		Expect(sut.Put(testName, e, cleanupFuncs)).To(BeNil())
-
-		// Then
-		id := sut.Get(testName)
-		Expect(id).To(Equal(e.id))
-
-		id = sut.Get(testName)
-		Expect(id).To(BeEmpty())
-	})
-	It("Put should fail to readd resource", func() {
-		// Given
-
-		// When
-		Expect(sut.Put(testName, e, cleanupFuncs)).To(BeNil())
-
-		// Then
-		Expect(sut.Put(testName, e, cleanupFuncs)).NotTo(BeNil())
-	})
-	It("Get should call SetCreated", func() {
-		// When
-		Expect(sut.Put(testName, e, cleanupFuncs)).To(BeNil())
-
-		// Then
-		id := sut.Get(testName)
-		Expect(id).To(Equal(e.id))
-		Expect(e.created).To(BeTrue())
-	})
-})
-
-var _ = t.Describe("ResourceStore and timeout", func() {
-	// Setup the test
-	var (
-		sut          *resourcestore.ResourceStore
-		cleanupFuncs []func()
-		e            *entry
-	)
-	BeforeEach(func() {
-		cleanupFuncs = make([]func(), 0)
-		e = &entry{
-			id: testID,
-		}
-	})
-	It("Put should call cleanup funcs after timeout", func() {
-		// Given
-		timeout := 2 * time.Second
-		sut = resourcestore.NewWithTimeout(timeout)
-
-		timedOutChan := make(chan bool)
-		cleanupFuncs = append(cleanupFuncs, func() {
-			timedOutChan <- true
+	Context("no timeout", func() {
+		BeforeEach(func() {
+			sut = resourcestore.New()
+			cleanupFuncs = make([]func(), 0)
+			e = &entry{
+				id: testID,
+			}
 		})
-		go func() {
-			time.Sleep(timeout * 3)
-			timedOutChan <- false
-		}()
+		It("Put should be able to get resource after adding", func() {
+			// Given
 
-		// When
-		Expect(sut.Put(testName, e, cleanupFuncs)).To(BeNil())
+			// When
+			Expect(sut.Put(testName, e, cleanupFuncs)).To(BeNil())
 
-		// Then
-		didStoreCallTimeoutFunc := <-timedOutChan
-		Expect(didStoreCallTimeoutFunc).To(Equal(true))
+			// Then
+			id := sut.Get(testName)
+			Expect(id).To(Equal(e.id))
 
-		id := sut.Get(testName)
-		Expect(id).To(BeEmpty())
+			id = sut.Get(testName)
+			Expect(id).To(BeEmpty())
+		})
+		It("Put should fail to readd resource", func() {
+			// Given
+
+			// When
+			Expect(sut.Put(testName, e, cleanupFuncs)).To(BeNil())
+
+			// Then
+			Expect(sut.Put(testName, e, cleanupFuncs)).NotTo(BeNil())
+		})
+		It("Get should call SetCreated", func() {
+			// When
+			Expect(sut.Put(testName, e, cleanupFuncs)).To(BeNil())
+
+			// Then
+			id := sut.Get(testName)
+			Expect(id).To(Equal(e.id))
+			Expect(e.created).To(BeTrue())
+		})
+	})
+	Context("with timeout", func() {
+		BeforeEach(func() {
+			cleanupFuncs = make([]func(), 0)
+			e = &entry{
+				id: testID,
+			}
+		})
+		It("Put should call cleanup funcs after timeout", func() {
+			// Given
+			timeout := 2 * time.Second
+			sut = resourcestore.NewWithTimeout(timeout)
+
+			timedOutChan := make(chan bool)
+			cleanupFuncs = append(cleanupFuncs, func() {
+				timedOutChan <- true
+			})
+			go func() {
+				time.Sleep(timeout * 3)
+				timedOutChan <- false
+			}()
+
+			// When
+			Expect(sut.Put(testName, e, cleanupFuncs)).To(BeNil())
+
+			// Then
+			didStoreCallTimeoutFunc := <-timedOutChan
+			Expect(didStoreCallTimeoutFunc).To(Equal(true))
+
+			id := sut.Get(testName)
+			Expect(id).To(BeEmpty())
+		})
 	})
 })
