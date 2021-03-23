cde7e9578e3950537ff0d85d4ba777efc2e9a2a4
Mrunal Patel <mrunalp@gmail.com>
2021-03-22 17:10:59 -0700

    Add support for workload settings
    Signed-off-by: Mrunal Patel <mrunalp@gmail.com>

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -173,8 +173,20 @@ type RuntimeHandler struct {
 	AllowedAnnotations []string `toml:"allowed_annotations,omitempty"`
 }
 
+type WorkloadSettings struct {
+	// CPUSet is the cpu set to apply for the workload
+	CPUSet string `toml:"cpu_set"`
+	// Label is the pod label that activates these workload settings
+	Label string `toml:"label"`
+	// CPUSharesAnnotation is the annotation used to specify
+	// CPU shares to apply for the workload
+	CPUSharesAnnotation string `toml:"cpu_shares_annotation"`
+}
+
 // Multiple runtime Handlers in a map
 type Runtimes map[string]*RuntimeHandler
+
+type Workloads map[string]*WorkloadSettings
 
 // RuntimeConfig represents the "crio.runtime" TOML config table.
 type RuntimeConfig struct {
@@ -300,6 +312,10 @@ type RuntimeConfig struct {
 	// no runtime_handler is provided, the runtime will be picked based on
 	// the level of trust of the workload.
 	Runtimes Runtimes `toml:"runtimes"`
+
+	// Workloads defines a list of workloads types that we group together
+	// settings to apply to.
+	Workloads Workloads `toml:"workloads"`
 
 	// PidsLimit is the number of processes each container is restricted to
 	// by the cgroup process number controller.
@@ -824,6 +840,10 @@ func (c *RuntimeConfig) Validate(systemContext *types.SystemContext, onExecution
 		}
 	}
 
+	if err := c.ValidateWorkloads(); err != nil {
+		return errors.Wrap(err, "workloads validation")
+	}
+
 	// check for validation on execution
 	if onExecution {
 		if err := c.ValidateRuntimes(); err != nil {
@@ -904,6 +924,27 @@ func (c *RuntimeConfig) ValidateRuntimes() error {
 		if err := handler.Validate(name); err != nil {
 			return err
 		}
+	}
+	return nil
+}
+
+// ValidateWorkloads validates the workload settings
+func (c *RuntimeConfig) ValidateWorkloads() error {
+	for workload, settings := range c.Workloads {
+		if err := settings.Validate(workload); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (w *WorkloadSettings) Validate(workloadType string) error {
+	_, err := cpuset.Parse(w.CPUSet)
+	if err != nil {
+		return errors.Wrapf(err, "failed to parse %q as CPU set for workload type %q", w.CPUSet, workloadType)
+	}
+	if w.Label == "" {
+		return fmt.Errorf("label shouldn't be empty for workload type %q", workloadType)
 	}
 	return nil
 }
