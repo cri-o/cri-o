d0602327acc27fca7e38faaf6843b0e5e68c1ae8
Sascha Grunert <sgrunert@redhat.com>
2021-04-16 11:15:25 +0200

    Add resource cleaner retry functionality
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

---
+++ internal/resourcestore/resourcecleaner_defaults.go
@@ -0,0 +1,7 @@
+// +build !test
+
+package resourcestore
+
+// defaultRetryTimes defines the amount of default retries for each cleanup
+// function.
+var defaultRetryTimes = 20
---
+++ internal/resourcestore/resourcecleaner_test_inject.go
@@ -0,0 +1,9 @@
+// +build test
+// All *_inject.go files are meant to be used by tests only. Purpose of this
+// files is to provide a way to inject mocked data into the current setup.
+
+package resourcestore
+
+// defaultRetryTimes reduces the amount of default retries for testing
+// purposes.
+var defaultRetryTimes = 3
