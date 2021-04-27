be0a4d2bffd8a678cb86b40e435a24bacc294d23
Peter Hunt <pehunt@redhat.com>
2021-04-27 09:30:14 -0400

    bump containers/image to 5.5.3
    to fix some fd leaks
    
    Signed-off-by: Peter Hunt <pehunt@redhat.com>

--- vendor/github.com/containers/image/v5/docker/docker_image_src.go
+++ vendor/github.com/containers/image/v5/docker/docker_image_src.go
@@ -241,6 +241,7 @@ func (s *dockerImageSource) getExternalBlob(ctx context.Context, urls []string) 
 			if resp.StatusCode != http.StatusOK {
 				err = errors.Errorf("error fetching external blob from %q: %d (%s)", url, resp.StatusCode, http.StatusText(resp.StatusCode))
 				logrus.Debug(err)
+				resp.Body.Close()
 				continue
 			}
 			break
