64870e3d85f0b43fa920eb3f66245e4057fc811b
Peter Hunt <pehunt@redhat.com>
2021-06-29 13:50:34 -0400

    server: use stats server to get container stats
    Signed-off-by: Peter Hunt <pehunt@redhat.com>

--- server/container_stats.go
+++ server/container_stats.go
@@ -2,33 +2,10 @@ package server
 
 import (
 	"context"
-	"path/filepath"
-	"time"
 
-	"github.com/cri-o/cri-o/internal/log"
-	oci "github.com/cri-o/cri-o/internal/oci"
 	"github.com/cri-o/cri-o/server/cri/types"
-	crioStorage "github.com/cri-o/cri-o/utils"
 	"github.com/pkg/errors"
 )
-
-func (s *Server) buildContainerStats(ctx context.Context, stats *types.ContainerStats, container *oci.Container) *types.ContainerStats {
-	// TODO: Fix this for other storage drivers. This will only work with overlay.
-	if s.ContainerServer.Config().RootConfig.Storage == "overlay" {
-		diffDir := filepath.Join(filepath.Dir(container.MountPoint()), "diff")
-		bytesUsed, inodeUsed, err := crioStorage.GetDiskUsageStats(diffDir)
-		if err != nil {
-			log.Warnf(ctx, "Unable to get disk usage for container %s， %s", container.ID(), err)
-		}
-		stats.WritableLayer = &types.FilesystemUsage{
-			Timestamp:  time.Now().UnixNano(),
-			FsID:       &types.FilesystemIdentifier{Mountpoint: container.MountPoint()},
-			UsedBytes:  &types.UInt64Value{Value: bytesUsed},
-			InodesUsed: &types.UInt64Value{Value: inodeUsed},
-		}
-	}
-	return stats
-}
 
 // ContainerStats returns stats of the container. If the container does not
 // exist, the call returns an error.
@@ -41,12 +18,8 @@ func (s *Server) ContainerStats(ctx context.Context, req *types.ContainerStatsRe
 	if sb == nil {
 		return nil, errors.Errorf("unable to get stats for container %s: sandbox %s not found", container.ID(), container.Sandbox())
 	}
-	cgroup := sb.CgroupParent()
 
-	stats, err := s.Runtime().ContainerStats(ctx, container, cgroup)
-	if err != nil {
-		return nil, err
-	}
-
-	return &types.ContainerStatsResponse{Stats: s.buildContainerStats(ctx, stats, container)}, nil
+	return &types.ContainerStatsResponse{
+		Stats: s.ContainerServer.StatsForContainer(container, sb),
+	}, nil
 }
--- server/container_stats_list.go
+++ server/container_stats_list.go
@@ -1,11 +1,8 @@
 package server
 
 import (
-	"github.com/containers/podman/v3/pkg/cgroups"
-	"github.com/cri-o/cri-o/internal/log"
 	"github.com/cri-o/cri-o/internal/oci"
 	"github.com/cri-o/cri-o/server/cri/types"
-	"github.com/pkg/errors"
 	"golang.org/x/net/context"
 )
 
@@ -29,31 +26,7 @@ func (s *Server) ListContainerStats(ctx context.Context, req *types.ListContaine
 		ctrList = s.filterContainerList(ctx, cFilter, ctrList)
 	}
 
-	allStats := make([]*types.ContainerStats, 0, len(ctrList))
-	for _, container := range ctrList {
-		sb := s.GetSandbox(container.Sandbox())
-		if sb == nil {
-			// Because we don't lock, we will get situations where the container was listed, and then
-			// its sandbox was deleted before we got to checking its stats.
-			// We should not log in this expected situation.
-			continue
-		}
-		cgroup := sb.CgroupParent()
-		stats, err := s.Runtime().ContainerStats(ctx, container, cgroup)
-		if err != nil {
-			// ErrCgroupDeleted is another situation that will happen if the container
-			// is deleted from underneath the call to this function.
-			if !errors.Is(err, cgroups.ErrCgroupDeleted) {
-				// The other errors are much less likely, and possibly useful to hear about.
-				log.Warnf(ctx, "Unable to get stats for container %s: %v", container.ID(), err)
-			}
-			continue
-		}
-		response := s.buildContainerStats(ctx, stats, container)
-		allStats = append(allStats, response)
-	}
-
 	return &types.ListContainerStatsResponse{
-		Stats: allStats,
+		Stats: s.ContainerServer.StatsForContainers(ctrList),
 	}, nil
 }
