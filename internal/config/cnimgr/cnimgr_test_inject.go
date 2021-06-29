d70cbee6adb3e0b5e8f33401f42b39f69a17e389
Peter Hunt <pehunt@redhat.com>
2021-06-29 17:14:15 -0400

    Introduce cnimgr
    CNIManager is a package for managing the CNI plugin
    in particular, it is reponsible for intermediately polling to see if the network is ready
    on server startup.
    
    Once the network is ready, hostNetwork: false pods can be created
    but before then, they should wait until the network is ready
    
    Signed-off-by: Peter Hunt <pehunt@redhat.com>

---
+++ internal/config/cnimgr/cnimgr_test_inject.go
@@ -0,0 +1,21 @@
+// +build test
+// All *_inject.go files are meant to be used by tests only. Purpose of this
+// files is to provide a way to inject mocked data into the current setup.
+
+package cnimgr
+
+import (
+	"github.com/cri-o/ocicni/pkg/ocicni"
+)
+
+// SetCNIPlugin sets the network plugin for the Configuration. The function
+// errors if a sane shutdown of the initially created network plugin failed.
+func (c *CNIManager) SetCNIPlugin(plugin ocicni.CNIPlugin) error {
+	if c.plugin != nil {
+		if err := c.plugin.Shutdown(); err != nil {
+			return err
+		}
+	}
+	c.plugin = plugin
+	return nil
+}
