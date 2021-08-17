2d0085d2e316f3fdd2ee62e67b7ca49f45f8d136
Peter Hunt <pehunt@redhat.com>
2021-08-17 15:02:19 -0400

    oci: fix panic on double close
    and listen to a comment saying explicitly not to close that channel there :facepalm:
    
    Signed-off-by: Peter Hunt <pehunt@redhat.com>

--- internal/oci/runtime_oci.go
+++ internal/oci/runtime_oci.go
@@ -1132,7 +1132,6 @@ func WatchForFile(path string, done chan struct{}, opsToWatch ...fsnotify.Op) (c
 		}
 	}()
 	if err := watcher.Add(dir); err != nil {
-		close(done)
 		return nil, err
 	}
 	return ch, nil
