4dd68496421491392a2fab6a566439468ca56c89
Sascha Grunert <sgrunert@redhat.com>
2021-12-10 10:27:03 +0100

    Use timeout for conmon cgroup move
    We're now using a timeout which is able to unblock gaining
    the sandbox mutex in a container creation vs sandbox stop race.
    
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>
    Signed-off-by: Peter Hunt <pehunt@redhat.com>

--- utils/utils.go
+++ utils/utils.go
@@ -14,6 +14,7 @@ import (
 	"strconv"
 	"strings"
 	"syscall"
+	"time"
 
 	"github.com/containers/libpod/v2/pkg/lookup"
 	"github.com/cri-o/cri-o/internal/dbusmgr"
@@ -68,23 +69,26 @@ func RunUnderSystemdScope(mgr *dbusmgr.DbusConnManager, pid int, slice, unitName
 		properties = append(properties, systemdDbus.PropSlice(slice))
 	}
 	ch := make(chan string)
-	for {
-		err := mgr.RetryOnDisconnect(func(c *systemdDbus.Conn) error {
-			_, err := c.StartTransientUnit(unitName, "replace", properties, ch)
-			return err
-		})
-
-		if err == nil {
-			break
-		}
-		if !errors.Is(err, syscall.EAGAIN) {
-			return err
-		}
+	if err := mgr.RetryOnDisconnect(func(c *systemdDbus.Conn) error {
+		_, err := c.StartTransientUnit(unitName, "replace", properties, ch)
+		return errors.Wrap(err, "start transient unit")
+	}); err != nil {
+		return err
 	}
 
 	// Block until job is started
-	<-ch
-	close(ch)
+	select {
+	case <-ch:
+		close(ch)
+	case <-time.After(time.Minute * 6):
+		// This case is a work around to catch situations where the dbus library sends the
+		// request but it unexpectedly disappears. We set the timeout very high to make sure
+		// we wait as long as possible to catch situations where dbus is overwhelmed.
+		// We also don't use the native context cancelling behavior of the dbus library,
+		// because experience has shown that it does not help.
+		// TODO: Find cause of the request being dropped in the dbus library and fix it.
+		return errors.Errorf("timed out moving conmon with pid %d to cgroup", pid)
+	}
 
 	return nil
 }
