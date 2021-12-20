9dcb022540945abea3b18c2a80ff3c84e7ad0bf2
Peter Hunt <pehunt@redhat.com>
2021-12-20 15:10:53 -0500

    cmdrunner: add tests for prepended commands
    Signed-off-by: Peter Hunt <pehunt@redhat.com>

---
+++ utils/cmdrunner/suite_test.go
@@ -0,0 +1,26 @@
+package cmdrunner_test
+
+import (
+	"testing"
+
+	. "github.com/cri-o/cri-o/test/framework"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+// TestCommandRunner runs the created specs
+func TestCommandRunner(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunFrameworkSpecs(t, "CommandRunner")
+}
+
+var t *TestFramework
+
+var _ = BeforeSuite(func() {
+	t = NewTestFramework(NilFunc, NilFunc)
+	t.Setup()
+})
+
+var _ = AfterSuite(func() {
+	t.Teardown()
+})
