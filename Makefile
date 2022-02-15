cri-o/cri-o

commit 7f9f098011f4bf9ca8c24d08abd744c99236906d

ci: move golangci-lint from circleci to GH actions
It should be faster, and the side benefit of using github
actions is linter warnings will be shown in the diff
as annotations (similar to review comments).

To make it work without installing additional packages,
fix the build tags in .golangci.yml. In particular,
disable seccomp and device mapper, as they need some C
headers to be compiled.

Note that this is merely to be able to compile packages
used by crio, and since we are only linting crio itself,
and not the packages it imports, changing the tags not
used by crio doesn't matter much (if at all).

Note we still have `lint` target in Makefile, to be used
by developers (but not for CI).

Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

Makefile
@@ -138,6 +138,7 @@ ifeq ("$(wildcard $(GOPKGDIR))","")
 endif
 	touch "$(GOPATH)/.gopathok"
 
+# See also: .github/workflows/lint.yml
 lint: .gopathok ${GOLANGCI_LINT}
 	${GOLANGCI_LINT} version
 	${GOLANGCI_LINT} linters

.circleci/config.yml
@@ -88,7 +88,6 @@ workflows:
       - integration:
           name: integration-cgroupfs
           cgroup_manager: cgroupfs
-      - lint
       - release-branch-forward:
           requires:
             - results
@@ -360,21 +359,6 @@ jobs:
             CONTAINER_CONMON_CGROUP: "<< parameters.conmon_cgroup >>"
       - save_cache:
           key: v1-integration-{{ checksum "go.sum" }}
-          paths:
-            - *gocache
-
-  lint:
-    executor: container
-    steps:
-      - checkout
-      - restore_cache:
-          keys:
-            - v4-lint-{{ checksum "go.sum" }}
-      - run:
-          name: lint
-          command: make lint
-      - save_cache:
-          key: v4-lint-{{ checksum "go.sum" }}
           paths:
             - *gocache
 

.github/workflows/verify.yml
@@ -1,0 +1,22 @@
+name: verify
+on:
+  push:
+    tags:
+      - v*
+    branches:
+      - master
+  pull_request:
+jobs:
+  golangci:
+    name: lint
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v2
+      - name: golangci-lint
+        uses: golangci/golangci-lint-action@v2
+        with:
+          # must be specified without patch version
+          version: v1.32
+
+          # Only show new issues for a pull request.
+          only-new-issues: true

.golangci.yml
@@ -3,12 +3,14 @@ run:
   build-tags:
     - apparmor
     - containers_image_ostree_stub
-    - seccomp
+    # containers/image would use mtrmac/gpgme otherwise, which requires gpgme C headers
+    - containers_image_openpgp
     - selinux
     - test
-    # containers/storage currently has btrfs included, but the images don't, so
-    # the linter fails.
+    # needs btrfs headers installed
     - exclude_graphdriver_btrfs
+    # needs devmapper headers installed
+    - exclude_graphdriver_devicemapper
   concurrency: 6
   deadline: 5m
 linters:

commit 98d945cc99336b8bc8c93e1e0b2908a4630c689e

Makefile: fix a comment
Fixes: 7f9f098011f4b
Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

Makefile
@@ -134,7 +134,7 @@ ifeq ("$(wildcard $(GOPKGDIR))","")
 endif
 	touch "$(GOPATH)/.gopathok"
 
-# See also: .github/workflows/lint.yml
+# See also: .github/workflows/verify.yml.
 lint: .gopathok ${GOLANGCI_LINT}
 	${GOLANGCI_LINT} version
 	${GOLANGCI_LINT} linters