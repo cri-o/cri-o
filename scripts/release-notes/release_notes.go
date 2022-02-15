cri-o/cri-o

commit a53f1d2213f18c06b0978073ae570bfa96f2a1d1

Generate checksum files for artifacts
Signed-off-by: T K Chandra Hasan <t.k.chandra.hasan@ibm.com>

scripts/release-notes/release_notes.go
@@ -114,7 +114,9 @@ The release notes have been generated for the commit range
 Download one of our static release bundles via our Google Cloud Bucket:
 
 - [cri-o.amd64.%s.tar.gz](https://storage.googleapis.com/cri-o/artifacts/cri-o.amd64.%s.tar.gz)
+- [cri-o.amd64.%s.tar.gz.sha256sum](https://storage.googleapis.com/cri-o/artifacts/cri-o.amd64.%s.tar.gz.sha256sum)
 - [cri-o.arm64.%s.tar.gz](https://storage.googleapis.com/cri-o/artifacts/cri-o.arm64.%s.tar.gz)
+- [cri-o.arm64.%s.tar.gz.sha256sum](https://storage.googleapis.com/cri-o/artifacts/cri-o.arm64.%s.tar.gz.sha256sum)
 
 ## Changelog since %s
 

contrib/bundle/build
@@ -154,6 +154,8 @@ TAR_DIR=cri-o
 tar cfz "$ARCHIVE" tmp --transform s/tmp/$TAR_DIR/
 rm -rf "$TMPDIR"
 echo "Created $ARCHIVE_PATH/$ARCHIVE"
+sha256sum "$ARCHIVE" > "$ARCHIVE_SHA256SUM"
+echo "Created $ARCHIVE_PATH/$ARCHIVE_SHA256SUM"
 
 # Test the archive
 echo "Testing archive"

contrib/bundle/vars
@@ -8,5 +8,7 @@ mkdir -p "$ARCHIVE_PATH"
 
 ARCH=${1:-amd64}
 ARCHIVE="cri-o.$ARCH.$(git describe --tags --exact-match 2>/dev/null || git rev-parse HEAD).tar.gz"
+ARCHIVE_SHA256SUM="$ARCHIVE.sha256sum"
 
 export ARCHIVE
+export ARCHIVE_SHA256SUM

scripts/upload-artifacts
@@ -12,7 +12,7 @@ else
     gcloud auth activate-service-account --key-file=/tmp/key.json
 
     BUCKET=gs://cri-o
-    gsutil cp -n build/bundle/*.tar.gz $BUCKET/artifacts
+    gsutil cp -n build/bundle/*.tar.gz* $BUCKET/artifacts
 
     # update the latest version marker file for the branch
     MARKER=$(git rev-parse --abbrev-ref HEAD)
@@ -40,7 +40,7 @@ if TAG=$(git describe --exact-match --tags 2>/dev/null); then
             exit 1
         fi
         UPLOAD_URL=${UPLOAD_URL%"{?name,label}"}
-        for FILE in build/bundle/*.tar.gz; do
+        for FILE in build/bundle/*.tar.gz*; do
             curl -f \
                 -H "Authorization: token $GITHUB_TOKEN" \
                 -H "Content-Type: $(file -b --mime-type "$FILE")" \

commit cc6ed292b92d495ae2b1a168ac2e1226dc669327

scripts/release-notes: fix printf args
Found by govet:

	scripts/release-notes/release_notes.go:107:40: printf: fmt.Sprintf format %s reads arg #12, but call has 11 args (govet)
		if _, err := templateFile.WriteString(fmt.Sprintf(`# CRI-O %s
						      ^

Fixes: a53f1d2213f18
Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

scripts/release-notes/release_notes.go
@@ -140,6 +140,8 @@ Download one of our static release bundles via our Google Cloud Bucket:
 		time.Now().Format(time.RFC1123),
 		bundleVersion, bundleVersion,
 		bundleVersion, bundleVersion,
+		bundleVersion, bundleVersion,
+		bundleVersion, bundleVersion,
 		startTag,
 	)); err != nil {
 		return errors.Wrap(err, "writing tmplate to file")