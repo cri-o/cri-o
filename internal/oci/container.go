8d58eaa7dc198d9afe0d206717c2f376561c7ca3
Peter Hunt <pehunt@redhat.com>
2022-05-18 16:57:52 -0400

    oci: kill children of container if it is in the host pid namespace
    When PID 1 of a PID namespace is killed, all of the remaining processes are also killed.
    However, for containers with a host PID namespace, this does not happen.
    
    While we will eventually kill these processes when the kubelet tears down the sandbox's cgroup,
    it looks like a leak (and could cause weird situations with duplicated processes running)
    
    Fix it by killing the processes in this case.
    
    Signed-off-by: Peter Hunt <pehunt@redhat.com>

--- internal/oci/container.go
+++ internal/oci/container.go
@@ -609,3 +609,16 @@ func (c *Container) RemoveManagedPIDNamespace() error {
 	}
 	return errors.Wrapf(c.pidns.Remove(), "remove PID namespace for container %s", c.ID())
 }
+
+// nodeLevelPIDNamespace searches through the container spec to see if there is
+// a PID namespace specified. If not, it returns `true` (because the runtime spec
+// defines a node level namespace as being absent from the Namespaces list)
+func (c *Container) nodeLevelPIDNamespace() bool {
+	for i := range c.spec.Linux.Namespaces {
+		// If it's specified in the namespace list, then it is something other than Node level
+		if c.spec.Linux.Namespaces[i].Type == specs.PIDNamespace {
+			return false
+		}
+	}
+	return true
+}
