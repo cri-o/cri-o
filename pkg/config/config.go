fc852b402f1698501cac71963c9ccc919c4b05fe
Peter Hunt <pehunt@redhat.com>
2022-05-23 15:19:33 -0400

    add support for conmon log-global-size-max
    Signed-off-by: Peter Hunt <pehunt@redhat.com>

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1179,6 +1179,10 @@ func (c *RuntimeConfig) ConmonSupportsSync() bool {
 	return c.conmonManager.SupportsSync()
 }
 
+func (c *RuntimeConfig) ConmonSupportsLogGlobalSizeMax() bool {
+	return c.conmonManager.SupportsLogGlobalSizeMax()
+}
+
 func (c *RuntimeConfig) ValidatePinnsPath(executable string) error {
 	var err error
 	c.PinnsPath, err = validateExecutablePath(executable, c.PinnsPath)
