6b653e7e189c441743be9dc4372e0c089b983a53
Peter Hunt <pehunt@redhat.com>
2022-07-01 16:25:05 -0400

    server: use CRIStatus for container status call
    Signed-off-by: Peter Hunt <pehunt@redhat.com>

--- server/container_status.go
+++ server/container_status.go
@@ -3,7 +3,6 @@ package server
 import (
 	"fmt"
 
-	"github.com/cri-o/cri-o/internal/log"
 	oci "github.com/cri-o/cri-o/internal/oci"
 	json "github.com/json-iterator/go"
 	spec "github.com/opencontainers/runtime-spec/specs-go"
@@ -13,12 +12,6 @@ import (
 	types "k8s.io/cri-api/pkg/apis/runtime/v1"
 )
 
-const (
-	oomKilledReason = "OOMKilled"
-	completedReason = "Completed"
-	errorReason     = "Error"
-)
-
 // ContainerStatus returns status of the container.
 func (s *Server) ContainerStatus(ctx context.Context, req *types.ContainerStatusRequest) (*types.ContainerStatusResponse, error) {
 	c, err := s.GetContainerFromShortID(req.ContainerId)
@@ -26,67 +19,9 @@ func (s *Server) ContainerStatus(ctx context.Context, req *types.ContainerStatus
 		return nil, status.Errorf(codes.NotFound, "could not find container %q: %v", req.ContainerId, err)
 	}
 
-	containerID := c.ID()
 	resp := &types.ContainerStatusResponse{
-		Status: &types.ContainerStatus{
-			Id:          containerID,
-			Metadata:    c.Metadata(),
-			Labels:      c.Labels(),
-			Annotations: c.Annotations(),
-			ImageRef:    c.ImageRef(),
-			Image: &types.ImageSpec{
-				Image: c.ImageName(),
-			},
-		},
+		Status: c.CRIStatus(),
 	}
-
-	resp.Status.Mounts = c.Mounts()
-
-	cState := c.StateNoLock()
-	rStatus := types.ContainerState_CONTAINER_UNKNOWN
-
-	// If we defaulted to exit code not set earlier then we attempt to
-	// get the exit code from the exit file again.
-	if cState.Status == oci.ContainerStateStopped && cState.ExitCode == nil {
-		err := s.Runtime().UpdateContainerStatus(ctx, c)
-		if err != nil {
-			log.Warnf(ctx, "Failed to UpdateStatus of container %s: %v", c.ID(), err)
-		}
-		cState = c.State()
-	}
-
-	resp.Status.CreatedAt = c.CreatedAt().UnixNano()
-	switch cState.Status {
-	case oci.ContainerStateCreated:
-		rStatus = types.ContainerState_CONTAINER_CREATED
-	case oci.ContainerStateRunning, oci.ContainerStatePaused:
-		rStatus = types.ContainerState_CONTAINER_RUNNING
-		started := cState.Started.UnixNano()
-		resp.Status.StartedAt = started
-	case oci.ContainerStateStopped:
-		rStatus = types.ContainerState_CONTAINER_EXITED
-		started := cState.Started.UnixNano()
-		resp.Status.StartedAt = started
-		finished := cState.Finished.UnixNano()
-		resp.Status.FinishedAt = finished
-		if cState.ExitCode == nil {
-			resp.Status.ExitCode = -1
-		} else {
-			resp.Status.ExitCode = *cState.ExitCode
-		}
-		switch {
-		case cState.OOMKilled:
-			resp.Status.Reason = oomKilledReason
-		case resp.Status.ExitCode == 0:
-			resp.Status.Reason = completedReason
-		default:
-			resp.Status.Reason = errorReason
-			resp.Status.Message = cState.Error
-		}
-	}
-
-	resp.Status.State = rStatus
-	resp.Status.LogPath = c.LogPath()
 
 	if req.Verbose {
 		info, err := s.createContainerInfo(c)
