cd9636049277f2eb13ca8d819248e767581bd34a
Sascha Grunert <sgrunert@redhat.com>
2023-04-25 13:24:39 +0200

    Download more dependencies instead of vendoring
    We can download many other dependencies instead of vendoring them into
    `go.mod` / `vendor`, which saves some space and CI time.
    
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

--- internal/tools/tools.go
+++ internal/tools/tools.go
@@ -5,8 +5,6 @@ package tools
 
 import (
 	_ "github.com/cpuguy83/go-md2man"
-	_ "github.com/golang/mock/mockgen"
+	_ "github.com/golang/mock/mockgen/model"
 	_ "github.com/onsi/ginkgo/v2/ginkgo"
-	_ "github.com/psampaz/go-mod-outdated"
-	_ "mvdan.cc/sh/v3/cmd/shfmt"
 )
