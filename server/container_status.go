3511aa72f8e1598ad7556442a0cacdacb6be3f4a
Peter Hunt <pehunt@redhat.com>
2023-05-16 13:00:55 -0400

    server: do not take lock to populate pid in container status and inspect
    We can do this best-effort, and don't need to take the lock to do so.
    Also, by using the InitPid, we're not prone to reading a variable that changes underneath us,
    as the init pid is set once and not changed.
    
    for inspect, we can use Created without a lock because it's only set once.
    
    Signed-off-by: Peter Hunt <pehunt@redhat.com>

--- server/container_status.go
+++ server/container_status.go
@@ -122,7 +122,7 @@ func (s *Server) createContainerInfo(container *oci.Container) (map[string]strin
 		Privileged  bool      `json:"privileged"`
 	}{
 		container.Sandbox(),
-		container.State().Pid,
+		container.StateNoLock().Pid,
 		container.Spec(),
 		metadata.Privileged,
 	}
