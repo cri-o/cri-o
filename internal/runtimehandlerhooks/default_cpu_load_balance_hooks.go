6da8e46b1a54eda348b8bc95bd2ffd0f3fbba54f
Peter Hunt <pehunt@redhat.com>
2023-05-24 16:28:53 -0400

    runtime handler hooks: add DefaultCPULoadBalanceHooks
    disabling cpu load balancing requires all overlapping cpusets to have the
    cpuset.sched_load_balance field to 0. There is a bug where stale containers
    that have stopped still have the field set to 1, and cpumanager won't attempt to
    load balance the cpus away from them. Thus, adding a pod that needs cpu load
    balancing to be disabled after other pods have stopped causes the cpu load balancing
    to not apply in the kernel.
    
    Fix this by adding PostStop hooks for all containers (when high performance hooks are enabled).
    This hook will disable sched_load_balance in the container's cgroup. This cgroup is stale, as
    the containers is already stopped and won't be restarted, so this won't change behavior past
    allowing cpu load balancing to be disabled for new containers.
    
    Signed-off-by: Peter Hunt <pehunt@redhat.com>

---
+++ internal/runtimehandlerhooks/default_cpu_load_balance_hooks.go
@@ -0,0 +1,50 @@
+package runtimehandlerhooks
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/cri-o/cri-o/internal/config/node"
+	"github.com/cri-o/cri-o/internal/lib/sandbox"
+	"github.com/cri-o/cri-o/internal/oci"
+	"github.com/opencontainers/runc/libcontainer/cgroups"
+)
+
+// DefaultCPULoadBalanceHooks is used to run additional hooks that will configure containers for CPU load balancing.
+// Specifically, it will define a PostStop that disables `cpuset.sched_load_balance` for a recently stopped container.
+// This must be done because guaranteed pods with exclusive cpu access may be created after other containers are terminated,
+// but before their cgroup is cleaned up. In this case, cpumanager will not load balancing the exclusive CPUs away from those pods,
+// thus causing their `cpuset.sched_load_balance=1` to prevent the kernel from disabling load balancing.
+// This is the only case it seeks to fix, and thus does not define any other members of the RuntimeHandlerHooks functions.
+type DefaultCPULoadBalanceHooks struct{}
+
+// No-op
+func (*DefaultCPULoadBalanceHooks) PreStart(context.Context, *oci.Container, *sandbox.Sandbox) error {
+	return nil
+}
+
+// No-op
+func (*DefaultCPULoadBalanceHooks) PreStop(context.Context, *oci.Container, *sandbox.Sandbox) error {
+	return nil
+}
+
+func (*DefaultCPULoadBalanceHooks) PostStop(ctx context.Context, c *oci.Container, s *sandbox.Sandbox) error {
+	// Disable cpuset.sched_load_balance for all stale cgroups.
+	// This way, cpumanager can ignore stopped containers, but the running ones will still have exclusive access.
+	if node.CgroupIsV2() || c.Spoofed() {
+		return nil
+	}
+	containerCgroup, containerCgroupParent, systemd, err := containerCgroupAndParent(s.CgroupParent(), c)
+	if err != nil {
+		return err
+	}
+	mgr, err := libctrManager(containerCgroup, containerCgroupParent, systemd)
+	if err != nil {
+		return err
+	}
+	path := mgr.Path("cpuset")
+	if path == "" {
+		return fmt.Errorf("failed to find cpuset for newly created cgroup")
+	}
+	return cgroups.WriteFile(path, "cpuset.sched_load_balance", "0")
+}
