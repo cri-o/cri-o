b7d1c2e618f17d4c4f797063748275ab332c17f3
Kir Kolyshkin <kolyshkin@gmail.com>
2023-05-30 16:49:29 -0700

    test/helpers: use cli to set container dirs
    Use CLI options is way more readable than sed.
    
    Fixes: 8e2ee9bafd9c759326
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- test/helpers.bash
+++ test/helpers.bash
@@ -167,6 +167,8 @@ function setup_crio() {
         --apparmor-profile "$apparmor" \
         --cgroup-manager "$CONTAINER_CGROUP_MANAGER" \
         --conmon "$CONMON_BINARY" \
+        --container-attach-socket-dir "$CONTAINER_ATTACH_SOCKET_DIR" \
+        --container-exits-dir "$CONTAINER_EXITS_DIR" \
         --listen "$CRIO_SOCKET" \
         --registry "quay.io" \
         --registry "docker.io" \
@@ -186,10 +188,6 @@ function setup_crio() {
     # make sure we don't run with nodev, or else mounting a readonly rootfs will fail: https://github.com/cri-o/cri-o/issues/1929#issuecomment-474240498
     sed -r -e 's/nodev(,)?//g' -i "$CRIO_CONFIG"
     sed -r -e 's/nodev(,)?//g' -i "$CRIO_CUSTOM_CONFIG"
-    sed -i -e 's;\(container_exits_dir =\) \(.*\);\1 "'"$CONTAINER_EXITS_DIR"'";g' "$CRIO_CONFIG"
-    sed -i -e 's;\(container_exits_dir =\) \(.*\);\1 "'"$CONTAINER_EXITS_DIR"'";g' "$CRIO_CUSTOM_CONFIG"
-    sed -i -e 's;\(container_attach_socket_dir =\) \(.*\);\1 "'"$CONTAINER_ATTACH_SOCKET_DIR"'";g' "$CRIO_CONFIG"
-    sed -i -e 's;\(container_attach_socket_dir =\) \(.*\);\1 "'"$CONTAINER_ATTACH_SOCKET_DIR"'";g' "$CRIO_CUSTOM_CONFIG"
     prepare_network_conf
 }
 
