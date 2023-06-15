5523c0203ce0d6f284409b3c995d71c05d4a2a86
Doug Rabson <dfr@rabson.org>
2023-06-15 08:18:35 +0100

    internal/runtimehandlerhooks: add non-linux stubs
    Signed-off-by: Doug Rabson <dfr@rabson.org>

---
+++ internal/runtimehandlerhooks/runtime_handler_hooks_linux.go
@@ -0,0 +1,62 @@
+package runtimehandlerhooks
+
+import (
+	"context"
+	"strings"
+
+	"github.com/cri-o/cri-o/internal/log"
+	crioann "github.com/cri-o/cri-o/pkg/annotations"
+	libconfig "github.com/cri-o/cri-o/pkg/config"
+)
+
+// GetRuntimeHandlerHooks returns RuntimeHandlerHooks implementation by the runtime handler name
+func GetRuntimeHandlerHooks(ctx context.Context, config *libconfig.Config, handler string, annotations map[string]string) (RuntimeHandlerHooks, error) {
+	ctx, span := log.StartSpan(ctx)
+	defer span.End()
+	if strings.Contains(handler, HighPerformance) {
+		log.Warnf(ctx, "The usage of the handler %q without adding high-performance feature annotations under allowed_annotations will be deprecated under 1.21", HighPerformance)
+		return &HighPerformanceHooks{config.IrqBalanceConfigFile}, nil
+	}
+	if highPerformanceAnnotationsSpecified(annotations) {
+		log.Warnf(ctx, "The usage of the handler %q without adding high-performance feature annotations under allowed_annotations will be deprecated under 1.21", HighPerformance)
+		return &HighPerformanceHooks{config.IrqBalanceConfigFile}, nil
+	}
+	if cpuLoadBalancingAllowed(config) {
+		return &DefaultCPULoadBalanceHooks{}, nil
+	}
+
+	return nil, nil
+}
+
+func highPerformanceAnnotationsSpecified(annotations map[string]string) bool {
+	for k := range annotations {
+		if strings.HasPrefix(k, crioann.CPULoadBalancingAnnotation) ||
+			strings.HasPrefix(k, crioann.CPUQuotaAnnotation) ||
+			strings.HasPrefix(k, crioann.IRQLoadBalancingAnnotation) ||
+			strings.HasPrefix(k, crioann.CPUCStatesAnnotation) ||
+			strings.HasPrefix(k, crioann.CPUFreqGovernorAnnotation) {
+			return true
+		}
+	}
+	return false
+}
+
+func cpuLoadBalancingAllowed(config *libconfig.Config) bool {
+	cpuLoadBalancingAllowedAnywhereOnce.Do(func() {
+		for _, runtime := range config.Runtimes {
+			for _, ann := range runtime.AllowedAnnotations {
+				if ann == crioann.CPULoadBalancingAnnotation {
+					cpuLoadBalancingAllowedAnywhere = true
+				}
+			}
+		}
+		for _, workload := range config.Workloads {
+			for _, ann := range workload.AllowedAnnotations {
+				if ann == crioann.CPULoadBalancingAnnotation {
+					cpuLoadBalancingAllowedAnywhere = true
+				}
+			}
+		}
+	})
+	return cpuLoadBalancingAllowedAnywhere
+}
