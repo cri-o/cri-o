34fbd671074a0814a5e5ebb404316ce536d5c55f
Peter Hunt <pehunt@redhat.com>
2023-06-16 13:24:59 -0400

    server: move post stop container behavior to server.go:handleExit
    Currently, we're duplicating a lot of the container stop operations. Whenever a container stops, conmon will write the exit file
    and CRI-O should catch the exit, no matter whether it was triggered by CRI-O or happened naturally.
    
    Not adding these will prevent duplicated work like UpdateContainerStatus, which calls `runc update` and is not negligable.
    
    Signed-off-by: Peter Hunt <pehunt@redhat.com>

--- server/server.go
+++ server/server.go
@@ -790,7 +790,6 @@ func (s *Server) handleExit(ctx context.Context, event fsnotify.Event) {
 	containerID := filepath.Base(event.Name)
 	log.Debugf(ctx, "Container or sandbox exited: %v", containerID)
 	c := s.GetContainer(ctx, containerID)
-	nriCtr := c
 	resource := "container"
 	var sb *sandbox.Sandbox
 	if c == nil {
@@ -805,26 +804,15 @@ func (s *Server) handleExit(ctx context.Context, event fsnotify.Event) {
 	}
 	log.Debugf(ctx, "%s exited and found: %v", resource, containerID)
 
-	if err := s.ContainerStateToDisk(ctx, c); err != nil {
-		log.Warnf(ctx, "Unable to write %s %s state to disk: %v", resource, c.ID(), err)
-	}
-
-	if nriCtr != nil {
-		if err := s.nri.stopContainer(ctx, nil, nriCtr); err != nil {
-			log.Warnf(ctx, "NRI stop container request of %s failed: %v", nriCtr.ID(), err)
-		}
-	}
-
 	hooks, err := runtimehandlerhooks.GetRuntimeHandlerHooks(ctx, &s.config, sb.RuntimeHandler(), sb.Annotations())
 	if err != nil {
 		log.Warnf(ctx, "Failed to get runtime handler %q hooks", sb.RuntimeHandler())
-	} else if hooks != nil {
-		if err := hooks.PostStop(ctx, c, sb); err != nil {
-			log.Errorf(ctx, "Failed to run post-stop hook for container %s: %v", c.ID(), err)
-		}
 	}
 
-	s.generateCRIEvent(ctx, c, types.ContainerEventType_CONTAINER_STOPPED_EVENT)
+	if err := s.postStopContainer(ctx, c, sb, hooks); err != nil {
+		log.Warnf(ctx, "Failed to do post stop container tasks: %v", err)
+	}
+
 	if err := os.Remove(event.Name); err != nil {
 		log.Warnf(ctx, "Failed to remove exit file: %v", err)
 	}
