fbfca3a528d704e1e4f05b762ed5045d55a60f4d
Peter Hunt <pehunt@redhat.com>
2023-07-12 13:08:44 -0400

    oci: update unit tests for new stop code
    Signed-off-by: Peter Hunt <pehunt@redhat.com>

--- internal/oci/runtime_oci_test.go
+++ internal/oci/runtime_oci_test.go
@@ -2,31 +2,35 @@ package oci_test
 
 import (
 	"context"
-	"fmt"
 	"math/rand"
 	"os"
 	"os/exec"
 	"time"
 
 	"github.com/cri-o/cri-o/internal/oci"
+	libconfig "github.com/cri-o/cri-o/pkg/config"
+	runnerMock "github.com/cri-o/cri-o/test/mocks/cmdrunner"
+	"github.com/cri-o/cri-o/utils/cmdrunner"
+	"github.com/golang/mock/gomock"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
 const (
 	shortTimeout  int64 = 1
-	mediumTimeout int64 = 3
+	mediumTimeout int64 = 5
 	longTimeout   int64 = 15
 )
 
 // The actual test suite
 var _ = t.Describe("Oci", func() {
-	Context("ContainerStop", func() {
+	Context("StopContainer", func() {
 		var (
 			sut          *oci.Container
 			sleepProcess *exec.Cmd
+			runner       *runnerMock.MockCommandRunner
+			runtime      oci.RuntimeOCI
 		)
-
 		BeforeEach(func() {
 			sleepProcess = exec.Command("sleep", "100000")
 			Expect(sleepProcess.Start()).To(BeNil())
@@ -38,6 +42,15 @@ var _ = t.Describe("Oci", func() {
 			state.Pid = sleepProcess.Process.Pid
 			Expect(state.SetInitPid(sleepProcess.Process.Pid)).To(BeNil())
 			sut.SetState(state)
+
+			runner = runnerMock.NewMockCommandRunner(mockCtrl)
+			cmdrunner.SetMocked(runner)
+
+			cfg, err := libconfig.DefaultConfig()
+			Expect(err).To(BeNil())
+			r, err := oci.New(cfg)
+			Expect(err).To(BeNil())
+			runtime = oci.NewRuntimeOCI(r, &libconfig.RuntimeHandler{})
 		})
 		AfterEach(func() {
 			// nolint:errcheck
@@ -45,103 +58,125 @@ var _ = t.Describe("Oci", func() {
 			// make sure the entry in the process table is cleaned up
 			// nolint:errcheck
 			sleepProcess.Wait()
+			cmdrunner.ResetPrependedCmd()
 		})
-		tests := []struct {
-			ignoreKill             bool
-			verifyCorrectlyStopped func(*oci.Container, *exec.Cmd, error)
-			name                   string
-		}{
-			{
-				ignoreKill:             true,
-				verifyCorrectlyStopped: verifyContainerNotStopped,
-				name:                   "ignoring kill",
-			},
-			{
-				ignoreKill:             false,
-				verifyCorrectlyStopped: verifyContainerStopped,
-				name:                   "not ignoring kill",
-			},
-		}
-		for _, test := range tests {
-			test := test
-			It("should stop container after timeout if "+test.name, func() {
-				// Given
-				sut.SetAsStopping(shortTimeout)
 
-				// When
-				err := oci.WaitContainerStop(context.Background(), sut, inSeconds(shortTimeout), test.ignoreKill)
+		It("should fail to stop if container paused", func() {
+			state := &oci.ContainerState{}
+			state.Status = oci.ContainerStatePaused
+			sut.SetState(state)
 
-				// Then
-				test.verifyCorrectlyStopped(sut, sleepProcess, err)
-			})
-			It("should interrupt longer stop timeout if "+test.name, func() {
-				// Given
-				stoppedChan := make(chan error, 1)
-				sut.SetAsStopping(longTimeout)
-				go waitContainerStopAndFailAfterTimeout(context.Background(), stoppedChan, sut, longTimeout, longTimeout, test.ignoreKill)
+			Expect(sut.ShouldBeStopped()).NotTo(BeNil())
+		})
+		It("should fail to stop if container stopped", func() {
+			state := &oci.ContainerState{}
+			state.Status = oci.ContainerStateStopped
+			sut.SetState(state)
 
-				// When
-				sut.SetAsStopping(shortTimeout)
+			Expect(sut.ShouldBeStopped()).To(Equal(oci.ErrContainerStopped))
+		})
+		It("should return early if runtime command fails and process stopped", func() {
+			// Given
+			gomock.InOrder(
+				runner.EXPECT().Command(gomock.Any(), gomock.Any()).DoAndReturn(
+					func(_ string, _ ...string) interface{} {
+						Expect(oci.Kill(sleepProcess.Process.Pid)).To(BeNil())
+						waitForKillToComplete(sleepProcess)
+						return exec.Command("/bin/false")
+					},
+				),
+			)
 
-				// Then
-				test.verifyCorrectlyStopped(sut, sleepProcess, <-stoppedChan)
-			})
-			It("should handle being killed mid-timeout if "+test.name, func() {
-				// Given
-				stoppedChan := make(chan error, 1)
-				sut.SetAsStopping(longTimeout)
-				go waitContainerStopAndFailAfterTimeout(context.Background(), stoppedChan, sut, longTimeout, mediumTimeout, test.ignoreKill)
+			// When
+			sut.SetAsStopping()
+			runtime.StopLoopForContainer(sut)
 
-				// When
-				// nolint:errcheck
-				oci.Kill(sleepProcess.Process.Pid)
-				waitForKillToComplete(sleepProcess)
+			// Then
+			Expect(sut.State().Finished).NotTo(BeZero())
+			verifyContainerStopped(sut, sleepProcess)
+		})
+		It("should stop container before timeout", func() {
+			// Given
+			gomock.InOrder(
+				runner.EXPECT().Command(gomock.Any(), gomock.Any()).DoAndReturn(
+					func(_ string, _ ...string) interface{} {
+						Expect(oci.Kill(sleepProcess.Process.Pid)).To(BeNil())
+						waitForKillToComplete(sleepProcess)
+						return exec.Command("/bin/true")
+					},
+				),
+			)
+			sut.SetAsStopping()
+			go runtime.StopLoopForContainer(sut)
 
-				// Then
-				// unconditionally expect the container was stopped
-				verifyContainerStopped(sut, sleepProcess, <-stoppedChan)
-			})
-			It("should handle context timeout if "+test.name, func() {
-				// Given
-				ctx, cancel := context.WithCancel(context.Background())
-				stoppedChan := make(chan error, 1)
-				sut.SetAsStopping(longTimeout)
-				go waitContainerStopAndFailAfterTimeout(ctx, stoppedChan, sut, longTimeout, mediumTimeout, test.ignoreKill)
+			// Then
+			waitOnContainerTimeout(sut, longTimeout, mediumTimeout, sleepProcess)
+		})
+		It("should fall back to KILL after timeout", func() {
+			// Given
+			containerIgnoreSignalCmdrunnerMock(sleepProcess, runner)
+			sut.SetAsStopping()
+			go runtime.StopLoopForContainer(sut)
 
-				// When
-				cancel()
+			// Then
+			waitOnContainerTimeout(sut, shortTimeout, mediumTimeout, sleepProcess)
+		})
+		It("should interrupt longer stop timeout", func() {
+			// Given
+			containerIgnoreSignalCmdrunnerMock(sleepProcess, runner)
+			sut.SetAsStopping()
+			go runtime.StopLoopForContainer(sut)
+			go sut.WaitOnStopTimeout(context.Background(), longTimeout)
 
-				// Then
-				// unconditionally expect the container was not stopped
-				verifyContainerNotStopped(sut, sleepProcess, <-stoppedChan)
-			})
-			It("should not update time if chronologically after if "+test.name, func() {
-				// Given
-				stoppedChan := make(chan error, 1)
-				sut.SetAsStopping(mediumTimeout)
-				go waitContainerStopAndFailAfterTimeout(context.Background(), stoppedChan, sut, mediumTimeout, mediumTimeout, test.ignoreKill)
+			// Then
+			waitOnContainerTimeout(sut, shortTimeout, mediumTimeout, sleepProcess)
+		})
 
-				// When
-				sut.SetAsStopping(longTimeout)
+		It("should not update time if chronologically after", func() {
+			// Given
+			containerIgnoreSignalCmdrunnerMock(sleepProcess, runner)
+			sut.SetAsStopping()
+			go runtime.StopLoopForContainer(sut)
 
-				// Then
-				test.verifyCorrectlyStopped(sut, sleepProcess, <-stoppedChan)
-			})
-			It("should handle many updates if "+test.name, func() {
-				// Given
-				stoppedChan := make(chan error, 1)
-				sut.SetAsStopping(longTimeout)
-				go waitContainerStopAndFailAfterTimeout(context.Background(), stoppedChan, sut, longTimeout, longTimeout, test.ignoreKill)
+			// When
+			shortStopChan := stopTimeoutWithChannel(context.Background(), sut, shortTimeout)
 
-				// When
-				for i := 0; i < 5; i++ {
-					go sut.SetAsStopping(int64(rand.Intn(10)))
-				}
+			// Then
+			waitOnContainerTimeout(sut, mediumTimeout, longTimeout, sleepProcess)
+			<-shortStopChan
+		})
+		It("should handle many updates", func() {
+			// Given
+			containerIgnoreSignalCmdrunnerMock(sleepProcess, runner)
+			sut.SetAsStopping()
+			go runtime.StopLoopForContainer(sut)
+			// very long timeout
+			stoppedChan := stopTimeoutWithChannel(context.Background(), sut, longTimeout*10)
 
-				// Then
-				test.verifyCorrectlyStopped(sut, sleepProcess, <-stoppedChan)
-			})
-		}
+			// When
+			for i := 0; i < 10; i++ {
+				go sut.WaitOnStopTimeout(context.Background(), int64(rand.Intn(100)+20))
+				time.Sleep(time.Second)
+			}
+			sut.WaitOnStopTimeout(context.Background(), mediumTimeout)
+
+			// Then
+			<-stoppedChan
+			verifyContainerStopped(sut, sleepProcess)
+		})
+		It("should handle context timeout", func() {
+			// Given
+			ctx, cancel := context.WithCancel(context.Background())
+			stoppedChan := stopTimeoutWithChannel(ctx, sut, shortTimeout)
+
+			// When
+			cancel()
+
+			// Then
+			// unconditionally expect the container was not stopped
+			<-stoppedChan
+			verifyContainerNotStopped(sut)
+		})
 	})
 	Context("TruncateAndReadFile", func() {
 		tests := []struct {
@@ -183,23 +218,44 @@ var _ = t.Describe("Oci", func() {
 	})
 })
 
-func waitContainerStopAndFailAfterTimeout(ctx context.Context,
-	stoppedChan chan error,
-	sut *oci.Container,
-	waitContainerStopTimeout int64,
-	failAfterTimeout int64,
-	ignoreKill bool,
-) {
-	select {
-	case stoppedChan <- oci.WaitContainerStop(ctx, sut, inSeconds(waitContainerStopTimeout), ignoreKill):
-	case <-time.After(inSeconds(failAfterTimeout)):
-		stoppedChan <- fmt.Errorf("%d seconds passed, container kill should have been recognized", failAfterTimeout)
-	}
-	close(stoppedChan)
+func containerIgnoreSignalCmdrunnerMock(sleepProcess *exec.Cmd, runner *runnerMock.MockCommandRunner) {
+	gomock.InOrder(
+		runner.EXPECT().Command(gomock.Any(), gomock.Any()).DoAndReturn(
+			func(_ string, _ ...string) interface{} {
+				return exec.Command("/bin/true")
+			},
+		),
+		runner.EXPECT().Command(gomock.Any(), gomock.Any()).DoAndReturn(
+			func(_ string, _ ...string) interface{} {
+				Expect(oci.Kill(sleepProcess.Process.Pid)).To(BeNil())
+				waitForKillToComplete(sleepProcess)
+				return exec.Command("/bin/true")
+			},
+		),
+	)
 }
 
-func verifyContainerStopped(sut *oci.Container, sleepProcess *exec.Cmd, waitError error) {
-	Expect(waitError).To(BeNil())
+func waitOnContainerTimeout(sut *oci.Container, stopTimeout, waitTimeout int64, sleepProcess *exec.Cmd) {
+	stoppedChan := stopTimeoutWithChannel(context.Background(), sut, stopTimeout)
+
+	select {
+	case <-stoppedChan:
+	case <-time.After(time.Second * time.Duration(waitTimeout)):
+		Fail("did not timeout quickly enough")
+	}
+	verifyContainerStopped(sut, sleepProcess)
+}
+
+func stopTimeoutWithChannel(ctx context.Context, sut *oci.Container, timeout int64) chan struct{} {
+	stoppedChan := make(chan struct{}, 1)
+	go func() {
+		sut.WaitOnStopTimeout(ctx, timeout)
+		close(stoppedChan)
+	}()
+	return stoppedChan
+}
+
+func verifyContainerStopped(sut *oci.Container, sleepProcess *exec.Cmd) {
 	waitForKillToComplete(sleepProcess)
 	pid, err := sut.Pid()
 	Expect(pid).To(Equal(0))
@@ -213,8 +269,7 @@ func waitForKillToComplete(sleepProcess *exec.Cmd) {
 	time.Sleep(inSeconds(shortTimeout))
 }
 
-func verifyContainerNotStopped(sut *oci.Container, _ *exec.Cmd, waitError error) {
-	Expect(waitError).NotTo(BeNil())
+func verifyContainerNotStopped(sut *oci.Container) {
 	pid, err := sut.Pid()
 	Expect(pid).NotTo(Equal(0))
 	Expect(err).To(BeNil())
