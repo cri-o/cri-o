b7aecfc2e9a8d3186ed700d0718c9c6226179e8d
Peter Hunt <pehunt@redhat.com>
2023-08-03 16:18:25 -0400

    build-static: misc fixes needed for 1.25.4 generation
    Signed-off-by: Peter Hunt <pehunt@redhat.com>

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -2,7 +2,7 @@ let
   json = builtins.fromJSON (builtins.readFile ./nixpkgs.json);
   nixpkgs = import (builtins.fetchTarball {
     name = "nixos-unstable";
-    url = "${json.url}/archive/${json.rev}.tar.gz";
+    url = "${json.url}/tarball/${json.rev}";
     inherit (json) sha256;
   });
 in nixpkgs
