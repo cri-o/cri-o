b83f610d403c072d9de2fb0d3eabcfe13c77a4bd
Sohan Kunkerkar <sohank2602@gmail.com>
2023-08-09 17:17:35 -0400

    internal/*: use libcontainer API for stats collection
    Signed-off-by: Sohan Kunkerkar <sohank2602@gmail.com>
    Co-authored-by: Peter Hunt <pehunt@redhat.com>

--- internal/config/cgmgr/stats_linux.go
+++ internal/config/cgmgr/stats_linux.go
@@ -14,67 +14,119 @@ import (
 	"github.com/cri-o/cri-o/internal/config/node"
 	libctrcgroups "github.com/opencontainers/runc/libcontainer/cgroups"
 	"github.com/sirupsen/logrus"
-	types "k8s.io/cri-api/pkg/apis/runtime/v1"
 )
 
-func populateSandboxCgroupStatsFromPath(cgroupPath string, stats *types.PodSandboxStats) error {
-	cgroupStats, err := cgroupStatsFromPath(cgroupPath)
-	if err != nil {
-		return err
-	}
-	systemNano := time.Now().UnixNano()
-	stats.Linux.Cpu = createCPUStats(systemNano, cgroupStats)
-	stats.Linux.Process = createProcessUsage(systemNano, cgroupStats)
-	stats.Linux.Memory, err = createMemoryStats(systemNano, cgroupStats, cgroupPath)
-	return err
+// CgroupStats is a structure used to augment libctr.Stats object, because
+// it does not have all of the information required for all of the stats we're
+// interested in.
+type CgroupStats struct {
+	MostStats     *libctrcgroups.Stats
+	OtherMemStats *CgroupMemoryStats
+	SystemNano    int64
 }
 
-func populateContainerCgroupStatsFromPath(cgroupPath string, stats *types.ContainerStats) error {
-	// checks cgroup just for the container, not the entire pod
-	cgroupStats, err := cgroupStatsFromPath(cgroupPath)
-	if err != nil {
-		return err
-	}
-	systemNano := time.Now().UnixNano()
-	stats.Cpu = createCPUStats(systemNano, cgroupStats)
-	stats.Memory, err = createMemoryStats(systemNano, cgroupStats, cgroupPath)
-	return err
+// CgroupMemoryStats is an object to hold the memory stats from the memory.stat file
+// of the given cgroup.
+type CgroupMemoryStats struct {
+	Rss            uint64
+	PgFault        uint64
+	PgMajFault     uint64
+	WorkingSet     uint64
+	AvailableBytes uint64
 }
 
-func cgroupStatsFromPath(cgroupPath string) (*libctrcgroups.Stats, error) {
+func cgroupStatsFromPath(cgroupPath string) (*CgroupStats, error) {
 	cg, err := cgroups.Load(cgroupPath)
 	if err != nil {
 		return nil, fmt.Errorf("unable to load cgroup at %s: %w", cgroupPath, err)
 	}
 
-	return cg.Stat()
-}
-
-func createCPUStats(systemNano int64, cgroupStats *libctrcgroups.Stats) *types.CpuUsage {
-	return &types.CpuUsage{
-		Timestamp:            systemNano,
-		UsageCoreNanoSeconds: &types.UInt64Value{Value: cgroupStats.CpuStats.CpuUsage.TotalUsage},
+	cgroupStats, err := cg.Stat()
+	if err != nil {
+		return nil, fmt.Errorf("unable to get cgroup stats: %w", err)
 	}
-}
 
-func createMemoryStats(systemNano int64, cgroupStats *libctrcgroups.Stats, cgroupPath string) (*types.MemoryUsage, error) {
 	memUsage := cgroupStats.MemoryStats.Usage.Usage
+
+	memStats, err := memoryStatsGivenPath(cgroupPath, memUsage)
+	if err != nil {
+		return nil, fmt.Errorf("unable to update with memory.stat info: %w", err)
+	}
+
 	memLimit := MemLimitGivenSystem(cgroupStats.MemoryStats.Usage.Limit)
+	memStats.AvailableBytes = memLimit - memUsage
 
-	memory := &types.MemoryUsage{
-		Timestamp:       systemNano,
-		WorkingSetBytes: &types.UInt64Value{},
-		RssBytes:        &types.UInt64Value{},
-		PageFaults:      &types.UInt64Value{},
-		MajorPageFaults: &types.UInt64Value{},
-		UsageBytes:      &types.UInt64Value{Value: memUsage},
-		AvailableBytes:  &types.UInt64Value{Value: memUsage - memLimit},
+	return &CgroupStats{
+		MostStats:     cgroupStats,
+		OtherMemStats: memStats,
+		SystemNano:    time.Now().UnixNano(),
+	}, nil
+}
+
+// memoryStatsGivenPath updates the CgroupMemoryStats object with info
+// from cgroup's memory.stat. Returns an error if the file does not exists,
+// or not parsable.
+func memoryStatsGivenPath(path string, usage uint64) (*CgroupMemoryStats, error) {
+	const memoryStatFile = "memory.stat"
+	var memoryStatPath, inactiveFileSearchString string
+	if !node.CgroupIsV2() {
+		memoryStatPath = filepath.Join(cgroupMemoryPathV1, path, memoryStatFile)
+		inactiveFileSearchString = "total_inactive_file "
+	} else {
+		memoryStatPath = filepath.Join(cgroupMemoryPathV2, path, memoryStatFile)
+		inactiveFileSearchString = "inactive_file "
+	}
+	return MemoryStatsFromFile(memoryStatPath, inactiveFileSearchString, usage)
+}
+
+func MemoryStatsFromFile(memoryStatPath, inactiveFileSearchString string, usage uint64) (*CgroupMemoryStats, error) {
+	var totalInactive uint64
+	f, err := os.Open(memoryStatPath)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	memStat := &CgroupMemoryStats{}
+
+	toUpdate := []struct {
+		prefix string
+		field  *uint64
+	}{
+		{inactiveFileSearchString, &totalInactive},
+		{"rss ", &memStat.Rss},
+		{"pgfault ", &memStat.PgFault},
+		{"pgmajfault ", &memStat.PgMajFault},
 	}
 
-	if err := updateWithMemoryStats(cgroupPath, memory, memUsage); err != nil {
-		return memory, fmt.Errorf("unable to update with memory.stat info: %w", err)
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		for _, field := range toUpdate {
+			if !strings.HasPrefix(scanner.Text(), field.prefix) {
+				continue
+			}
+			val, err := strconv.ParseUint(
+				strings.TrimPrefix(scanner.Text(), field.prefix), 10, 64,
+			)
+			if err != nil {
+				return nil, fmt.Errorf("unable to parse %s: %w", field.prefix, err)
+			}
+			*field.field = val
+		}
 	}
-	return memory, nil
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+	if usage > totalInactive {
+		memStat.WorkingSet = usage - totalInactive
+	} else {
+		logrus.Warnf(
+			"Unable to account working set stats: total_inactive_file (%d) > memory usage (%d)",
+			totalInactive, usage,
+		)
+	}
+
+	return memStat, nil
 }
 
 // MemLimitGivenSystem limit returns the memory limit for a given cgroup
@@ -90,80 +142,11 @@ func MemLimitGivenSystem(cgroupLimit uint64) uint64 {
 	// conversion to uint64 needed to build on 32-bit
 	// but lint complains about unnecessary conversion
 	// see: pr#2409
-	physicalLimit := uint64(si.Totalram) //nolint:unconvert
-	if cgroupLimit > physicalLimit {
-		return physicalLimit
+	physicalMemory := uint64(si.Totalram)
+
+	// If the cgroup limit exceeds the available physical memory, use the physical memory as the limit
+	if cgroupLimit > physicalMemory {
+		return physicalMemory
 	}
 	return cgroupLimit
-}
-
-// updateWithMemoryStats updates the ContainerStats object with info
-// from cgroup's memory.stat. Returns an error if the file does not exists,
-// or not parsable.
-func updateWithMemoryStats(path string, memory *types.MemoryUsage, usage uint64) error {
-	const memoryStatFile = "memory.stat"
-	var memoryStatPath, inactiveFileSearchString string
-	if !node.CgroupIsV2() {
-		memoryStatPath = filepath.Join(cgroupMemoryPathV1, path, memoryStatFile)
-		inactiveFileSearchString = "total_inactive_file "
-	} else {
-		memoryStatPath = filepath.Join(cgroupMemoryPathV2, path, memoryStatFile)
-		inactiveFileSearchString = "inactive_file "
-	}
-	return UpdateWithMemoryStatsFromFile(memoryStatPath, inactiveFileSearchString, memory, usage)
-}
-
-func UpdateWithMemoryStatsFromFile(memoryStatPath, inactiveFileSearchString string, memory *types.MemoryUsage, usage uint64) error {
-	var totalInactive uint64
-	f, err := os.Open(memoryStatPath)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	toUpdate := []struct {
-		prefix string
-		field  *uint64
-	}{
-		{inactiveFileSearchString, &totalInactive},
-		{"rss ", &memory.RssBytes.Value},
-		{"pgfault ", &memory.PageFaults.Value},
-		{"pgmajfault ", &memory.MajorPageFaults.Value},
-	}
-
-	scanner := bufio.NewScanner(f)
-	for scanner.Scan() {
-		for _, field := range toUpdate {
-			if !strings.HasPrefix(scanner.Text(), field.prefix) {
-				continue
-			}
-			val, err := strconv.ParseUint(
-				strings.TrimPrefix(scanner.Text(), field.prefix), 10, 64,
-			)
-			if err != nil {
-				return fmt.Errorf("unable to parse %s: %w", field.prefix, err)
-			}
-			*field.field = val
-		}
-	}
-	if err := scanner.Err(); err != nil {
-		return err
-	}
-	if usage > totalInactive {
-		memory.WorkingSetBytes.Value = usage - totalInactive
-	} else {
-		logrus.Warnf(
-			"Unable to account working set stats: total_inactive_file (%d) > memory usage (%d)",
-			totalInactive, usage,
-		)
-	}
-
-	return nil
-}
-
-func createProcessUsage(systemNano int64, cgroupStats *libctrcgroups.Stats) *types.ProcessUsage {
-	return &types.ProcessUsage{
-		Timestamp:    systemNano,
-		ProcessCount: &types.UInt64Value{Value: cgroupStats.PidsStats.Current},
-	}
 }
