d2fa125a4b8be669af52b2f148e5401ad9c37a99
Sascha Grunert <sgrunert@redhat.com>
2023-08-15 12:35:10 +0200

    Update nixpkgs and use overlay
    - Run `make nixpkgs` to update the dependencies
    - Reduce duplication by using `overlay.nix`
    - Pushed manually to cachix to refresh the build cache
    
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

--- nix/default-arm64.nix
+++ nix/default-arm64.nix
@@ -1,26 +1,7 @@
-let
-  static = import ./static.nix;
-
-  pkgs = (import ./nixpkgs.nix {
-    crossSystem = {
-      config = "aarch64-unknown-linux-gnu";
-    };
-    config = {
-      packageOverrides = pkg: {
-        gpgme = (static pkg.gpgme);
-        libassuan = (static pkg.libassuan);
-        libgpgerror = (static pkg.libgpgerror);
-        libseccomp = (static pkg.libseccomp);
-        gnupg = pkg.gnupg.override {
-          libusb1 = null;
-          pcsclite = null;
-          enableMinimal = true;
-          guiSupport = false;
-        };
-      };
-    };
-  });
-
-  self = import ./derivation.nix { inherit pkgs; };
-in
-self
+(import ./nixpkgs.nix {
+  crossSystem = {
+    config = "aarch64-unknown-linux-gnu";
+  };
+  overlays = [ (import ./overlay.nix) ];
+}).callPackage ./derivation.nix
+{ }
