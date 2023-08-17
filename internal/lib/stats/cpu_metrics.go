57d4a124b619cfa2ad61f8a9eb085b4e13e02ca2
Sohan Kunkerkar <sohank2602@gmail.com>
2023-08-17 16:44:04 -0400

    internal/lib/stats: add metrics implementation
    Signed-off-by: Sohan Kunkerkar <sohank2602@gmail.com>

---
+++ internal/lib/stats/cpu_metrics.go
@@ -0,0 +1,115 @@
+package statsserver
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/cri-o/cri-o/internal/lib/sandbox"
+	"github.com/opencontainers/runc/libcontainer/cgroups"
+	types "k8s.io/cri-api/pkg/apis/runtime/v1"
+)
+
+var (
+	cpuMetrics []ContainerStats = []ContainerStats{
+		{
+			desc: &types.MetricDescriptor{
+				Name:      "container_cpu_user_seconds_total", // stats.CpuStats.CpuUsage.UsageInUsermode (converted from nano)
+				Help:      "Cumulative user cpu time consumed in seconds.",
+				LabelKeys: baseLabelKeys,
+			},
+			valueFunc: func(stats interface{}) metricValues {
+				cpu := stats.(*cgroups.CpuStats)
+				return metricValues{{
+					value:      cpu.CpuUsage.UsageInUsermode / uint64(time.Second),
+					metricType: types.MetricType_COUNTER,
+				}}
+			},
+		}, {
+			desc: &types.MetricDescriptor{
+				Name:      "container_cpu_system_seconds_total", // stats.CpuStats.CpuUsage.UsageInKernelmode (converted from nano)
+				Help:      "Cumulative system cpu time consumed in seconds.",
+				LabelKeys: baseLabelKeys,
+			},
+			valueFunc: func(stats interface{}) metricValues {
+				cpu := stats.(*cgroups.CpuStats)
+				return metricValues{{
+					value:      cpu.CpuUsage.UsageInKernelmode / uint64(time.Second),
+					metricType: types.MetricType_COUNTER,
+				}}
+			},
+		}, {
+			desc: &types.MetricDescriptor{
+				Name:      "container_cpu_usage_seconds_total",
+				Help:      "Cumulative cpu time consumed in seconds.",
+				LabelKeys: append(baseLabelKeys, "cpu"),
+			},
+			valueFunc: func(stats interface{}) metricValues {
+				cpu := stats.(*cgroups.CpuStats)
+				if len(cpu.CpuUsage.PercpuUsage) == 0 {
+					if cpu.CpuUsage.TotalUsage > 0 {
+						return metricValues{{
+							value:      cpu.CpuUsage.TotalUsage / uint64(time.Second),
+							labels:     []string{"total"},
+							metricType: types.MetricType_COUNTER,
+						}}
+					}
+				}
+				metricValues := make(metricValues, 0, len(cpu.CpuUsage.PercpuUsage))
+				for i, value := range cpu.CpuUsage.PercpuUsage {
+					if value > 0 {
+						metricValues = append(metricValues, metricValue{
+							value:      value / uint64(time.Second),
+							labels:     []string{fmt.Sprintf("cpu%02d", i)},
+							metricType: types.MetricType_COUNTER,
+						})
+					}
+				}
+				return metricValues
+			},
+		}, {
+			desc: &types.MetricDescriptor{
+				Name:      "container_cpu_cfs_periods_total",
+				Help:      "Number of elapsed enforcement period intervals.",
+				LabelKeys: baseLabelKeys,
+			},
+			valueFunc: func(stats interface{}) metricValues {
+				cpu := stats.(*cgroups.CpuStats)
+				return metricValues{{
+					value:      cpu.ThrottlingData.Periods,
+					metricType: types.MetricType_COUNTER,
+				}}
+			},
+		}, {
+			desc: &types.MetricDescriptor{
+				Name:      "container_cpu_cfs_throttled_periods_total", // stats.CpuStats.ThrottlingData.ThrottledPeriods
+				Help:      "Number of throttled period intervals.",
+				LabelKeys: baseLabelKeys,
+			},
+			valueFunc: func(stats interface{}) metricValues {
+				cpu := stats.(*cgroups.CpuStats)
+				return metricValues{{
+					value:      cpu.ThrottlingData.ThrottledPeriods,
+					metricType: types.MetricType_COUNTER,
+				}}
+			},
+		}, {
+			desc: &types.MetricDescriptor{
+				Name:      "container_cpu_cfs_throttled_seconds_total", // stats.CpuStats.ThrottlingData.ThrottledTime (converted from nano)
+				Help:      "Total time duration the container has been throttled.",
+				LabelKeys: baseLabelKeys,
+			},
+			valueFunc: func(stats interface{}) metricValues {
+				cpu := stats.(*cgroups.CpuStats)
+				return metricValues{{
+					value:      cpu.ThrottlingData.ThrottledTime / uint64(time.Second),
+					metricType: types.MetricType_COUNTER,
+				}}
+			},
+		},
+	}
+)
+
+func GenerateSandboxCPUMetrics(sb *sandbox.Sandbox, sm *SandboxMetrics) []*types.Metric {
+	var c *cgroups.CpuStats
+	return ComputeSandboxMetrics(sb, c, cpuMetrics, "cpu", sm)
+}
---
+++ internal/lib/stats/memory_metrics.go
@@ -0,0 +1,193 @@
+package statsserver
+
+import (
+	"github.com/cri-o/cri-o/internal/lib/sandbox"
+	"github.com/opencontainers/runc/libcontainer/cgroups"
+	types "k8s.io/cri-api/pkg/apis/runtime/v1"
+)
+
+var (
+	memoryMetrics []ContainerStats = []ContainerStats{
+		{
+			desc: &types.MetricDescriptor{
+				Name:      "container_memory_cache",
+				Help:      "Number of bytes of page cache memory.",
+				LabelKeys: baseLabelKeys,
+			},
+			valueFunc: func(stats interface{}) metricValues {
+				mem := stats.(*cgroups.MemoryStats)
+				return metricValues{{value: mem.Cache, metricType: types.MetricType_GAUGE}}
+			},
+		}, {
+			desc: &types.MetricDescriptor{
+				Name:      "container_memory_rss",
+				Help:      "Size of RSS in bytes.",
+				LabelKeys: baseLabelKeys,
+			},
+			valueFunc: func(stats interface{}) metricValues {
+				mem := stats.(*cgroups.MemoryStats)
+				var value uint64
+				if cgroups.IsCgroup2UnifiedMode() {
+					value = mem.Stats["anon"]
+				} else if mem.UseHierarchy {
+					value = mem.Stats["total_rss"]
+				} else {
+					value = mem.Stats["rss"]
+				}
+				return metricValues{{value: value, metricType: types.MetricType_GAUGE}}
+			},
+		}, {
+			desc: &types.MetricDescriptor{
+				Name:      "container_memory_kernel_usage",
+				Help:      "Size of kernel memory allocated in bytes.",
+				LabelKeys: baseLabelKeys,
+			},
+			valueFunc: func(stats interface{}) metricValues {
+				mem := stats.(*cgroups.MemoryStats)
+				return metricValues{{value: mem.KernelUsage.Usage, metricType: types.MetricType_GAUGE}}
+			},
+		},
+		{
+			desc: &types.MetricDescriptor{
+				Name:      "container_memory_mapped_file",
+				Help:      "Size of memory mapped files in bytes.",
+				LabelKeys: baseLabelKeys,
+			},
+			valueFunc: func(stats interface{}) metricValues {
+				mem := stats.(*cgroups.MemoryStats)
+				var value uint64
+				if cgroups.IsCgroup2UnifiedMode() {
+					value = mem.Stats["file_mapped"]
+				} else if mem.UseHierarchy {
+					value = mem.Stats["total_mapped_file"]
+				} else {
+					value = mem.Stats["mapped_file"]
+				}
+				return metricValues{{value: value, metricType: types.MetricType_GAUGE}}
+			},
+		},
+		{
+			desc: &types.MetricDescriptor{
+				Name:      "container_memory_swap",
+				Help:      "Container swap usage in bytes.",
+				LabelKeys: baseLabelKeys,
+			},
+			valueFunc: func(stats interface{}) metricValues {
+				mem := stats.(*cgroups.MemoryStats)
+				return metricValues{{value: mem.SwapUsage.Usage, metricType: types.MetricType_GAUGE}}
+			},
+		},
+		{
+			desc: &types.MetricDescriptor{
+				Name:      "container_memory_failcnt",
+				Help:      "Number of memory usage hits limits",
+				LabelKeys: baseLabelKeys,
+			},
+			valueFunc: func(stats interface{}) metricValues {
+				mem := stats.(*cgroups.MemoryStats)
+				return metricValues{{value: mem.SwapUsage.Failcnt, metricType: types.MetricType_COUNTER}}
+			},
+		},
+		{
+			desc: &types.MetricDescriptor{
+				Name:      "container_memory_usage_bytes",
+				Help:      "Current memory usage in bytes, including all memory regardless of when it was accessed",
+				LabelKeys: baseLabelKeys,
+			},
+			valueFunc: func(stats interface{}) metricValues {
+				mem := stats.(*cgroups.MemoryStats)
+				return metricValues{{
+					value:      mem.Usage.Usage,
+					metricType: types.MetricType_GAUGE,
+				}}
+			},
+		},
+		{
+			desc: &types.MetricDescriptor{
+				Name:      "container_memory_max_usage_bytes",
+				Help:      "Maximum memory usage recorded in bytes",
+				LabelKeys: baseLabelKeys,
+			},
+			valueFunc: func(stats interface{}) metricValues {
+				mem := stats.(*cgroups.MemoryStats)
+				return metricValues{{
+					value:      mem.Usage.MaxUsage,
+					metricType: types.MetricType_GAUGE,
+				}}
+			},
+		},
+		{
+			desc: &types.MetricDescriptor{
+				Name:      "container_memory_working_set_bytes",
+				Help:      "Current working set in bytes.",
+				LabelKeys: baseLabelKeys,
+			},
+			valueFunc: func(stats interface{}) metricValues {
+				mem := stats.(*cgroups.MemoryStats)
+				var workingSet uint64
+				inactiveFileKeyName := "total_inactive_file"
+				if cgroups.IsCgroup2UnifiedMode() {
+					inactiveFileKeyName = "inactive_file"
+				}
+				workingSet = mem.Usage.Usage
+				if v, ok := mem.Stats[inactiveFileKeyName]; ok {
+					if workingSet < v {
+						workingSet = 0
+					} else {
+						workingSet -= v
+					}
+				}
+				return metricValues{{value: workingSet, metricType: types.MetricType_GAUGE}}
+			},
+		},
+		{
+			desc: &types.MetricDescriptor{
+				Name:      "container_memory_failures_total",
+				Help:      "Cumulative count of memory allocation failures.",
+				LabelKeys: append(baseLabelKeys, "failure_type", "scope"),
+			},
+			valueFunc: func(stats interface{}) metricValues {
+				mem := stats.(*cgroups.MemoryStats)
+				metrics := make([]metricValue, 0)
+				if v, ok := mem.Stats["pgfault"]; ok {
+					pgfaultMetrics := metricValues{
+						{
+							value:      v,
+							labels:     []string{"pgfault", "container"},
+							metricType: types.MetricType_COUNTER,
+						},
+						{
+							value:      v,
+							labels:     []string{"pgfault", "hierarchy"},
+							metricType: types.MetricType_COUNTER,
+						},
+					}
+
+					metrics = append(metrics, pgfaultMetrics...)
+				}
+
+				if v, ok := mem.Stats["pgmajfault"]; ok {
+					pgmajfaultMetrics := metricValues{
+						{
+							value:      v,
+							labels:     []string{"pgmajfault", "container"},
+							metricType: types.MetricType_COUNTER,
+						},
+						{
+							value:      v,
+							labels:     []string{"pgmajfault", "hierarchy"},
+							metricType: types.MetricType_COUNTER,
+						},
+					}
+					metrics = append(metrics, pgmajfaultMetrics...)
+				}
+				return metrics
+			},
+		},
+	}
+)
+
+func GenerateSandboxMemoryMetrics(sb *sandbox.Sandbox, sm *SandboxMetrics) []*types.Metric {
+	var c *cgroups.MemoryStats
+	return ComputeSandboxMetrics(sb, c, memoryMetrics, "memory", sm)
+}
