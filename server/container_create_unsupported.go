ecf0a630422def3659258f48ccf6ef19aa823efe
Miloslav Trmač <mitr@redhat.com>
2023-09-11 18:40:52 +0200

    Remove a function unused on non-Linux
    Should not change behavior.
    
    Signed-off-by: Miloslav Trmač <mitr@redhat.com>

--- server/container_create_unsupported.go
+++ server/container_create_unsupported.go
@@ -12,10 +12,6 @@ import (
 	"golang.org/x/net/context"
 )
 
-func findCgroupMountpoint(name string) error {
-	return fmt.Errorf("no cgroups on this platform")
-}
-
 func (s *Server) createSandboxContainer(ctx context.Context, ctr container.Container, sb *sandbox.Sandbox) (*oci.Container, error) {
 	return nil, fmt.Errorf("not implemented yet")
 }
