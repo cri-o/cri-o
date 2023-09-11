f674c3b906d5b33e46a37e82aab3e8fb3992033a
Miloslav Trmač <mitr@redhat.com>
2023-09-11 20:07:03 +0200

    Remove some completely unused stubs
    Signed-off-by: Miloslav Trmač <mitr@redhat.com>

--- internal/lib/sandbox/sandbox_unsupported.go
+++ internal/lib/sandbox/sandbox_unsupported.go
@@ -5,55 +5,7 @@ package sandbox
 
 import (
 	"context"
-	"fmt"
-	"os"
 )
-
-func (n *NetNs) Initialize() (*NetNs, error) {
-	return &NetNs{}, fmt.Errorf("netns is not implemented for this platform")
-}
-
-func (n *NetNs) Initialized(nsType string) bool {
-	return false
-}
-
-func getNetNs(path string) (*NetNs, error) {
-	return &NetNs{}, fmt.Errorf("netns is not implemented for this platform")
-}
-
-// NetNs handles data pertaining a network namespace
-// for non-linux this is a noop
-type NetNs struct {
-	symlink *os.File
-}
-
-func (n *NetNs) Get() *NetNs {
-	return n
-}
-
-func (n *NetNs) Path() string {
-	return ""
-}
-
-func (n *NetNs) SymlinkCreate(name string) error {
-	return nil
-}
-
-func (n *NetNs) symlinkRemove() error {
-	return nil
-}
-
-func (n *NetNs) Close() error {
-	return nil
-}
-
-func (n *NetNs) Remove() error {
-	return nil
-}
-
-func hostNetNsPath() (string, error) {
-	return "", fmt.Errorf("netns is not implemented for this platform")
-}
 
 // UnmountShm removes the shared memory mount for the sandbox and returns an
 // error if any failure occurs.
