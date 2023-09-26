e0750462ff4fdf0714992f6682e783aef8018af5
Miloslav Trmač <mitr@redhat.com>
2023-09-26 19:42:50 +0200

    Turn ListImages(filter) into ImageStatus
    That "filter" was never really much of a filter, we always only
    got one image.
    
    The current logic is, effectively, the only user which accepts
    that specific form of heuristically-interpreted syntax (because
    all other non-PauseImage users of internal/storage use ResolveNames),
    so removing that syntax would consolidate and clarify the semantics
    of internal/storage names.
    
    Kubelet never calls ListImages with a filter AFAIK, so this should
    make no difference in practice - and it means we get to remove a chunk
    of code and tests.
    
    We do need to update the tests for the @id filter syntax which
    is no longer recognized, though.
    
    Signed-off-by: Miloslav Trmač <mitr@redhat.com>

--- server/image_list_test.go
+++ server/image_list_test.go
@@ -48,12 +48,18 @@ var _ = t.Describe("ImageList", func() {
 
 		It("should succeed with filter", func() {
 			// Given
+			size := uint64(100)
 			gomock.InOrder(
-				imageServerMock.EXPECT().ListImages(
-					gomock.Any(), gomock.Any()).
-					Return([]storage.ImageResult{{ID: imageID}}, nil),
+				imageServerMock.EXPECT().ResolveNames(
+					gomock.Any(), "image").
+					Return([]string{"docker.io/library/image"}, nil),
+				imageServerMock.EXPECT().ImageStatus(
+					gomock.Any(), "docker.io/library/image").
+					Return(&storage.ImageResult{
+						ID:   "2a03a6059f21e150ae84b0973863609494aad70f0a80eaeb64bddd8d92465812",
+						User: "10", Size: &size,
+					}, nil),
 			)
-
 			// When
 			response, err := sut.ListImages(context.Background(),
 				&types.ListImagesRequest{Filter: &types.ImageFilter{
@@ -76,6 +82,28 @@ var _ = t.Describe("ImageList", func() {
 			// When
 			response, err := sut.ListImages(context.Background(),
 				&types.ListImagesRequest{})
+
+			// Then
+			Expect(err).NotTo(BeNil())
+			Expect(response).To(BeNil())
+		})
+
+		It("should fail with filter status error", func() {
+			// Given
+			gomock.InOrder(
+				imageServerMock.EXPECT().ResolveNames(
+					gomock.Any(), "image").
+					Return([]string{"docker.io/library/image"}, nil),
+				imageServerMock.EXPECT().ImageStatus(
+					gomock.Any(), "docker.io/library/image").
+					Return(nil, t.TestError),
+			)
+
+			// When
+			response, err := sut.ListImages(context.Background(),
+				&types.ListImagesRequest{Filter: &types.ImageFilter{
+					Image: &types.ImageSpec{Image: "image"},
+				}})
 
 			// Then
 			Expect(err).NotTo(BeNil())
