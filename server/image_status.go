1b0e82a225614dce4c5c3d1aab219bcf0891c8ee
Miloslav Trmač <mitr@redhat.com>
2023-09-26 19:27:29 +0200

    Split storageImageStatus from ImageStatus
    We will also call the "resolve and get status" code from ListImages.
    
    Should not change behavior.
    
    Signed-off-by: Miloslav Trmač <mitr@redhat.com>

--- server/image_status.go
+++ server/image_status.go
@@ -29,34 +29,11 @@ func (s *Server) ImageStatus(ctx context.Context, req *types.ImageStatusRequest)
 	}
 
 	log.Infof(ctx, "Checking image status: %s", image)
-	images, err := s.StorageImageServer().ResolveNames(s.config.SystemContext, image)
-	if err != nil {
-		return nil, err
-	}
-	var (
-		status  *pkgstorage.ImageResult
-		lastErr error
-	)
-	for _, image := range images {
-		status_, err := s.StorageImageServer().ImageStatus(s.config.SystemContext, image)
-		if err != nil {
-			if errors.Is(err, storage.ErrImageUnknown) {
-				log.Debugf(ctx, "Can't find %s", image)
-				continue
-			}
-			log.Warnf(ctx, "Error getting status from %s: %v", image, err)
-			lastErr = err
-			continue
-		}
-		status = status_
-		break
-	}
+	status, err := s.storageImageStatus(ctx, *img)
 	if status == nil {
-		if lastErr != nil {
-			return nil, lastErr
+		if err != nil {
+			return nil, err
 		}
-		// ResolveNames returns at least one value if it doesn't fail.
-		// So, if we got here, there was at least one ErrImageUnknown, and no other errors.
 		log.Infof(ctx, "Image %s not found", image)
 		return &types.ImageStatusResponse{}, nil
 	}
@@ -94,6 +71,43 @@ func (s *Server) ImageStatus(ctx context.Context, req *types.ImageStatusRequest)
 	resp.Image.Username = username
 	log.Infof(ctx, "Image status: %v", resp)
 	return resp, nil
+}
+
+// storageImageStatus calls ImageStatus for a k8s ImageSpec.
+// Returns (nil, nil) if image was not found.
+func (s *Server) storageImageStatus(ctx context.Context, spec types.ImageSpec) (*pkgstorage.ImageResult, error) {
+	images, err := s.StorageImageServer().ResolveNames(s.config.SystemContext, spec.Image)
+	if err != nil {
+		return nil, err
+	}
+	var (
+		status  *pkgstorage.ImageResult
+		lastErr error
+	)
+	for _, image := range images {
+		status_, err := s.StorageImageServer().ImageStatus(s.config.SystemContext, image)
+		if err != nil {
+			if errors.Is(err, storage.ErrImageUnknown) {
+				log.Debugf(ctx, "Can't find %s", image)
+				continue
+			}
+			log.Warnf(ctx, "Error getting status from %s: %v", image, err)
+			lastErr = err
+			continue
+		}
+		status = status_
+		break
+	}
+	if status == nil {
+		if lastErr != nil {
+			return nil, lastErr
+		}
+		// ResolveNames returns at least one value if it doesn't fail.
+		// So, if we got here, there was at least one ErrImageUnknown, and no other errors.
+		log.Infof(ctx, "Image %s not found", spec.Image)
+		return nil, nil
+	}
+	return status, nil
 }
 
 // getUserFromImage gets uid or user name of the image user.
