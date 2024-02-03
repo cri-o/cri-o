f491a6823183c38c932cc49fec6314808898324d
Doug Rabson <dfr@rabson.org>
2024-02-03 16:58:52 +0000

    server: move createContainerPlatform to container_create_linux.go
    Signed-off-by: Doug Rabson <dfr@rabson.org>

--- server/container_create_common.go
+++ server/container_create_common.go
@@ -49,24 +49,6 @@ const (
 	cgroupSysFsSystemdPath = "/sys/fs/cgroup/systemd"
 )
 
-// createContainerPlatform performs platform dependent intermediate steps before calling the container's oci.Runtime().CreateContainer()
-func (s *Server) createContainerPlatform(ctx context.Context, container *oci.Container, cgroupParent string, idMappings *idtools.IDMappings) error {
-	ctx, span := log.StartSpan(ctx)
-	defer span.End()
-	if idMappings != nil && !container.Spoofed() {
-		rootPair := idMappings.RootPair()
-		for _, path := range []string{container.BundlePath(), container.MountPoint()} {
-			if err := makeAccessible(path, rootPair.UID, rootPair.GID, false); err != nil {
-				return fmt.Errorf("cannot make %s accessible to %d:%d: %w", path, rootPair.UID, rootPair.GID, err)
-			}
-		}
-		if err := makeMountsAccessible(rootPair.UID, rootPair.GID, container.Spec().Mounts); err != nil {
-			return err
-		}
-	}
-	return s.Runtime().CreateContainer(ctx, container, cgroupParent, false)
-}
-
 // makeAccessible changes the path permission and each parent directory to have --x--x--x
 func makeAccessible(path string, uid, gid int, doChown bool) error {
 	if doChown {
