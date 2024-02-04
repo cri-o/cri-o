83e320fe3fe7653e2011212c6654a8345d28b857
Doug Rabson <dfr@rabson.org>
2024-02-04 11:33:32 +0000

    internal/factory/container: make SpecAddNamespaces platform-specific
    This allows us to set the network namespace of a FreeBSD container.
    
    Signed-off-by: Doug Rabson <dfr@rabson.org>

---
+++ internal/factory/container/namespaces_freebsd.go
@@ -0,0 +1,21 @@
+package container
+
+import (
+	"github.com/cri-o/cri-o/internal/config/nsmgr"
+	"github.com/cri-o/cri-o/internal/lib/sandbox"
+	oci "github.com/cri-o/cri-o/internal/oci"
+	"github.com/cri-o/cri-o/pkg/config"
+)
+
+func (c *container) SpecAddNamespaces(sb *sandbox.Sandbox, targetCtr *oci.Container, serverConfig *config.Config) error {
+	// Join the namespace paths for the pod sandbox container.
+	managedNamespaces := sb.NamespacePaths()
+
+	for _, ns := range managedNamespaces {
+		if ns.Type() == nsmgr.NETNS {
+			c.Spec().AddAnnotation("org.freebsd.parentJail", ns.Path())
+		}
+	}
+
+	return nil
+}
