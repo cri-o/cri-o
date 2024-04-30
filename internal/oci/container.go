9c157f868e117b472ff1725b16f68a22905d9e75
Krzysztof Wilczyński <kwilczynski@redhat.com>
2024-05-01 06:01:52 +0900

    Add debug log line to track newly started exec PIDs
    Signed-off-by: Krzysztof Wilczyński <kwilczynski@redhat.com>

--- internal/oci/container.go
+++ internal/oci/container.go
@@ -844,6 +844,8 @@ func (c *Container) RuntimePathForPlatform(r *runtimeOCI) string {
 func (c *Container) AddExecPID(pid int, shouldKill bool) error {
 	c.stopLock.Lock()
 	defer c.stopLock.Unlock()
+
+	logrus.Debugf("Starting to track exec PID %d for container %s (should kill = %t) ...", pid, c.ID(), shouldKill)
 	if c.stopping {
 		return errors.New("cannot register an exec PID: container is stopping")
 	}
