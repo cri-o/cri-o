9c40df268c797363bea665e7a9b0d78bc825d065
Peter Hunt <pehunt@redhat.com>
2024-06-14 10:03:15 -0400

    server: remove container after failed start
    The kubelet isn't going to try again to start a container, instead opting to recreate it.
    Thus, this container will get leaked (until the pod cgroup is deleted)
    
    This has impacts in disabling cpu load balancing on v2, as the container cgroup sticking around
    means the target cpus are in use, breaking the kernel's exclusivity rule
    
    Signed-off-by: Peter Hunt <pehunt@redhat.com>

--- server/container_start.go
+++ server/container_start.go
@@ -86,6 +86,9 @@ func (s *Server) StartContainer(ctx context.Context, req *types.StartContainerRe
 			if err := s.nri.stopContainer(ctx, sandbox, c); err != nil {
 				log.Warnf(ctx, "NRI stop failed for container %q: %v", c.ID(), err)
 			}
+			if err := s.removeContainerInPod(ctx, sandbox, c); err != nil {
+				log.Warnf(ctx, "Failed to delete container in runtime %s: %v", c.ID(), err)
+			}
 		}
 		if err := s.ContainerStateToDisk(ctx, c); err != nil {
 			log.Warnf(ctx, "Unable to write containers %s state to disk: %v", c.ID(), err)
