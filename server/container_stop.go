08180027b1ad7e717717583b5b4ea7c223afab2e
Sohan Kunkerkar <sohank2602@gmail.com>
2024-06-24 11:52:25 -0400

    oci: move oci-specific code into their respective runtime
    Signed-off-by: Sohan Kunkerkar <sohank2602@gmail.com>

--- server/container_stop.go
+++ server/container_stop.go
@@ -49,15 +49,6 @@ func (s *Server) stopContainer(ctx context.Context, ctr *oci.Container, timeout 
 		}
 	}
 
-	if ctr.StateNoLock().Status == oci.ContainerStatePaused {
-		if err := s.Runtime().UnpauseContainer(ctx, ctr); err != nil {
-			return fmt.Errorf("failed to stop container %s: %w", ctr.Name(), err)
-		}
-		if err := s.Runtime().UpdateContainerStatus(ctx, ctr); err != nil {
-			return fmt.Errorf("failed to update container status %s: %w", ctr.Name(), err)
-		}
-	}
-
 	if err := s.Runtime().StopContainer(ctx, ctr, timeout); err != nil {
 		return fmt.Errorf("failed to stop container %s: %w", ctr.ID(), err)
 	}
