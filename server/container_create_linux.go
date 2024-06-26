4534feb962b0049c73bb0ede80be4f243d086fc1
Doug Rabson <dfr@rabson.org>
2024-06-26 17:04:41 +0100

    server: factor out adding /dev/shm mount
    Signed-off-by: Doug Rabson <dfr@rabson.org>

--- server/container_create_linux.go
+++ server/container_create_linux.go
@@ -525,12 +525,7 @@ func (s *Server) createSandboxContainer(ctx context.Context, ctr ctrfactory.Cont
 		}
 	}
 
-	ctr.SpecAddMount(rspec.Mount{
-		Destination: "/dev/shm",
-		Type:        "bind",
-		Source:      sb.ShmPath(),
-		Options:     []string{"rw", "bind"},
-	})
+	addShmMount(ctr, sb)
 
 	options := []string{"rw"}
 	if readOnlyRootfs {
@@ -1380,4 +1375,13 @@ func addSysfsMounts(ctr ctrfactory.Container, containerConfig *types.ContainerCo
 			Options:     []string{"nosuid", "noexec", "nodev", "rw", "relatime", "rslave"},
 		})
 	}
+}
+
+func addShmMount(ctr ctrfactory.Container, sb *sandbox.Sandbox) {
+	ctr.SpecAddMount(rspec.Mount{
+		Destination: "/dev/shm",
+		Type:        "bind",
+		Source:      sb.ShmPath(),
+		Options:     []string{"rw", "bind"},
+	})
 }
