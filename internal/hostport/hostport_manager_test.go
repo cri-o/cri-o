75b73e6d32640754e509b8525174ab886cd84e98
Dan Winship <danwinship@redhat.com>
2024-11-03 12:47:24 -0500

    Remove hostport.PodPortMapping
    hostport.PodPortMapping is only used to aggregate the arguments to
    HostPortManager.Add() and .Remove(), but they could just as easily be
    passed separately (which will turn out to be useful later).
    
    (Also, the existence of the struct was hiding the fact that the
    toplevel code was not actually passing the Namespace field. Since this
    only ended up being used for an iptables comment anyway, I just
    removed it and simplified the code to work only with the pod name.)
    
    Signed-off-by: Dan Winship <danwinship@redhat.com>

--- internal/hostport/hostport_manager_test.go
+++ internal/hostport/hostport_manager_test.go
@@ -18,7 +18,6 @@ package hostport
 
 import (
 	"bytes"
-	"net"
 	"strings"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -30,98 +29,88 @@ import (
 )
 
 type testCase struct {
-	id      string
-	mapping *PodPortMapping
+	id           string
+	name         string
+	podIP        string
+	portMappings []*PortMapping
 }
 
 var testCasesV4 = []testCase{
 	{
-		id: "0855d5396cdc673af13203c9cc5c95367cad0133306ba4d74d1da6e2876ebe51",
-		mapping: &PodPortMapping{
-			Name:      "pod1",
-			Namespace: "ns1",
-			IP:        net.ParseIP("10.1.1.2"),
-			PortMappings: []*PortMapping{
-				{
-					HostPort:      8080,
-					ContainerPort: 80,
-					Protocol:      v1.ProtocolTCP,
-				},
-				{
-					HostPort:      8081,
-					ContainerPort: 81,
-					Protocol:      v1.ProtocolUDP,
-				},
-				{
-					HostPort:      8083,
-					ContainerPort: 83,
-					Protocol:      v1.ProtocolSCTP,
-				},
-				{
-					HostPort:      8084,
-					ContainerPort: 84,
-					Protocol:      v1.ProtocolTCP,
-					HostIP:        "127.0.0.1",
-				},
+		id:    "0855d5396cdc673af13203c9cc5c95367cad0133306ba4d74d1da6e2876ebe51",
+		name:  "pod1_ns1",
+		podIP: "10.1.1.2",
+		portMappings: []*PortMapping{
+			{
+				HostPort:      8080,
+				ContainerPort: 80,
+				Protocol:      v1.ProtocolTCP,
+			},
+			{
+				HostPort:      8081,
+				ContainerPort: 81,
+				Protocol:      v1.ProtocolUDP,
+			},
+			{
+				HostPort:      8083,
+				ContainerPort: 83,
+				Protocol:      v1.ProtocolSCTP,
+			},
+			{
+				HostPort:      8084,
+				ContainerPort: 84,
+				Protocol:      v1.ProtocolTCP,
+				HostIP:        "127.0.0.1",
 			},
 		},
 	},
 	{
-		id: "2da827da280ff31f6b257138f625d94b90472f614dee4d5f415d99b3e49a2c72",
-		mapping: &PodPortMapping{
-			Name:      "pod3",
-			Namespace: "ns1",
-			IP:        net.ParseIP("10.1.1.4"),
-			PortMappings: []*PortMapping{
-				{
-					HostPort:      8443,
-					ContainerPort: 443,
-					Protocol:      v1.ProtocolTCP,
-				},
+		id:    "2da827da280ff31f6b257138f625d94b90472f614dee4d5f415d99b3e49a2c72",
+		name:  "pod3_ns1",
+		podIP: "10.1.1.4",
+		portMappings: []*PortMapping{
+			{
+				HostPort:      8443,
+				ContainerPort: 443,
+				Protocol:      v1.ProtocolTCP,
 			},
 		},
 	},
 	{
 		// open same HostPort on different HostIPs
-		id: "f51d8a623d1d3d31d6552da3bc080a33ae57ef47daf34c7c5f7d4159d19849b7",
-		mapping: &PodPortMapping{
-			Name:      "pod5",
-			Namespace: "ns5",
-			IP:        net.ParseIP("10.1.1.5"),
-			PortMappings: []*PortMapping{
-				{
-					HostPort:      8888,
-					ContainerPort: 443,
-					Protocol:      v1.ProtocolTCP,
-					HostIP:        "127.0.0.2",
-				},
-				{
-					HostPort:      8888,
-					ContainerPort: 443,
-					Protocol:      v1.ProtocolTCP,
-					HostIP:        "127.0.0.1",
-				},
+		id:    "f51d8a623d1d3d31d6552da3bc080a33ae57ef47daf34c7c5f7d4159d19849b7",
+		name:  "pod5_ns5",
+		podIP: "10.1.1.5",
+		portMappings: []*PortMapping{
+			{
+				HostPort:      8888,
+				ContainerPort: 443,
+				Protocol:      v1.ProtocolTCP,
+				HostIP:        "127.0.0.2",
+			},
+			{
+				HostPort:      8888,
+				ContainerPort: 443,
+				Protocol:      v1.ProtocolTCP,
+				HostIP:        "127.0.0.1",
 			},
 		},
 	},
 	{
 		// open same HostPort with different protocols
-		id: "aa6b20dc29d075700fa53f623a00fe4ec8e9042d48f5964e601a1f3257ddc518",
-		mapping: &PodPortMapping{
-			Name:      "pod6",
-			Namespace: "ns1",
-			IP:        net.ParseIP("10.1.1.6"),
-			PortMappings: []*PortMapping{
-				{
-					HostPort:      9999,
-					ContainerPort: 443,
-					Protocol:      v1.ProtocolTCP,
-				},
-				{
-					HostPort:      9999,
-					ContainerPort: 443,
-					Protocol:      v1.ProtocolUDP,
-				},
+		id:    "aa6b20dc29d075700fa53f623a00fe4ec8e9042d48f5964e601a1f3257ddc518",
+		name:  "pod6_ns1",
+		podIP: "10.1.1.6",
+		portMappings: []*PortMapping{
+			{
+				HostPort:      9999,
+				ContainerPort: 443,
+				Protocol:      v1.ProtocolTCP,
+			},
+			{
+				HostPort:      9999,
+				ContainerPort: 443,
+				Protocol:      v1.ProtocolUDP,
 			},
 		},
 	},
@@ -130,49 +119,43 @@ var testCasesV4 = []testCase{
 var testCasesV6 = []testCase{
 	{
 		// Same id and mappings as testCasesV4[0] (but with an IPv6 HostIP)
-		id: "0855d5396cdc673af13203c9cc5c95367cad0133306ba4d74d1da6e2876ebe51",
-		mapping: &PodPortMapping{
-			Name:      "pod1",
-			Namespace: "ns1",
-			IP:        net.ParseIP("2001:beef::2"),
-			PortMappings: []*PortMapping{
-				{
-					HostPort:      8080,
-					ContainerPort: 80,
-					Protocol:      v1.ProtocolTCP,
-				},
-				{
-					HostPort:      8081,
-					ContainerPort: 81,
-					Protocol:      v1.ProtocolUDP,
-				},
-				{
-					HostPort:      8083,
-					ContainerPort: 83,
-					Protocol:      v1.ProtocolSCTP,
-				},
-				{
-					HostPort:      8084,
-					ContainerPort: 84,
-					Protocol:      v1.ProtocolTCP,
-					HostIP:        "::1",
-				},
+		id:    "0855d5396cdc673af13203c9cc5c95367cad0133306ba4d74d1da6e2876ebe51",
+		name:  "pod1_ns1",
+		podIP: "2001:beef::2",
+		portMappings: []*PortMapping{
+			{
+				HostPort:      8080,
+				ContainerPort: 80,
+				Protocol:      v1.ProtocolTCP,
+			},
+			{
+				HostPort:      8081,
+				ContainerPort: 81,
+				Protocol:      v1.ProtocolUDP,
+			},
+			{
+				HostPort:      8083,
+				ContainerPort: 83,
+				Protocol:      v1.ProtocolSCTP,
+			},
+			{
+				HostPort:      8084,
+				ContainerPort: 84,
+				Protocol:      v1.ProtocolTCP,
+				HostIP:        "::1",
 			},
 		},
 	},
 	{
 		// Same id and mappings as testCasesV4[1]
-		id: "2da827da280ff31f6b257138f625d94b90472f614dee4d5f415d99b3e49a2c72",
-		mapping: &PodPortMapping{
-			Name:      "pod3",
-			Namespace: "ns1",
-			IP:        net.ParseIP("2001:beef::4"),
-			PortMappings: []*PortMapping{
-				{
-					HostPort:      8443,
-					ContainerPort: 443,
-					Protocol:      v1.ProtocolTCP,
-				},
+		id:    "2da827da280ff31f6b257138f625d94b90472f614dee4d5f415d99b3e49a2c72",
+		name:  "pod3_ns1",
+		podIP: "2001:beef::4",
+		portMappings: []*PortMapping{
+			{
+				HostPort:      8443,
+				ContainerPort: 443,
+				Protocol:      v1.ProtocolTCP,
 			},
 		},
 	},
@@ -284,7 +267,7 @@ var _ = t.Describe("HostPortManager", func() {
 
 		// Add Hostports
 		for _, tc := range testCasesV4 {
-			err := manager.Add(tc.id, tc.mapping)
+			err := manager.Add(tc.id, tc.name, tc.podIP, tc.portMappings)
 			Expect(err).NotTo(HaveOccurred())
 		}
 
@@ -293,7 +276,7 @@ var _ = t.Describe("HostPortManager", func() {
 
 		// Remove all added hostports
 		for _, tc := range testCasesV4 {
-			err := manager.Remove(tc.id, tc.mapping)
+			err := manager.Remove(tc.id, tc.portMappings)
 			Expect(err).NotTo(HaveOccurred())
 		}
 
@@ -310,7 +293,7 @@ var _ = t.Describe("HostPortManager", func() {
 
 		// Add Hostports
 		for _, tc := range testCasesV6 {
-			err := manager.Add(tc.id, tc.mapping)
+			err := manager.Add(tc.id, tc.name, tc.podIP, tc.portMappings)
 			Expect(err).NotTo(HaveOccurred())
 		}
 
@@ -319,7 +302,7 @@ var _ = t.Describe("HostPortManager", func() {
 
 		// Remove all added hostports
 		for _, tc := range testCasesV6 {
-			err := manager.Remove(tc.id, tc.mapping)
+			err := manager.Remove(tc.id, tc.portMappings)
 			Expect(err).NotTo(HaveOccurred())
 		}
 
