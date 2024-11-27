f8533fb9db5901fea3559f3ffccc569fe31eb077
Sascha Grunert <sgrunert@redhat.com>
2024-11-27 10:29:44 +0100

    Fix container restore lint report
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

--- server/container_restore_test.go
+++ server/container_restore_test.go
@@ -8,8 +8,11 @@ import (
 	"github.com/containers/podman/v4/pkg/criu"
 	"github.com/containers/storage/pkg/archive"
 	"github.com/cri-o/cri-o/internal/oci"
+	"github.com/cri-o/cri-o/internal/storage"
+	"github.com/golang/mock/gomock"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	v1 "github.com/opencontainers/image-spec/specs-go/v1"
 	specs "github.com/opencontainers/runtime-spec/specs-go"
 	types "k8s.io/cri-api/pkg/apis/runtime/v1"
 )
@@ -39,6 +42,7 @@ var _ = t.Describe("ContainerRestore", func() {
 		It("should fail because archive does not exist", func() {
 			// Given
 			containerConfig := &types.ContainerConfig{
+				Metadata: &types.ContainerMetadata{Name: "name"},
 				Image: &types.ImageSpec{
 					Image: "does-not-exist.tar",
 				},
@@ -64,6 +68,7 @@ var _ = t.Describe("ContainerRestore", func() {
 			archive.Close()
 			defer os.RemoveAll("empty.tar")
 			containerConfig := &types.ContainerConfig{
+				Metadata: &types.ContainerMetadata{Name: "name"},
 				Image: &types.ImageSpec{
 					Image: "empty.tar",
 				},
@@ -86,6 +91,7 @@ var _ = t.Describe("ContainerRestore", func() {
 			Expect(err).To(BeNil())
 			defer os.RemoveAll("no.tar")
 			containerConfig := &types.ContainerConfig{
+				Metadata: &types.ContainerMetadata{Name: "name"},
 				Image: &types.ImageSpec{
 					Image: "no.tar",
 				},
@@ -120,6 +126,7 @@ var _ = t.Describe("ContainerRestore", func() {
 			_, err = io.Copy(outFile, input)
 			Expect(err).To(BeNil())
 			containerConfig := &types.ContainerConfig{
+				Metadata: &types.ContainerMetadata{Name: "name"},
 				Image: &types.ImageSpec{
 					Image: "archive.tar",
 				},
@@ -157,6 +164,7 @@ var _ = t.Describe("ContainerRestore", func() {
 			_, err = io.Copy(outFile, input)
 			Expect(err).To(BeNil())
 			containerConfig := &types.ContainerConfig{
+				Metadata: &types.ContainerMetadata{Name: "name"},
 				Image: &types.ImageSpec{
 					Image: "archive.tar",
 				},
@@ -170,7 +178,7 @@ var _ = t.Describe("ContainerRestore", func() {
 			)
 
 			// Then
-			Expect(err.Error()).To(ContainSubstring(`failed to read "io.kubernetes.cri-o.Metadata": unexpected end of JSON input`))
+			Expect(err.Error()).To(ContainSubstring(`failed to read "io.kubernetes.cri-o.Annotations": unexpected end of JSON input`))
 		})
 	})
 	t.Describe("ContainerRestore from archive into new pod", func() {
@@ -195,6 +203,7 @@ var _ = t.Describe("ContainerRestore", func() {
 			_, err = io.Copy(outFile, input)
 			Expect(err).To(BeNil())
 			containerConfig := &types.ContainerConfig{
+				Metadata: &types.ContainerMetadata{Name: "name"},
 				Image: &types.ImageSpec{
 					Image: "archive.tar",
 				},
@@ -240,6 +249,7 @@ var _ = t.Describe("ContainerRestore", func() {
 			_, err = io.Copy(outFile, input)
 			Expect(err).To(BeNil())
 			containerConfig := &types.ContainerConfig{
+				Metadata: &types.ContainerMetadata{Name: "name"},
 				Image: &types.ImageSpec{
 					Image: "archive.tar",
 				},
@@ -288,6 +298,7 @@ var _ = t.Describe("ContainerRestore", func() {
 			_, err = io.Copy(outFile, input)
 			Expect(err).To(BeNil())
 			containerConfig := &types.ContainerConfig{
+				Metadata: &types.ContainerMetadata{Name: "name"},
 				Image: &types.ImageSpec{
 					Image: "archive.tar",
 				},
@@ -303,6 +314,157 @@ var _ = t.Describe("ContainerRestore", func() {
 
 			// Then
 			Expect(err.Error()).To(Equal(`failed to read "io.kubernetes.cri-o.Annotations": unexpected end of JSON input`))
+		})
+	})
+	t.Describe("ContainerRestore from archive into new pod", func() {
+		It("should fail with 'PodSandboxId should not be empty'", func() {
+			// Given
+			addContainerAndSandbox()
+			testContainer.SetStateAndSpoofPid(&oci.ContainerState{
+				State: specs.State{Status: oci.ContainerStateRunning},
+			})
+
+			err := os.WriteFile(
+				"spec.dump",
+				[]byte(
+					`{"annotations":{"io.kubernetes.cri-o.Metadata"`+
+						`:"{\"name\":\"container-to-restore\"}",`+
+						`"io.kubernetes.cri-o.Annotations": "{\"name\":\"NAME\"}",`+
+						`"io.kubernetes.cri-o.Labels": "{\"io.kubernetes.container.name\":\"counter\"}"}}`),
+				0o644,
+			)
+			Expect(err).To(BeNil())
+			defer os.RemoveAll("spec.dump")
+			err = os.WriteFile("config.dump", []byte(`{"rootfsImageName": "image"}`), 0o644)
+			Expect(err).To(BeNil())
+			defer os.RemoveAll("config.dump")
+			outFile, err := os.Create("archive.tar")
+			Expect(err).To(BeNil())
+			defer outFile.Close()
+			input, err := archive.TarWithOptions(".", &archive.TarOptions{
+				Compression:      archive.Uncompressed,
+				IncludeSourceDir: true,
+				IncludeFiles:     []string{"spec.dump", "config.dump"},
+			})
+			Expect(err).To(BeNil())
+			defer os.RemoveAll("archive.tar")
+			_, err = io.Copy(outFile, input)
+			Expect(err).To(BeNil())
+			containerConfig := &types.ContainerConfig{
+				Metadata: &types.ContainerMetadata{Name: "name"},
+				Image: &types.ImageSpec{
+					Image: "archive.tar",
+				},
+			}
+			// When
+
+			_, err = sut.CRImportCheckpoint(
+				context.Background(),
+				containerConfig,
+				"",
+				"",
+			)
+
+			// Then
+			Expect(err.Error()).To(Equal(`PodSandboxId should not be empty`))
+		})
+	})
+	t.Describe("ContainerRestore from archive into new pod", func() {
+		It("should succeed", func() {
+			// Given
+			addContainerAndSandbox()
+			testContainer.SetStateAndSpoofPid(&oci.ContainerState{
+				State: specs.State{Status: oci.ContainerStateRunning},
+			})
+
+			err := os.WriteFile(
+				"spec.dump",
+				[]byte(`{"annotations":{"io.kubernetes.cri-o.Metadata"`+
+					`:"{\"name\":\"container-to-restore\"}",`+
+					`"io.kubernetes.cri-o.Annotations": "{\"name\":\"NAME\"}",`+
+					`"io.kubernetes.cri-o.Labels": "{\"io.kubernetes.container.name\":\"counter\"}",`+
+					`"io.kubernetes.cri-o.SandboxID": "sandboxID"},`+
+					`"mounts": [{"destination": "/proc"},`+
+					`{"destination":"/data","source":"/data","options":`+
+					`["rw","ro","rbind","rprivate","rshared","rslaved"]}],`+
+					`"linux": {"maskedPaths": ["/proc/acpi"], "readonlyPaths": ["/proc/asound"]}}`),
+				0o644,
+			)
+			Expect(err).To(BeNil())
+			defer os.RemoveAll("spec.dump")
+			err = os.WriteFile("config.dump", []byte(`{"rootfsImageName": "image"}`), 0o644)
+			Expect(err).To(BeNil())
+			defer os.RemoveAll("config.dump")
+			outFile, err := os.Create("archive.tar")
+			Expect(err).To(BeNil())
+			defer outFile.Close()
+			input, err := archive.TarWithOptions(".", &archive.TarOptions{
+				Compression:      archive.Uncompressed,
+				IncludeSourceDir: true,
+				IncludeFiles:     []string{"spec.dump", "config.dump"},
+			})
+			Expect(err).To(BeNil())
+			defer os.RemoveAll("archive.tar")
+			_, err = io.Copy(outFile, input)
+			Expect(err).To(BeNil())
+			containerConfig := &types.ContainerConfig{
+				Image: &types.ImageSpec{
+					Image: "archive.tar",
+				},
+				Linux: &types.LinuxContainerConfig{
+					Resources:       &types.LinuxContainerResources{},
+					SecurityContext: &types.LinuxContainerSecurityContext{},
+				},
+				Metadata: &types.ContainerMetadata{
+					Name: "new-container-name",
+				},
+				Mounts: []*types.Mount{{
+					ContainerPath: "/data",
+					HostPath:      "/data",
+				}},
+			}
+
+			size := uint64(100)
+			gomock.InOrder(
+				imageServerMock.EXPECT().ResolveNames(
+					gomock.Any(), gomock.Any()).
+					Return([]string{"image"}, nil),
+
+				imageServerMock.EXPECT().ImageStatus(
+					gomock.Any(), gomock.Any()).
+					Return(&storage.ImageResult{
+						ID:   "image",
+						User: "10", Size: &size,
+					}, nil),
+
+				runtimeServerMock.EXPECT().CreateContainer(gomock.Any(), gomock.Any(),
+					gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any(),
+					gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any(),
+					gomock.Any(), gomock.Any()).
+					Return(storage.ContainerInfo{
+						Config: &v1.Image{
+							Config: v1.ImageConfig{
+								Entrypoint: []string{"sh"},
+							},
+						},
+					},
+						nil,
+					),
+				runtimeServerMock.EXPECT().StartContainer(gomock.Any()).
+					Return(emptyDir, nil),
+			)
+
+			// When
+
+			_, err = sut.CRImportCheckpoint(
+				context.Background(),
+				containerConfig,
+				"",
+				"",
+			)
+
+			// Then
+			Expect(err).To(BeNil())
 		})
 	})
 })
