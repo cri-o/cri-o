3dab91d5609e41ff0b0881e8a33e3f243582a9c1
Ayato Tokubi <atokubi@redhat.com>
2024-12-19 16:43:33 +0000

    Avoid using UpdateContainerStatus for ReopenContainerLog and add logs tests
    Signed-off-by: Ayato Tokubi <atokubi@redhat.com>
    
    (cherry picked from commit 05296551a060b9c96f0ca9001a0abb37780a5dc3)
    Signed-off-by: Ayato Tokubi <atokubi@redhat.com>

--- internal/oci/runtime_pod.go
+++ internal/oci/runtime_pod.go
@@ -296,3 +296,7 @@ func (r *runtimePod) ReopenContainerLog(ctx context.Context, c *Container) error
 		ID: c.ID(),
 	})
 }
+
+func (r *runtimePod) IsContainerAlive(c *Container) bool {
+	return c.Living() == nil
+}
