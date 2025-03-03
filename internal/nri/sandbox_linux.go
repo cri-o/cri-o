e95984e0d95738c4d2b4253f3efc44090ceefc08
Filipe Xavier <felipeaggger@gmail.com>
2025-03-03 14:23:32 -0300

    populate updatePodSandboxRequest and create tests
    Signed-off-by: Filipe Xavier <felipe_life@live.com>

--- internal/nri/sandbox_linux.go
+++ internal/nri/sandbox_linux.go
@@ -20,3 +20,12 @@ func podSandboxToNRI(pod PodSandbox) *nri.PodSandbox {
 
 	return nriPod
 }
+
+func createUpdatePodSandboxRequest(pod PodSandbox) *nri.UpdatePodSandboxRequest {
+	podNri := podSandboxToNRI(pod)
+	return &nri.UpdatePodSandboxRequest{
+		Pod:                    podNri,
+		OverheadLinuxResources: podNri.Linux.PodOverhead,
+		LinuxResources:         podNri.Linux.PodResources,
+	}
+}
