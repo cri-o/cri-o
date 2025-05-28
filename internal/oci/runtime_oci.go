cri-o/cri-o

commit c2255b71872df7383f13d15ab88a36b99403d16f

oci: add debug logs for runtime state calls
Signed-off-by: Peter Hunt <pehunt@redhat.com>

internal/oci/runtime_oci.go
@@ -718,6 +718,11 @@ func (r *runtimeOCI) UpdateContainerStatus(c *Container) error {
 			// went away we do not error out stopping kubernetes to recover.
 			// We always populate the fields below so kube can restart/reschedule
 			// containers failing.
+			if exitErr, isExitError := err.(*exec.ExitError); isExitError {
+				logrus.Errorf("failed to update container state for %s: stdout: %s, stderr: %s", c.id, string(out), string(exitErr.Stderr))
+			} else {
+				logrus.Errorf("failed to update container state for %s: %v", c.id, err)
+			}
 			c.state.Status = ContainerStateStopped
 			if err := updateContainerStatusFromExitFile(c); err != nil {
 				c.state.Finished = time.Now()
@@ -788,6 +793,7 @@ func (r *runtimeOCI) UpdateContainerStatus(c *Container) error {
 			return fmt.Errorf("status code conversion failed: %v", err)
 		}
 		c.state.ExitCode = utils.Int32Ptr(int32(statusCode))
+		logrus.Debugf("found exit code for %s: %d", c.id, statusCode)
 	}
 
 	oomFilePath := filepath.Join(c.bundlePath, "oom")

commit 25b3dfb58c3aab7da7927c2953f0359c7cf7d44e

UpdateContainerStatus: fix error logging
Commit c2255b718 introduced error logging in case low-level runtime
"state" command fails. It had a special case for exec.ExitError, which
was correct at the time.

Commit f09c1d31b later switched this code to use runtimeCmd wrapper.
That wrapper do not return stdout in case of an error, and also sets
cmd.Stderr to a buffer, meaning exec.ExitError won't have Stderr filled
in.

This explains why both printed stdout and stderr are empty, but the
issue was not uncovered until commit c69b09563, which replaced error
typecasting with error unwrapping. So, now return of a wrapped
`exec.ExitError` from runtimeCmd resulted in printing empty stdout and
stderr.

The fix is trivial -- always print an error as is.

While at it, fix a comment placement (misplaced since commit c2255b7).

Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

internal/oci/runtime_oci.go
@@ -1106,19 +1106,13 @@ func (r *runtimeOCI) UpdateContainerStatus(ctx context.Context, c *Container) er
 	stateCmd := func() (*ContainerState, bool, error) {
 		out, err := r.runtimeCmd("state", c.ID())
 		if err != nil {
-			// there are many code paths that could lead to have a bad state in the
+			log.Errorf(ctx, "Failed to update container state for %s: %v", c.ID(), err)
+			// There are many code paths that could lead to have a bad state in the
 			// underlying runtime.
 			// On any error like a container went away or we rebooted and containers
 			// went away we do not error out stopping kubernetes to recover.
 			// We always populate the fields below so kube can restart/reschedule
 			// containers failing.
-			var exitErr *exec.ExitError
-			if errors.As(err, &exitErr) {
-				log.Errorf(ctx, "Failed to update container state for %s: stdout: %s, stderr: %s", c.ID(), out, string(exitErr.Stderr))
-			} else {
-				log.Errorf(ctx, "Failed to update container state for %s: %v", c.ID(), err)
-			}
-
 			c.state.Status = ContainerStateStopped
 			if err := updateContainerStatusFromExitFile(c); err != nil {
 				log.Errorf(ctx, "Failed to update container status from exit file for %s: %v", c.ID(), err)