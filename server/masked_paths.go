dc9d78ed3de128b525e5a5cd0dc6ffb4e4e22e6a
Ayato Tokubi <atokubi@redhat.com>
2025-08-26 12:28:08 +0000

    Remove deprecate masks
    Signed-off-by: Ayato Tokubi <atokubi@redhat.com>

--- server/masked_paths.go
+++ server/masked_paths.go
@@ -1,12 +1,8 @@
 package server
 
 import (
-	"fmt"
 	"os"
-	"runtime"
 	"slices"
-	"strconv"
-	"strings"
 	"sync"
 
 	"github.com/containers/common/pkg/config"
@@ -22,65 +18,19 @@ func appendDefaultMaskedPaths(additionalPaths []string) []string {
 }
 
 // defaultLinuxMaskedPaths will be used to evaluate the default masked paths once.
-var defaultLinuxMaskedPaths = sync.OnceValue(func() []string {
+func defaultLinuxMaskedPaths() []string {
+	maskedPaths := defaultLinuxMaskedPathsWithoutThermalThrottle()
+	slices.DeleteFunc(maskedPaths, func(path string) bool {
+		_, err := os.Stat(path)
+		return err != nil
+	})
+	return maskedPaths
+}
+
+var defaultLinuxMaskedPathsWithoutThermalThrottle = sync.OnceValue(func() []string {
 	maskedPaths := slices.Concat(
 		config.DefaultMaskedPaths(),
 		[]string{"/proc/asound", "/proc/interrupts"},
 	)
-
-	for _, cpu := range possibleCPUs() {
-		path := fmt.Sprintf("/sys/devices/system/cpu/cpu%d/thermal_throttle", cpu)
-		if _, err := os.Stat(path); err == nil {
-			maskedPaths = append(maskedPaths, path)
-		}
-	}
-
 	return maskedPaths
-})
-
-// possibleCPUs returns the number of possible CPUs on this host.
-func possibleCPUs() (cpus []int) {
-	if ncpu := possibleCPUsParsed(); ncpu != nil {
-		return ncpu
-	}
-
-	for i := range runtime.NumCPU() {
-		cpus = append(cpus, i)
-	}
-
-	return cpus
-}
-
-// possibleCPUsParsed is parsing the amount of possible CPUs on this host from
-// /sys/devices.
-var possibleCPUsParsed = sync.OnceValue(func() (cpus []int) {
-	data, err := os.ReadFile("/sys/devices/system/cpu/possible")
-	if err != nil {
-		return nil
-	}
-
-	for r := range strings.SplitSeq(strings.TrimSpace(string(data)), ",") {
-		if rStart, rEnd, ok := strings.Cut(r, "-"); !ok {
-			cpu, err := strconv.Atoi(rStart)
-			if err != nil {
-				return nil
-			}
-			cpus = append(cpus, cpu)
-		} else {
-			var start, end int
-			start, err := strconv.Atoi(rStart)
-			if err != nil {
-				return nil
-			}
-			end, err = strconv.Atoi(rEnd)
-			if err != nil {
-				return nil
-			}
-			for i := start; i <= end; i++ {
-				cpus = append(cpus, i)
-			}
-		}
-	}
-
-	return cpus
 })
