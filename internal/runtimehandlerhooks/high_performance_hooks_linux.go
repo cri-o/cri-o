2172b77f4dfd737ba451450d469cb6853a8c89f7
Sohan Kunkerkar <sohank2602@gmail.com>
2025-09-09 09:01:10 -0400

    internal/factory/container: fix CPU weight setting
    This resolves Pod InPlace Resize Container test failure where cpu.weight file
    was empty for Guaranteed QoS pods with restartable init containers. Sometimes,
    systemd fails to convert CPUShares → CPUWeight for this specific scenario,
    likely due to how it handles complex pod hierarchies with concurrent containers,
    but the exact internal mechanism is unclear. This acts more as a safeguard in
    case systemd doesn’t set the value.
    
    Signed-off-by: Sohan Kunkerkar <sohank2602@gmail.com>

--- internal/runtimehandlerhooks/high_performance_hooks_linux.go
+++ internal/runtimehandlerhooks/high_performance_hooks_linux.go
@@ -1136,6 +1136,11 @@ func isCgroupParentBestEffort(s *sandbox.Sandbox) bool {
 }
 
 func isContainerRequestWholeCPU(cSpec *specs.Spec) bool {
+	if cSpec == nil || cSpec.Linux == nil || cSpec.Linux.Resources == nil ||
+		cSpec.Linux.Resources.CPU == nil || cSpec.Linux.Resources.CPU.Shares == nil {
+		return false
+	}
+
 	return *(cSpec.Linux.Resources.CPU.Shares)%1024 == 0
 }
 
