e85f5140561c773156af8d27a6332d0759d91913
Snir Schreiber <ssheribe@redhat.com>
2025-10-05 16:09:02 +0300

    Simplify runtimeVM struct by removing redundant fields
    This eliminates duplication and makes the struct cleaner with a single
    source of truth for configuration values from the handler.
    
    Signed-off-by: Snir Schreiber <ssheribe@redhat.com>

--- internal/oci/runtime_vm.go
+++ internal/oci/runtime_vm.go
@@ -53,15 +53,11 @@ import (
 // runtimeVM is the Runtime interface implementation that is more appropriate
 // for VM based container runtimes.
 type runtimeVM struct {
-	path       string
-	fifoDir    string
-	configPath string
-	exitsPath  string
-	pullImage  bool
-	ctx        context.Context
-	client     *ttrpc.Client
-	task       task.TaskService
-	handler    *config.RuntimeHandler
+	exitsPath string
+	ctx       context.Context
+	client    *ttrpc.Client
+	task      task.TaskService
+	handler   *config.RuntimeHandler
 
 	sync.Mutex
 	ctrs map[string]containerInfo
@@ -98,15 +94,16 @@ func newRuntimeVM(handler *config.RuntimeHandler, exitsPath string) RuntimeImpl 
 	typeurl.Register(&rspec.WindowsResources{}, prefix, "opencontainers/runtime-spec", major, "WindowsResources")
 
 	return &runtimeVM{
-		path:       handler.RuntimePath,
-		configPath: handler.RuntimeConfigPath,
-		exitsPath:  exitsPath,
-		pullImage:  handler.RuntimePullImage,
-		fifoDir:    filepath.Join(handler.RuntimeRoot, "crio", "fifo"),
-		ctx:        context.Background(),
-		handler:    handler,
-		ctrs:       make(map[string]containerInfo),
+		exitsPath: exitsPath,
+		ctx:       context.Background(),
+		handler:   handler,
+		ctrs:      make(map[string]containerInfo),
 	}
+}
+
+// getFIFOPath returns the FIFO path for the container.
+func (r *runtimeVM) getFIFOPath() string {
+	return filepath.Join(r.handler.RuntimeRoot, "crio", "fifo")
 }
 
 func addVolumeMountsToCreateRequest(ctx context.Context, request *task.CreateTaskRequest, c *Container) error {
@@ -162,9 +159,9 @@ func (r *runtimeVM) CreateContainer(ctx context.Context, c *Container, cgroupPar
 	// fail early if something goes wrong.
 	var opts *anypb.Any = nil
 
-	if r.configPath != "" {
+	if r.handler.RuntimeConfigPath != "" {
 		runtimeOptions := &runtimeoptions.Options{
-			ConfigPath: r.configPath,
+			ConfigPath: r.handler.RuntimeConfigPath,
 		}
 
 		marshaledOtps, err := typeurl.MarshalAny(runtimeOptions)
@@ -180,7 +177,7 @@ func (r *runtimeVM) CreateContainer(ctx context.Context, c *Container, cgroupPar
 		return err
 	}
 
-	containerIO, err := r.createContainerIO(ctx, c, cio.WithNewFIFOs(r.fifoDir, c.terminal, c.stdin))
+	containerIO, err := r.createContainerIO(ctx, c, cio.WithNewFIFOs(r.getFIFOPath(), c.terminal, c.stdin))
 	if err != nil {
 		return err
 	}
@@ -210,7 +207,7 @@ func (r *runtimeVM) CreateContainer(ctx context.Context, c *Container, cgroupPar
 		Options:  opts,
 	}
 
-	if r.pullImage {
+	if r.handler.RuntimePullImage {
 		err := addVolumeMountsToCreateRequest(ctx, request, c)
 		if err != nil {
 			log.Warnf(ctx, "Failed to add KataVirtualVolume information to CreateContainer: %v", err)
@@ -272,7 +269,7 @@ func (r *runtimeVM) startRuntimeDaemon(ctx context.Context, c *Container) error 
 	cmd, err := client.Command(
 		r.ctx,
 		&client.CommandConfig{
-			Runtime: r.path,
+			Runtime: r.handler.RuntimePath,
 			Path:    c.BundlePath(),
 			Args:    args,
 		},
@@ -477,7 +474,7 @@ func (r *runtimeVM) execContainerCommon(ctx context.Context, c *Container, cmd [
 	}
 
 	// Create IO fifos
-	execIO, err := cio.NewExecIO(c.ID(), r.fifoDir, tty, stdin != nil)
+	execIO, err := cio.NewExecIO(c.ID(), r.getFIFOPath(), tty, stdin != nil)
 	if err != nil {
 		return execError, errdefs.FromGRPC(err)
 	}
@@ -911,7 +908,7 @@ func (r *runtimeVM) restoreContainerIO(ctx context.Context, c *Container, state 
 		Stderr:   state.GetStderr(),
 	}
 	// The existing fifos is created by NewFIFOSetInDir. stdin, stdout, stderr should exist
-	// in a same temporary directory under r.fifoDir. crio is responsible for removing these
+	// in a same temporary directory under the fifo directory. crio is responsible for removing these
 	// files after container io is closed.
 	var iofiles []string
 	if cioCfg.Stdin != "" {
