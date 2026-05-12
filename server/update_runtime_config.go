c4dee0661f4c38644ef5d16cf08b744190c9723c
nispriha <njagan@redhat.com>
2026-05-12 16:37:01 +0530

    Implement UpdateRuntimeConfig CRI method
    Signed-off-by: nispriha <njagan@redhat.com>

--- server/update_runtime_config.go
+++ server/update_runtime_config.go
@@ -4,10 +4,20 @@ import (
 	"context"
 
 	types "k8s.io/cri-api/pkg/apis/runtime/v1"
+
+	"github.com/cri-o/cri-o/internal/log"
 )
 
 func (s *Server) UpdateRuntimeConfig(
 	ctx context.Context, req *types.UpdateRuntimeConfigRequest,
 ) (*types.UpdateRuntimeConfigResponse, error) {
+	ctx, span := log.StartSpan(ctx)
+	defer span.End()
+
+	podCIDR := req.GetRuntimeConfig().GetNetworkConfig().GetPodCidr()
+	if podCIDR != "" {
+		log.Infof(ctx, "Updating runtime config with pod CIDR: %s", podCIDR)
+	}
+
 	return &types.UpdateRuntimeConfigResponse{}, nil
 }
