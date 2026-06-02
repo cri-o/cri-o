9e0ff2a90fb319afeba6b6538c71eec8803056bc
Kubernetes Release Robot <k8s-release-robot@users.noreply.github.com>
2026-06-02 10:58:16 +0000

    version: bump to 1.35.4
    Signed-off-by: Kubernetes Release Robot <k8s-release-robot@users.noreply.github.com>

--- internal/version/version.go
+++ internal/version/version.go
@@ -21,7 +21,7 @@ import (
 )
 
 // Version is the version of the build.
-const Version = "1.35.3"
+const Version = "1.35.4"
 
 // ReleaseMinorVersions are the currently supported minor versions.
 var ReleaseMinorVersions = []string{"1.34", "1.33", "1.32"}
